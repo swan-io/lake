42691fff0d7b045315409cda6c9a1a1bcc589cc8
Mathieu Acthernoene <zoontek@gmail.com>
2023-03-22 16:12:55 +0100

    Switch vite config to TS

--- packages/lake/vite.config.ts
+++ packages/lake/vite.config.ts
@@ -2,12 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
-    alias: {
-      "react-native": "react-native-web",
-    },
+    alias: { "react-native": "react-native-web" },
   },
   test: {
-    setupFiles: ["scripts/tests/testSetup.ts"],
     environment: "jsdom",
     watch: false,
     threads: false,
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
-    alias: {
-      "react-native": "react-native-web",
-    },
+    alias: { "react-native": "react-native-web" },
   },
   test: {
+    setupFiles: ["scripts/tests/testSetup.ts"],
     environment: "jsdom",
     watch: false,
     threads: false,
