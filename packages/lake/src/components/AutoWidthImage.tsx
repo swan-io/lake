f72dfd38c11723f8e9245219d663284ee9c436a7
Mathieu Acthernoene <zoontek@gmail.com>
2023-03-31 14:40:19 +0200

    Simplify AutoWidthImage

--- packages/lake/src/components/AutoWidthImage.tsx
+++ packages/lake/src/components/AutoWidthImage.tsx
@@ -1,41 +1,48 @@
 import { forwardRef, memo, useEffect, useState } from "react";
-import isEqual from "react-fast-compare";
 import { Image, ImageProps } from "react-native";
 import { Except } from "type-fest";
 import { isNotNullish } from "../utils/nullish";
 
 type Props = Except<ImageProps, "source"> & {
+  ariaLabel?: string;
   maxWidth?: number;
   height: number;
   sourceUri: string;
 };
 
 export const AutoWidthImage = memo(
-  forwardRef<Image, Props>(({ maxWidth, height, sourceUri, style, ...props }, forwardedRef) => {
-    const [size, setSize] = useState<{ height: number; width: number }>({
-      height,
-      width: 0,
-    });
-
-    useEffect(() => {
-      Image.getSize(sourceUri, (fetchedWidth, fetchedHeight) => {
-        const ratio = fetchedWidth / fetchedHeight;
-        const width = ratio * height;
-
-        if (isNotNullish(maxWidth) && maxWidth < width) {
-          const height = maxWidth / ratio;
-          setSize({ height, width: maxWidth });
-        } else {
-          setSize({ height, width });
-        }
+  forwardRef<Image, Props>(
+    ({ ariaLabel, maxWidth, height, sourceUri, style, ...props }, forwardedRef) => {
+      const [size, setSize] = useState<{ height: number; width: number }>({
+        height,
+        width: 0,
       });
-    }, [maxWidth, height, sourceUri]);
 
-    return (
-      <Image ref={forwardedRef} source={{ uri: sourceUri }} style={[style, size]} {...props} />
-    );
-  }),
-  isEqual,
+      useEffect(() => {
+        Image.getSize(sourceUri, (fetchedWidth, fetchedHeight) => {
+          const ratio = fetchedWidth / fetchedHeight;
+          const width = ratio * height;
+
+          if (isNotNullish(maxWidth) && maxWidth < width) {
+            const height = maxWidth / ratio;
+            setSize({ height, width: maxWidth });
+          } else {
+            setSize({ height, width });
+          }
+        });
+      }, [maxWidth, height, sourceUri]);
+
+      return (
+        <Image
+          aria-label={ariaLabel}
+          ref={forwardedRef}
+          source={{ uri: sourceUri }}
+          style={[style, size]}
+          {...props}
+        />
+      );
+    },
+  ),
 );
 
 AutoWidthImage.displayName = "AutoWidthImage";
