4059d8f1ad14040645f0e11f03434cfe5fa760df
Epimodev <Epimodev@users.noreply.github.com>
2023-04-17 13:38:57 +0000

    deploy: 5c331093e370ab37903880d31fd041b52d8565ea

--- assets/1.7.0/useBodyClassName-bd00433d.js
+++
@@ -1,2 +0,0 @@
-import{r as e}from"./index-f1f749bf.js";const r=(o,{enabled:t=!0}={})=>{e.useEffect(()=>(t?document.body.classList.add(o):document.body.classList.remove(o),()=>{document.body.classList.remove(o)}),[o,t])};export{r as u};
-//# sourceMappingURL=useBodyClassName-bd00433d.js.map
