af92687f500b0e434650c8050c7704c448b23a1e
zoontek <zoontek@users.noreply.github.com>
2023-04-17 13:23:15 +0000

    deploy: 6d0f6aa4533f4a06287f43e4c46509cd75160570

---
+++ assets/1.7.0/CopyButton.stories-ab3b0c22.js
@@ -0,0 +1,34 @@
+import{S,j as c,a as e,b as l}from"./Space-131d2659.js";import{r as u}from"./index-f1f749bf.js";import{B as v}from"./index.module-1db6bb3b.js";import{L as r,c as x}from"./LakeCopyButton-e50af072.js";import{L as a}from"./LakeLabel-aac4e0bb.js";import{L}from"./LakeTextInput-14b7eb18.js";import{S as b,a as y}from"./_StoriesComponents-4317655e.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-042e6b4d.js";import"./Icon-73a40682.js";import"./Svg-2acfd8ba.js";import"./LakeTooltip-617ea287.js";import"./index-96c5f47c.js";import"./useMergeRefs-66f03dce.js";import"./Portal-73206726.js";import"./WithCurrentColor-109acf67.js";import"./Pressable-9d717e71.js";import"./index-ca925b5c.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Fill-d3a29646.js";import"./index-6257b084.js";const h=S.create({inputPart:{maxWidth:400}}),G={title:"Interactivity/CopyButton",component:r},d=()=>{const o=new Date,i=o.getHours(),t=o.getMinutes(),p=o.getSeconds();return`${i}:${t}:${p}`},n=()=>{const[o,i]=u.useState(""),[t,p]=u.useState(()=>d());return u.useEffect(()=>{const s=setInterval(()=>{p(d())},1e3);return()=>clearInterval(s)},[]),c(b,{title:"CopyButton",children:[e(y,{title:"Sizes",children:c(v,{direction:"row",alignItems:"start",children:[e(a,{label:`Default size (${x})`,render:()=>e(r,{copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 16",render:()=>e(r,{size:16,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 21",render:()=>e(r,{size:21,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 26",render:()=>e(r,{size:26,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})})]})}),e(y,{title:"You can test if value is well copied by paste it in this input",style:h.inputPart,children:e(a,{label:"Paste here",render:()=>e(L,{value:o,onChange:s=>i(s.currentTarget.value)})})})]})};var C,m,T;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`() => {
+  const [pastedValue, setPastedValue] = useState("");
+  const [valueToCopy, setValueToCopy] = useState(() => getTime());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValueToCopy(getTime());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return <StoryBlock title="CopyButton">
+      <StoryPart title="Sizes">
+        <Box direction="row" alignItems="start">
+          <LakeLabel label={\`Default size (\${copyButtondefaultSize})\`} render={() => <LakeCopyButton copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 16" render={() => <LakeCopyButton size={16} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 21" render={() => <LakeCopyButton size={21} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 26" render={() => <LakeCopyButton size={26} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="You can test if value is well copied by paste it in this input" style={styles.inputPart}>
+        <LakeLabel label="Paste here" render={() => <LakeTextInput value={pastedValue} onChange={event => setPastedValue(event.currentTarget.value)} />} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(T=(m=n.parameters)==null?void 0:m.docs)==null?void 0:T.source}}};const J=["Variations"];export{n as Variations,J as __namedExportsOrder,G as default};
+//# sourceMappingURL=CopyButton.stories-ab3b0c22.js.map
