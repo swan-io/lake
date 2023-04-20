81c77029c348a98cde82ff9c037b5c586fdbb060
Epimodev <Epimodev@users.noreply.github.com>
2023-04-20 16:46:08 +0000

    deploy: f986ef77fc0bfe6727602a5ef8866e87e57a5d2a

--- assets/1.8.0/preview-8b355808.js
+++ assets/1.8.0/preview-8b355808.js
@@ -1,2 +1,2 @@
 const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{method:"alphabetical"}}};export{o as parameters};
-//# sourceMappingURL=preview-984be32a.js.map
+//# sourceMappingURL=preview-8b355808.js.map
