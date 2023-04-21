af9089165c710dd866d7d066c1e9a870765d3c16
Mathieu Acthernoene <zoontek@gmail.com>
2023-04-21 12:08:07 +0200

    Update dependencies (#50)
    * Use vite/client types
    
    * Update dependencies

--- packages/lake/src/utils/function.ts
+++ packages/lake/src/utils/function.ts
@@ -13,11 +13,14 @@ export const memoize = <Input extends Array<unknown>, Output>(
   getCacheKey: (...input: Input) => string,
 ): ((...input: Input) => Output) => {
   const cache = new Map<string, Output>();
+
   return (...input: Input) => {
     const key = getCacheKey(...input);
+
     if (cache.has(key)) {
       return cache.get(key) as Output;
     }
+
     const output = func(...input);
     cache.set(key, output);
     return output;
