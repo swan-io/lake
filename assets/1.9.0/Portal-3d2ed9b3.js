41e0d58d6741eb7454a5f3012e9c412b546efb37
zoontek <zoontek@users.noreply.github.com>
2023-04-21 10:09:56 +0000

    deploy: af9089165c710dd866d7d066c1e9a870765d3c16

--- assets/1.9.0/Portal-3d2ed9b3.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./index-f1f749bf.js";import{r as s}from"./index-96c5f47c.js";import{C as c,u as l}from"./WithCurrentColor-d0b6ed11.js";const o=({children:r,container:e})=>{const a=t.useContext(c),n=t.useRef(e);return l(n,a),s.createPortal(r,e)};try{o.displayName="Portal",o.__docgenInfo={description:"",displayName:"Portal",props:{container:{defaultValue:null,description:"",name:"container",required:!0,type:{name:"Element"}}}}}catch{}export{o as P};
-//# sourceMappingURL=Portal-3d2ed9b3.js.map
