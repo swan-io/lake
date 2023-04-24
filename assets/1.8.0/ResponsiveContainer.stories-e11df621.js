4d8f1be5c02d7e0265f451d6f9b7f4ebdf0c4013
Epimodev <Epimodev@users.noreply.github.com>
2023-04-24 12:12:52 +0000

    deploy: b2c3a9640b6769b9266a6260d3ab39a07d641bfe

---
+++ assets/1.8.0/ResponsiveContainer.stories-e11df621.js
@@ -0,0 +1,35 @@
+import{S as y,c as k,j as l,a as e,V as c,b as s,l as u,L as r}from"./Space-5c29d500.js";import{r as f}from"./index-f1f749bf.js";import{B as i}from"./index.module-bef243ca.js";import{L as g}from"./LakeLabel-9a52756e.js";import{R as m}from"./ResponsiveContainer-488f74a8.js";import{S as x}from"./Switch-45dcf7b5.js";import{S as w}from"./_StoriesComponents-bbcd7ec3.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-042e6b4d.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Animated-6594fad5.js";import"./index-37690212.js";import"./index-c86de1d8.js";import"./index-b2b01097.js";import"./math-d8257b44.js";import"./Icon-27bbe041.js";import"./Svg-ff455c54.js";import"./index-37abc2ac.js";const t=y.create({container:{width:"100%"},containerMobile:{maxWidth:400},switchContainer:{alignSelf:"flex-start"},block:{width:120,height:60,backgroundColor:k.gray[0],borderRadius:8}}),q={title:"Layout/ResponsiveContainer",component:m},n=()=>{const[a,b]=f.useState(!1);return l(w,{title:"ResponsiveContainer",description:["ResponsiveContainer is a component that allows you to render different content depending on the screen size.",'You can try it by toggling the "Mobile mode" switch'],children:[e(g,{label:"Mobile mode",render:()=>e(c,{style:t.switchContainer,children:e(x,{value:a,onValueChange:b})})}),e(s,{height:16}),e(c,{style:a?t.containerMobile:t.container,children:e(m,{breakpoint:u.tiny,children:({small:o})=>l(i,{direction:o?"column":"row",children:[e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})})]})})})]})};var p,h,d;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const [forceMobileWidth, setForceMobileWidth] = useState(false);
+  return <StoryBlock title="ResponsiveContainer" description={["ResponsiveContainer is a component that allows you to render different content depending on the screen size.", 'You can try it by toggling the "Mobile mode" switch']}>
+      <LakeLabel label="Mobile mode" render={() => <View style={styles.switchContainer}>
+            <Switch value={forceMobileWidth} onValueChange={setForceMobileWidth} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={forceMobileWidth ? styles.containerMobile : styles.container}>
+        <ResponsiveContainer breakpoint={breakpoints.tiny}>
+          {({
+          small
+        }) => <Box direction={small ? "column" : "row"}>
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+            </Box>}
+        </ResponsiveContainer>
+      </View>
+    </StoryBlock>;
+}`,...(d=(h=n.parameters)==null?void 0:h.docs)==null?void 0:d.source}}};const A=["Default"];export{n as Default,A as __namedExportsOrder,q as default};
+//# sourceMappingURL=ResponsiveContainer.stories-e11df621.js.map
---
+++ assets/1.8.0/TransitionView.stories-a5bc7a4a.js
@@ -0,0 +1,21 @@
+import{S as p,c as l,j as h,a as e,V as i,b as w,L as y}from"./Space-5c29d500.js";import{r as d}from"./index-f1f749bf.js";import{B as u}from"./index.module-bef243ca.js";import{L as S}from"./LakeLabel-9a52756e.js";import{S as f}from"./Switch-45dcf7b5.js";import{T as c}from"./TransitionView-c4b12149.js";import{S as g}from"./_StoriesComponents-bbcd7ec3.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-042e6b4d.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Animated-6594fad5.js";import"./index-37690212.js";import"./index-c86de1d8.js";import"./index-b2b01097.js";import"./math-d8257b44.js";import"./Icon-27bbe041.js";import"./Svg-ff455c54.js";import"./index-37abc2ac.js";const t=p.create({switchContainer:{alignSelf:"flex-start"},container:{position:"relative",maxWidth:400},enterAnimation:{animationKeyframes:{"0%":{opacity:0,transform:"translateZ(0px) translateX(200px)"}},animationDuration:"300ms"},leaveAnimation:{animationKeyframes:{"100%":{opacity:0,transform:"translateZ(0px) translateX(200px)"}},animationDuration:"300ms"},block:{position:"absolute",top:0,left:0,width:"100%",height:60,backgroundColor:l.negative[400],borderRadius:8}}),Y={title:"Animations/TransitionView",component:c},n=()=>{const[o,m]=d.useState(!1);return h(g,{title:"TransitionView",description:["TransitionView is component triggers a transition when an element enters or leaves the DOM.","You can try it by toggling the switch"],children:[e(S,{label:"Switch displayed content",render:()=>e(i,{style:t.switchContainer,children:e(f,{value:o,onValueChange:m})})}),e(w,{height:16}),e(i,{style:t.container,children:e(c,{style:t.container,enter:t.enterAnimation,leave:t.leaveAnimation,children:o?e(u,{justifyContent:"center",alignItems:"center",style:t.block,children:e(y,{color:l.live.contrast,variant:"semibold",children:"Second block"})}):null})})]})};var a,r,s;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  const [showBlock, setShowBlock] = useState(false);
+  return <StoryBlock title="TransitionView" description={["TransitionView is component triggers a transition when an element enters or leaves the DOM.", "You can try it by toggling the switch"]}>
+      <LakeLabel label="Switch displayed content" render={() => <View style={styles.switchContainer}>
+            <Switch value={showBlock} onValueChange={setShowBlock} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={styles.container}>
+        <TransitionView style={styles.container} enter={styles.enterAnimation} leave={styles.leaveAnimation}>
+          {showBlock ? <Box justifyContent="center" alignItems="center" style={styles.block}>
+              <LakeText color={colors.live.contrast} variant="semibold">
+                Second block
+              </LakeText>
+            </Box> : null}
+        </TransitionView>
+      </View>
+    </StoryBlock>;
+}`,...(s=(r=n.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const Z=["Default"];export{n as Default,Z as __namedExportsOrder,Y as default};
+//# sourceMappingURL=TransitionView.stories-a5bc7a4a.js.map
