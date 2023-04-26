ad894d7e1eaa094ae65cf24e54f71ebdf8c7e543
zoontek <zoontek@users.noreply.github.com>
2023-04-26 15:31:00 +0000

    deploy: 8d346aa89ef492717b47c5c6d671af606f7bc63a

---
+++ assets/1.9.1/ResponsiveContainer.stories-de98daa4.js
@@ -0,0 +1,35 @@
+import{S as y,c as k,j as l,a as e,V as c,b as s,l as u,L as r}from"./Space-fb680d8f.js";import{r as f}from"./index-f1f749bf.js";import{B as i}from"./index.module-89595a06.js";import{L as g}from"./LakeLabel-e8475586.js";import{R as m}from"./ResponsiveContainer-721a00f5.js";import{S as x}from"./Switch-2f217f2f.js";import{S as w}from"./_StoriesComponents-cffce164.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-042e6b4d.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Animated-08ee11e7.js";import"./index-972f480f.js";import"./index-5b1466cf.js";import"./index-99e396fc.js";import"./math-2bedbf6c.js";import"./Icon-6ed2878b.js";import"./Svg-c89cbc0b.js";import"./index-75b38219.js";const t=y.create({container:{width:"100%"},containerMobile:{maxWidth:400},switchContainer:{alignSelf:"flex-start"},block:{width:120,height:60,backgroundColor:k.gray[0],borderRadius:8}}),q={title:"Layout/ResponsiveContainer",component:m},n=()=>{const[a,b]=f.useState(!1);return l(w,{title:"ResponsiveContainer",description:["ResponsiveContainer is a component that allows you to render different content depending on the screen size.",'You can try it by toggling the "Mobile mode" switch'],children:[e(g,{label:"Mobile mode",render:()=>e(c,{style:t.switchContainer,children:e(x,{value:a,onValueChange:b})})}),e(s,{height:16}),e(c,{style:a?t.containerMobile:t.container,children:e(m,{breakpoint:u.tiny,children:({small:o})=>l(i,{direction:o?"column":"row",children:[e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})})]})})})]})};var p,h,d;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const [forceMobileWidth, setForceMobileWidth] = useState(false);
+  return <StoryBlock title="ResponsiveContainer" description={["ResponsiveContainer is a component that allows you to render different content depending on the screen size.", 'You can try it by toggling the "Mobile mode" switch']}>
+      <LakeLabel label="Mobile mode" render={() => <View style={styles.switchContainer}>
+            <Switch value={forceMobileWidth} onValueChange={setForceMobileWidth} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={forceMobileWidth ? styles.containerMobile : styles.container}>
+        <ResponsiveContainer breakpoint={breakpoints.tiny}>
+          {({
+          small
+        }) => <Box direction={small ? "column" : "row"}>
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+            </Box>}
+        </ResponsiveContainer>
+      </View>
+    </StoryBlock>;
+}`,...(d=(h=n.parameters)==null?void 0:h.docs)==null?void 0:d.source}}};const A=["Default"];export{n as Default,A as __namedExportsOrder,q as default};
+//# sourceMappingURL=ResponsiveContainer.stories-de98daa4.js.map
---
+++ assets/1.9.1/function-624a6799.js
@@ -0,0 +1,2 @@
+import"./Boxed-aef12ad0.js";const n=()=>{};export{n};
+//# sourceMappingURL=function-624a6799.js.map
