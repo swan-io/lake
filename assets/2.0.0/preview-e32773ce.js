f40cdc5381a1e802a38aff7e3cdd64ebe3f1036f
Epimodev <Epimodev@users.noreply.github.com>
2023-04-27 16:26:05 +0000

    deploy: 6267fd4eb441b9f6b683536134e53ce776b01dae

--- assets/2.0.0/preview-e32773ce.js
+++
@@ -1,2 +0,0 @@
-import{d as t}from"./dayjs.min-33e2be6b.js";import{u as o,c as e,r as a,l as r}from"./utc-fe2a1d3a.js";import"./_commonjsHelpers-042e6b4d.js";t.extend(o);t.extend(e);t.extend(a);t.extend(r);const m={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{method:"alphabetical"}}};export{m as parameters};
-//# sourceMappingURL=preview-e32773ce.js.map
