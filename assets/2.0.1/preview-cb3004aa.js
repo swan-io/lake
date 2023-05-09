f7ac24792e77659ceab43d849f5bf71a8497cd1a
zoontek <zoontek@users.noreply.github.com>
2023-05-09 09:26:35 +0000

    deploy: b1c28fd4998ef6bd2bdcdaaeb543936bce935c0e

--- assets/2.0.1/preview-cb3004aa.js
+++ assets/2.0.1/preview-cb3004aa.js
@@ -1,2 +1,2 @@
 const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{method:"alphabetical"}}};export{o as parameters};
-//# sourceMappingURL=preview-69ac9c74.js.map
+//# sourceMappingURL=preview-cb3004aa.js.map
