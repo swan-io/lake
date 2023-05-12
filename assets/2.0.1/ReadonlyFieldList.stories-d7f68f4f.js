c831770bb9830b29c86156c104052508a1ad5a30
Epimodev <Epimodev@users.noreply.github.com>
2023-05-12 15:56:32 +0000

    deploy: 94a96ab83fe983599a661b425279504aef7e233a

---
+++ assets/2.0.1/ReadonlyFieldList.stories-d7f68f4f.js
@@ -0,0 +1,30 @@
+import{a as e,F as m,i as k,j as y,V as b,L as r,c as o}from"./Space-866c6d2d.js";import{L as T}from"./LakeCopyButton-2e108323.js";import{L as a}from"./LakeLabel-2a8f3c0c.js";import{r as g}from"./index-ebeaab24.js";import{c as x,S as h}from"./_StoriesComponents-143445d5.js";import{T as c}from"./Tag-9b4c9700.js";import"./extends-98964cd2.js";import"./Icon-3493efb8.js";import"./Svg-36b671a5.js";import"./LakeTooltip-b50c3212.js";import"./index-9c09ad76.js";import"./index-9b96a6d6.js";import"./useMergeRefs-f233a2be.js";import"./Portal-0e36c32f.js";import"./WithCurrentColor-cfe5d31c.js";import"./Pressable-4fafd1ef.js";import"./index-ca925b5c.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./index-b75cb4fd.js";const t=({children:i})=>e(m,{children:g.Children.toArray(i).filter(k).map((L,n)=>y(b,{children:[n!==0&&e(x,{horizontal:!1,space:8}),L]},n))});try{t.displayName="ReadOnlyFieldList",t.__docgenInfo={description:"",displayName:"ReadOnlyFieldList",props:{}}}catch{}const z={title:"Forms/ReadOnlyFieldList",component:t},l=()=>e(h,{title:"Readonly field list",children:y(t,{children:[e(a,{label:"Card holder",type:"view",render:()=>e(r,{color:o.gray[900],children:"John Doe"})}),e(a,{label:"Account",type:"view",render:()=>e(r,{color:o.gray[900],children:"Compte courant"})}),e(a,{label:"Account holder",type:"view",render:()=>e(r,{color:o.gray[900],children:"John Doe"})}),e(a,{label:"Type",type:"view",render:()=>e(r,{color:o.gray[900],children:"Single use"})}),e(a,{label:"Name",type:"view",render:()=>e(r,{color:o.gray[900],children:"John Doe"})}),e(a,{label:"Status",type:"view",render:()=>e(r,{color:o.gray[900],children:e(c,{color:"positive",children:"Enabled"})})}),e(a,{label:"Contract expiry date",type:"view",render:()=>e(r,{color:o.gray[900],children:e(c,{color:"gray",children:"Unlimited"})})}),e(a,{label:"ID",type:"view",render:()=>e(r,{color:o.gray[900],children:"123456789"}),actions:e(T,{valueToCopy:"123456789",copyText:"Click to copy",copiedText:"Copied to clipboard!"})}),e(a,{label:"Created",type:"view",render:()=>e(r,{color:o.gray[900],children:"16-01-2023"})}),e(a,{label:"Updated",type:"view",render:()=>e(r,{color:o.gray[900],children:"17-01-2023"})})]})});var d,p,s;l.parameters={...l.parameters,docs:{...(d=l.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Readonly field list">
+      <ReadOnlyFieldList>
+        <LakeLabel label="Card holder" type="view" render={() => <LakeText color={colors.gray[900]}>John Doe</LakeText>} />
+
+        <LakeLabel label="Account" type="view" render={() => <LakeText color={colors.gray[900]}>Compte courant</LakeText>} />
+
+        <LakeLabel label="Account holder" type="view" render={() => <LakeText color={colors.gray[900]}>John Doe</LakeText>} />
+
+        <LakeLabel label="Type" type="view" render={() => <LakeText color={colors.gray[900]}>Single use</LakeText>} />
+
+        <LakeLabel label="Name" type="view" render={() => <LakeText color={colors.gray[900]}>John Doe</LakeText>} />
+
+        <LakeLabel label="Status" type="view" render={() => <LakeText color={colors.gray[900]}>
+              <Tag color="positive">Enabled</Tag>
+            </LakeText>} />
+
+        <LakeLabel label="Contract expiry date" type="view" render={() => <LakeText color={colors.gray[900]}>
+              <Tag color="gray">Unlimited</Tag>
+            </LakeText>} />
+
+        <LakeLabel label="ID" type="view" render={() => <LakeText color={colors.gray[900]}>123456789</LakeText>} actions={<LakeCopyButton valueToCopy="123456789" copyText="Click to copy" copiedText="Copied to clipboard!" />} />
+
+        <LakeLabel label="Created" type="view" render={() => <LakeText color={colors.gray[900]}>16-01-2023</LakeText>} />
+
+        <LakeLabel label="Updated" type="view" render={() => <LakeText color={colors.gray[900]}>17-01-2023</LakeText>} />
+      </ReadOnlyFieldList>
+    </StoryBlock>;
+}`,...(s=(p=l.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const q=["Default"];export{l as Default,q as __namedExportsOrder,z as default};
+//# sourceMappingURL=ReadonlyFieldList.stories-d7f68f4f.js.map
