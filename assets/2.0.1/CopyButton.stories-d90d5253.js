6fd3d055b229250b37662fe547e21194b3122df2
sandrine-ds <sandrine-ds@users.noreply.github.com>
2023-05-17 07:26:01 +0000

    deploy: 17c2b2577b3fc13c113e11e3746d4e5d4458791a

---
+++ assets/2.0.1/CopyButton.stories-d90d5253.js
@@ -0,0 +1,34 @@
+import{S,j as c,a as e,b as l}from"./Space-d8a3a2fb.js";import{r as u}from"./index-ebeaab24.js";import{B as v}from"./index-792e89f6.js";import{L as r,c as x}from"./LakeCopyButton-48e146e0.js";import{L as a}from"./LakeLabel-5e3d1023.js";import{L}from"./LakeTextInput-94b8be42.js";import{S as b,a as y}from"./_StoriesComponents-209aaa6f.js";import"./extends-98964cd2.js";import"./Icon-b1271a47.js";import"./Svg-e0dc1839.js";import"./LakeTooltip-23ccc337.js";import"./index-9c09ad76.js";import"./useMergeRefs-c81cc1be.js";import"./Portal-7eda4751.js";import"./WithCurrentColor-c06016a8.js";import"./Pressable-f145b70b.js";import"./index-ca925b5c.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Fill-1cd895f0.js";import"./index-e69b7e8d.js";const h=S.create({inputPart:{maxWidth:400}}),F={title:"Interactivity/CopyButton",component:r},d=()=>{const o=new Date,i=o.getHours(),t=o.getMinutes(),p=o.getSeconds();return`${i}:${t}:${p}`},n=()=>{const[o,i]=u.useState(""),[t,p]=u.useState(()=>d());return u.useEffect(()=>{const s=setInterval(()=>{p(d())},1e3);return()=>clearInterval(s)},[]),c(b,{title:"CopyButton",children:[e(y,{title:"Sizes",children:c(v,{direction:"row",alignItems:"start",children:[e(a,{label:`Default size (${x})`,render:()=>e(r,{copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 16",render:()=>e(r,{size:16,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 21",render:()=>e(r,{size:21,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 26",render:()=>e(r,{size:26,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})})]})}),e(y,{title:"You can test if value is well copied by paste it in this input",style:h.inputPart,children:e(a,{label:"Paste here",render:()=>e(L,{value:o,onChange:s=>i(s.currentTarget.value)})})})]})};var C,m,T;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`() => {
+  const [pastedValue, setPastedValue] = useState("");
+  const [valueToCopy, setValueToCopy] = useState(() => getTime());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValueToCopy(getTime());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return <StoryBlock title="CopyButton">
+      <StoryPart title="Sizes">
+        <Box direction="row" alignItems="start">
+          <LakeLabel label={\`Default size (\${copyButtondefaultSize})\`} render={() => <LakeCopyButton copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 16" render={() => <LakeCopyButton size={16} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 21" render={() => <LakeCopyButton size={21} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 26" render={() => <LakeCopyButton size={26} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="You can test if value is well copied by paste it in this input" style={styles.inputPart}>
+        <LakeLabel label="Paste here" render={() => <LakeTextInput value={pastedValue} onChange={event => setPastedValue(event.currentTarget.value)} />} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(T=(m=n.parameters)==null?void 0:m.docs)==null?void 0:T.source}}};const G=["Variations"];export{n as Variations,G as __namedExportsOrder,F as default};
+//# sourceMappingURL=CopyButton.stories-d90d5253.js.map
---
+++ assets/2.0.1/Toast.stories-e3b1c30e.js
@@ -0,0 +1,103 @@
+import{S as A,e as $,h as j,a as t,V as P,j as g,c as l,b as m,L as E,F as O,f as x}from"./Space-d8a3a2fb.js";import{L as d}from"./LakeButton-b38e79d4.js";import{r as v}from"./index-ebeaab24.js";import{K as W,B as D}from"./index-792e89f6.js";import{w as F}from"./with-selector-6c55c874.js";import{A as S,E as T}from"./Animated-796203dc.js";import{I as f}from"./Icon-b1271a47.js";import{P as N}from"./Portal-7eda4751.js";import{P as Y}from"./Pressable-f145b70b.js";import{T as H}from"./TransitionGroupView-8f85057e.js";import{S as _,a as B}from"./_StoriesComponents-209aaa6f.js";import"./extends-98964cd2.js";import"./index-e69b7e8d.js";import"./index-1cb3eff1.js";import"./index-a06d815a.js";import"./index-da6f378b.js";import"./index-4ca979f8.js";import"./Svg-e0dc1839.js";import"./index-9c09ad76.js";import"./WithCurrentColor-c06016a8.js";import"./useMergeRefs-c81cc1be.js";import"./Boxed-b55f7b9d.js";import"./TransitionView-12824d5b.js";function G(o){var n=new Set,r=o;return{get:function(){return r},set:function(e){r=typeof e=="function"?e(r):e,n.forEach(function(s){return s(r)})},subscribe:function(e){return n.add(e),function(){n.delete(e)}},reset:function(){r=o,n.forEach(function(e){return e(r)})}}}function K(o){return o}function M(o,n){return n===void 0&&(n=Object.is),F.useSyncExternalStoreWithSelector(o.subscribe,o.get,o.get,K,n)}const X=o=>{const{duration:n,onStart:r,onReset:e,onEnd:s}=o;let a=0,u=n;const i=()=>{document.removeEventListener("visibilitychange",L),window.clearTimeout(a)},y=()=>{u<=0||(a=window.setTimeout(()=>{u=0,i(),s()},u),r(n))},c=()=>{window.clearTimeout(a),u=n,a=window.setTimeout(()=>{u=0,i(),s()},n),e(n)},L=()=>{document.removeEventListener("visibilitychange",L),y()};return document.hidden?document.addEventListener("visibilitychange",L):y(),{duration:n,clear:i,reset:c}},b=G([]),q=()=>M(b),I=o=>{var r,e;const n=b.get().find(s=>s.uid===o);n&&((r=n.timeout)==null||r.clear(),(e=n.progress)==null||e.stopAnimation(),b.set(s=>s.filter(a=>a.uid!==o)))},p=({variant:o,title:n,description:r,autoClose:e})=>{const s=`${o} - ${n} - ${r??""}`,a=b.get().find(c=>c.uid===s);if(a!=null)return a.timeout&&a.progress&&(a.timeout.clear(),S.timing(a.progress,{duration:100,easing:T.linear,toValue:1,useNativeDriver:!1}).start(()=>{var c;(c=a.timeout)==null||c.reset()})),s;const i=e??(o==="info"||o==="success")?new S.Value(1):void 0,y=i?X({duration:1e4,onStart:c=>{S.timing(i,{duration:c,easing:T.linear,toValue:0,useNativeDriver:!1}).start()},onReset:c=>{S.timing(i,{duration:c,easing:T.linear,toValue:0,useNativeDriver:!1}).start()},onEnd:()=>{I(s)}}):void 0;return b.set(c=>[{uid:s,variant:o,title:n,description:r,progress:i,timeout:y},...c]),s},w=A.create({list:{position:"absolute",right:0,bottom:0,maxHeight:"100%",maxWidth:400,paddingVertical:8,width:"100%",zIndex:10},toastWrapper:{paddingHorizontal:16,paddingVertical:8},toast:{padding:24,borderRadius:4,borderWidth:1,borderLeftWidth:4,overflow:"hidden",boxShadow:$.modal},contentContainer:{paddingRight:36},closeButton:{zIndex:1,position:"absolute",width:24,height:24,right:24,top:0,bottom:0,margin:"auto"},progressBar:{height:2,transformOrigin:"left"}}),J=v.memo(({variant:o,uid:n,title:r,description:e,progress:s,onClose:a})=>{const u=v.useRef(null),i=W(o).with("success",()=>"positive").with("error",()=>"negative").with("info",()=>"shakespear").with("warning",()=>"warning").exhaustive();return v.useEffect(()=>{if(j(s))return;const y=s.addListener(({value:c})=>{u.current instanceof HTMLElement&&(u.current.style.transform=`scaleX(${c})`)});return()=>s.removeListener(y)},[s]),t(P,{style:w.toastWrapper,children:g(P,{style:[w.toast,{borderColor:l[i][200],borderLeftColor:l[i][500],backgroundColor:l[i][0]}],children:[g(D,{style:w.contentContainer,children:[g(D,{direction:"row",alignItems:"center",children:[W(o).with("success",()=>t(f,{name:"checkmark-circle-regular",size:20,color:l[i][700]})).with("error",()=>t(f,{name:"dismiss-circle-regular",size:20,color:l[i][700]})).with("info",()=>t(f,{name:"info-regular",size:20,color:l[i][700]})).with("warning",()=>t(f,{name:"warning-regular",size:20,color:l[i][700]})).exhaustive(),t(m,{width:12}),t(E,{variant:"regular",color:l[i][700],children:r})]}),e!=null&&g(O,{children:[t(m,{height:8}),t(E,{variant:"smallRegular",color:l.gray[700],children:e})]})]}),t(Y,{onPress:()=>a(n),style:w.closeButton,children:t(f,{name:"lake-close",size:24,color:l.gray[500]})}),s!=null&&g(O,{children:[t(m,{height:24}),t(P,{ref:u,role:"progressbar",style:[w.progressBar,{backgroundColor:l[i][500]}]})]})]})})}),z=()=>{const o=q(),[n,r]=v.useState(()=>{});return v.useEffect(()=>{const e=document.createElement("div");return document.body.append(e),r(e),()=>{e.remove(),r(void 0)}},[]),n==null?null:t(N,{container:n,children:t(H,{style:w.list,enter:x.fadeAndSlideInFromRight.enter,leave:x.fadeAndSlideInFromRight.leave,children:o.map(e=>t(J,{uid:e.uid,variant:e.variant,progress:e.progress,title:e.title,description:e.description,onClose:I},e.uid))})})},h={button:{width:200}},bt={title:"Interactivity/Toast",component:z},k=()=>g(O,{children:[t(z,{}),g(_,{title:"Toast",children:[g(B,{title:"With only title",children:[t(d,{color:"positive",style:h.button,onPress:()=>p({variant:"success",title:"Successfully copy to clipboard !"}),children:"Open success toast"}),t(m,{height:16}),t(d,{color:"negative",style:h.button,onPress:()=>p({variant:"error",title:"Oops ! Something went wrong"}),children:"Open error toast"}),t(m,{height:16}),t(d,{color:"shakespear",style:h.button,onPress:()=>p({variant:"info",title:"Successfully copy to clipboard !"}),children:"Open success toast"}),t(m,{height:16}),t(d,{color:"warning",style:h.button,onPress:()=>p({variant:"warning",title:"Oops ! Something went wrong"}),children:"Open warning toast"})]}),g(B,{title:"With title and description",children:[t(d,{color:"positive",style:h.button,onPress:()=>p({variant:"success",title:"Successfully copy to clipboard !",description:"You can now paste it anywhere"}),children:"Open success toast"}),t(m,{height:16}),t(d,{color:"negative",style:h.button,onPress:()=>p({variant:"error",title:"Oops ! Something went wrong",description:"Please retry or contact us"}),children:"Open error toast"}),t(m,{height:16}),t(d,{color:"shakespear",style:h.button,onPress:()=>p({variant:"info",title:"Successfully copy to clipboard !",description:"You can now paste it anywhere"}),children:"Open success toast"}),t(m,{height:16}),t(d,{color:"warning",style:h.button,onPress:()=>p({variant:"warning",title:"Oops ! Something went wrong",description:"Talk to an expert"}),children:"Open warning toast"})]}),g(B,{title:"With long title and description",children:[t(d,{color:"positive",style:h.button,onPress:()=>p({variant:"success",title:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut"}),children:"Open success toast"}),t(m,{height:16}),t(d,{color:"positive",style:h.button,onPress:()=>p({variant:"success",title:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut"}),children:"Open success toast"})]})]})]});var V,C,R;k.parameters={...k.parameters,docs:{...(V=k.parameters)==null?void 0:V.docs,source:{originalSource:`() => {
+  return <>
+      <ToastStack />
+
+      <StoryBlock title="Toast">
+        <StoryPart title="With only title">
+          <LakeButton color="positive" style={styles.button} onPress={() => showToast({
+          variant: "success",
+          title: "Successfully copy to clipboard !"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="negative" style={styles.button} onPress={() => showToast({
+          variant: "error",
+          title: "Oops ! Something went wrong"
+        })}>
+            Open error toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="shakespear" style={styles.button} onPress={() => showToast({
+          variant: "info",
+          title: "Successfully copy to clipboard !"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="warning" style={styles.button} onPress={() => showToast({
+          variant: "warning",
+          title: "Oops ! Something went wrong"
+        })}>
+            Open warning toast
+          </LakeButton>
+        </StoryPart>
+
+        <StoryPart title="With title and description">
+          <LakeButton color="positive" style={styles.button} onPress={() => showToast({
+          variant: "success",
+          title: "Successfully copy to clipboard !",
+          description: "You can now paste it anywhere"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="negative" style={styles.button} onPress={() => showToast({
+          variant: "error",
+          title: "Oops ! Something went wrong",
+          description: "Please retry or contact us"
+        })}>
+            Open error toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="shakespear" style={styles.button} onPress={() => showToast({
+          variant: "info",
+          title: "Successfully copy to clipboard !",
+          description: "You can now paste it anywhere"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="warning" style={styles.button} onPress={() => showToast({
+          variant: "warning",
+          title: "Oops ! Something went wrong",
+          description: "Talk to an expert"
+        })}>
+            Open warning toast
+          </LakeButton>
+        </StoryPart>
+
+        <StoryPart title="With long title and description">
+          <LakeButton color="positive" style={styles.button} onPress={() => showToast({
+          variant: "success",
+          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="positive" style={styles.button} onPress={() => showToast({
+          variant: "success",
+          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut"
+        })}>
+            Open success toast
+          </LakeButton>
+        </StoryPart>
+      </StoryBlock>
+    </>;
+}`,...(R=(C=k.parameters)==null?void 0:C.docs)==null?void 0:R.source}}};const St=["Default"];export{k as Default,St as __namedExportsOrder,bt as default};
+//# sourceMappingURL=Toast.stories-e3b1c30e.js.map
