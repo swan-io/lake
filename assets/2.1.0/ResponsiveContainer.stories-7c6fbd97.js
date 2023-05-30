a19dc34504c0f1254098b8a257a78df9f3510cfe
zoontek <zoontek@users.noreply.github.com>
2023-05-30 09:45:33 +0000

    deploy: 75b13177f184d4ef2cf60189e3da06fc0446379a

---
+++ assets/2.1.0/ResponsiveContainer.stories-7c6fbd97.js
@@ -0,0 +1,35 @@
+import{S as y,c as k,j as l,a as e,V as c,b as s,m as u,L as r}from"./Space-e35c911d.js";import{r as f}from"./index-ebeaab24.js";import{B as i}from"./index-20e6f370.js";import{L as g}from"./LakeLabel-71103375.js";import{R as m}from"./ResponsiveContainer-d5440b19.js";import{S as x}from"./Switch-85805bd5.js";import{S as w}from"./_StoriesComponents-2262edbc.js";import"./extends-98964cd2.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Animated-8d06a1e9.js";import"./index-35289edc.js";import"./index-102f1fad.js";import"./index-95b850cb.js";import"./math-f1529a8d.js";import"./Icon-5b6031a2.js";import"./Svg-fcc77fb1.js";import"./index-90a64126.js";const t=y.create({container:{width:"100%"},containerMobile:{maxWidth:400},switchContainer:{alignSelf:"flex-start"},block:{width:120,height:60,backgroundColor:k.gray[0],borderRadius:8}}),O={title:"Layout/ResponsiveContainer",component:m},n=()=>{const[a,b]=f.useState(!1);return l(w,{title:"ResponsiveContainer",description:["ResponsiveContainer is a component that allows you to render different content depending on the screen size.",'You can try it by toggling the "Mobile mode" switch'],children:[e(g,{label:"Mobile mode",render:()=>e(c,{style:t.switchContainer,children:e(x,{value:a,onValueChange:b})})}),e(s,{height:16}),e(c,{style:a?t.containerMobile:t.container,children:e(m,{breakpoint:u.tiny,children:({small:o})=>l(i,{direction:o?"column":"row",children:[e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})})]})})})]})};var h,p,d;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`() => {
+  const [forceMobileWidth, setForceMobileWidth] = useState(false);
+  return <StoryBlock title="ResponsiveContainer" description={["ResponsiveContainer is a component that allows you to render different content depending on the screen size.", 'You can try it by toggling the "Mobile mode" switch']}>
+      <LakeLabel label="Mobile mode" render={() => <View style={styles.switchContainer}>
+            <Switch value={forceMobileWidth} onValueChange={setForceMobileWidth} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={forceMobileWidth ? styles.containerMobile : styles.container}>
+        <ResponsiveContainer breakpoint={breakpoints.tiny}>
+          {({
+          small
+        }) => <Box direction={small ? "column" : "row"}>
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+            </Box>}
+        </ResponsiveContainer>
+      </View>
+    </StoryBlock>;
+}`,...(d=(p=n.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const q=["Default"];export{n as Default,q as __namedExportsOrder,O as default};
+//# sourceMappingURL=ResponsiveContainer.stories-7c6fbd97.js.map
