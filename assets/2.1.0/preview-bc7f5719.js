dd26a35773c13fc61a5d67e568569c53fdc849d6
sandrine-ds <sandrine-ds@users.noreply.github.com>
2023-06-12 16:27:35 +0000

    deploy: 471710a67e3b6e2fc9c0a5dab709d090975755a8

--- assets/2.1.0/preview-bc7f5719.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{method:"alphabetical"}}};export{o as parameters};
-//# sourceMappingURL=preview-bc7f5719.js.map
