aaa9783aaa684a2f78ab20d00e2a463cedbde265
Epimodev <godin.fred8@gmail.com>
2023-06-20 11:57:55 +0200

    setup text and colors

--- packages/lake/__stories__/Card3dPreview.stories.tsx
+++ packages/lake/__stories__/Card3dPreview.stories.tsx
@@ -1,6 +1,17 @@
 import { Meta } from "@storybook/react";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import Card3dPreview from "../src/components/Card3dPreview";
+import { LakeLabel } from "../src/components/LakeLabel";
+import { LakeTextInput } from "../src/components/LakeTextInput";
+import { RadioGroup } from "../src/components/RadioGroup";
 import { StoryBlock, StoryPart } from "./_StoriesComponents";
+
+const styles = StyleSheet.create({
+  canvasContainer: {
+    height: 500,
+  },
+});
 
 export default {
   title: "Informations/Card3dPreview",
@@ -8,15 +19,40 @@ export default {
 } as Meta<typeof Card3dPreview>;
 
 export const Card = () => {
+  const [name, setName] = useState("John Doe");
+  const [color, setColor] = useState<"Silver" | "Black">("Silver");
+
   return (
     <StoryBlock title="Card">
       <StoryPart title="Default">
-        <Card3dPreview
-          ownerName="John Doe"
-          cardNumber="1234 5678 9012 3456"
-          expirationDate="12/24"
-          cvv="123"
+        <LakeLabel
+          label="Name"
+          render={() => <LakeTextInput value={name} onChangeText={setName} />}
         />
+
+        <LakeLabel
+          label="Color"
+          render={() => (
+            <RadioGroup
+              value={color}
+              onValueChange={setColor}
+              items={[
+                { name: "Silver", value: "Silver" },
+                { name: "Black", value: "Black" },
+              ]}
+            />
+          )}
+        />
+
+        <View style={styles.canvasContainer}>
+          <Card3dPreview
+            color={color}
+            ownerName={name}
+            cardNumber="1234 5678 9012 3456"
+            expirationDate="12/24"
+            cvv="123"
+          />
+        </View>
       </StoryPart>
     </StoryBlock>
   );
