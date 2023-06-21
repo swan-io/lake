78f40599dddae5f65a907983b5c74ab16f2fe8ca
zoontek <zoontek@users.noreply.github.com>
2023-06-21 08:47:22 +0000

    deploy: ce8521d32f44644b7715b31a866365e479c8a034

--- assets/2.2.0/preview-8935b1ce.js
+++
@@ -1,2 +0,0 @@
-const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{method:"alphabetical"}}};export{o as parameters};
-//# sourceMappingURL=preview-8935b1ce.js.map
