30821e350972ac9eaf6312ea1549b20adcdc40d3
Epimodev <Epimodev@users.noreply.github.com>
2023-06-23 16:05:58 +0000

    deploy: 8d473fce7723843349ebf3ee5dacc8fb38a064e6

---
+++ assets/2.3.1/Button.stories-578c5876.js
@@ -0,0 +1,66 @@
+import{S as g,j as c,a as t,c as s,L as k}from"./Space-09b2011d.js";import{r as x}from"./index-ebeaab24.js";import{B as i}from"./index-35ad09cf.js";import{L as S}from"./LakeButton-7b54ef6d.js";import{S as b,a as e}from"./_StoriesComponents-d63fc2ec.js";import"./extends-98964cd2.js";import"./Icon-b21292d8.js";import"./Svg-acec65c8.js";import"./Pressable-12f0a478.js";import"./useMergeRefs-c821bd07.js";import"./index-cc3175a7.js";const n=g.create({buttons:{flexWrap:"wrap"},buttonWrapper:{marginRight:32,marginBottom:16},button:{minWidth:200},countValue:{width:24}}),T={title:"Interactivity/Button",component:S},u=o=>o.charAt(0).toUpperCase()+o.slice(1),r=o=>{const[W,C]=x.useState(0);return c(i,{direction:"row",alignItems:"center",style:n.buttonWrapper,children:[t(S,{...o,style:n.button,onPress:()=>C(P=>P+1)}),t(k,{align:"right",style:n.countValue,children:W})]})},l=()=>c(b,{title:"Button colors",children:[t(e,{title:"Primary",children:t(i,{direction:"row",style:n.buttons,children:Object.keys(s).map(o=>t(r,{mode:"primary",color:o,children:u(o)},o))})}),t(e,{title:"Secondary",children:t(i,{direction:"row",style:n.buttons,children:Object.keys(s).map(o=>t(r,{mode:"secondary",color:o,children:u(o)},o))})}),t(e,{title:"Tertiary",children:t(i,{direction:"row",style:n.buttons,children:Object.keys(s).map(o=>t(r,{mode:"tertiary",color:o,children:u(o)},o))})})]}),a=()=>c(b,{title:"Button states",children:[t(e,{title:"Small",children:t(r,{size:"small",color:"live",children:"Small button"})}),t(e,{title:"Disabled",children:t(r,{disabled:!0,color:"live",children:"Disabled"})}),t(e,{title:"Loading",children:t(r,{loading:!0,color:"live",children:"Loading"})}),t(e,{title:"With pill",children:t(r,{pill:!0,color:"live",children:"Pill"})}),t(e,{title:"With icon",children:c(i,{direction:"row",style:n.buttons,children:[t(r,{icon:"add-circle-filled",color:"positive",children:"Add"}),t(r,{icon:"delete-filled",iconPosition:"end",color:"negative",children:"Remove"})]})})]});var d,y,m;l.parameters={...l.parameters,docs:{...(d=l.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Button colors">
+      <StoryPart title="Primary">
+        <Box direction="row" style={styles.buttons}>
+          {Object.keys(colors).map(color => <ButtonWithCounter key={color} mode="primary" color={(color as ColorVariants)}>
+              {capitalize(color)}
+            </ButtonWithCounter>)}
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Secondary">
+        <Box direction="row" style={styles.buttons}>
+          {Object.keys(colors).map(color => <ButtonWithCounter mode="secondary" key={color} color={(color as ColorVariants)}>
+              {capitalize(color)}
+            </ButtonWithCounter>)}
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Tertiary">
+        <Box direction="row" style={styles.buttons}>
+          {Object.keys(colors).map(color => <ButtonWithCounter mode="tertiary" key={color} color={(color as ColorVariants)}>
+              {capitalize(color)}
+            </ButtonWithCounter>)}
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(m=(y=l.parameters)==null?void 0:y.docs)==null?void 0:m.source}}};var h,p,B;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Button states">
+      <StoryPart title="Small">
+        <ButtonWithCounter size="small" color="live">
+          Small button
+        </ButtonWithCounter>
+      </StoryPart>
+
+      <StoryPart title="Disabled">
+        <ButtonWithCounter disabled={true} color="live">
+          Disabled
+        </ButtonWithCounter>
+      </StoryPart>
+
+      <StoryPart title="Loading">
+        <ButtonWithCounter loading={true} color="live">
+          Loading
+        </ButtonWithCounter>
+      </StoryPart>
+
+      <StoryPart title="With pill">
+        <ButtonWithCounter pill={true} color="live">
+          Pill
+        </ButtonWithCounter>
+      </StoryPart>
+
+      <StoryPart title="With icon">
+        <Box direction="row" style={styles.buttons}>
+          <ButtonWithCounter icon="add-circle-filled" color="positive">
+            Add
+          </ButtonWithCounter>
+
+          <ButtonWithCounter icon="delete-filled" iconPosition="end" color="negative">
+            Remove
+          </ButtonWithCounter>
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(B=(p=a.parameters)==null?void 0:p.docs)==null?void 0:B.source}}};const _=["Colors","ButtonStates"];export{a as ButtonStates,l as Colors,_ as __namedExportsOrder,T as default};
+//# sourceMappingURL=Button.stories-578c5876.js.map
---
+++ assets/2.3.1/ResponsiveContainer.stories-d5a98a9d.js
@@ -0,0 +1,35 @@
+import{S as y,c as k,j as l,a as e,V as c,b as s,p as u,L as r}from"./Space-09b2011d.js";import{r as f}from"./index-ebeaab24.js";import{B as i}from"./index-35ad09cf.js";import{L as g}from"./LakeLabel-b8b7817c.js";import{R as m}from"./ResponsiveContainer-1950009e.js";import{S as x}from"./Switch-4c2ca5a9.js";import{S as w}from"./_StoriesComponents-d63fc2ec.js";import"./extends-98964cd2.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Animated-cca32fd9.js";import"./index-96e79e21.js";import"./index-c274a5be.js";import"./index-a04573ea.js";import"./math-6724607b.js";import"./Icon-b21292d8.js";import"./Svg-acec65c8.js";import"./index-34acb216.js";const t=y.create({container:{width:"100%"},containerMobile:{maxWidth:400},switchContainer:{alignSelf:"flex-start"},block:{width:120,height:60,backgroundColor:k.gray[0],borderRadius:8}}),O={title:"Layout/ResponsiveContainer",component:m},n=()=>{const[a,b]=f.useState(!1);return l(w,{title:"ResponsiveContainer",description:["ResponsiveContainer is a component that allows you to render different content depending on the screen size.",'You can try it by toggling the "Mobile mode" switch'],children:[e(g,{label:"Mobile mode",render:()=>e(c,{style:t.switchContainer,children:e(x,{value:a,onValueChange:b})})}),e(s,{height:16}),e(c,{style:a?t.containerMobile:t.container,children:e(m,{breakpoint:u.tiny,children:({small:o})=>l(i,{direction:o?"column":"row",children:[e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})})]})})})]})};var p,h,d;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const [forceMobileWidth, setForceMobileWidth] = useState(false);
+  return <StoryBlock title="ResponsiveContainer" description={["ResponsiveContainer is a component that allows you to render different content depending on the screen size.", 'You can try it by toggling the "Mobile mode" switch']}>
+      <LakeLabel label="Mobile mode" render={() => <View style={styles.switchContainer}>
+            <Switch value={forceMobileWidth} onValueChange={setForceMobileWidth} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={forceMobileWidth ? styles.containerMobile : styles.container}>
+        <ResponsiveContainer breakpoint={breakpoints.tiny}>
+          {({
+          small
+        }) => <Box direction={small ? "column" : "row"}>
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+            </Box>}
+        </ResponsiveContainer>
+      </View>
+    </StoryBlock>;
+}`,...(d=(h=n.parameters)==null?void 0:h.docs)==null?void 0:d.source}}};const q=["Default"];export{n as Default,q as __namedExportsOrder,O as default};
+//# sourceMappingURL=ResponsiveContainer.stories-d5a98a9d.js.map
