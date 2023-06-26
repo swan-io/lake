784c8d654a245c8af352f79673b90f0f0c00471f
Epimodev <Epimodev@users.noreply.github.com>
2023-06-26 08:48:27 +0000

    deploy: 116a35290a8f486e458d4fca29413ea15bfccb00

---
+++ assets/2.3.1/RIB.stories-dedc0f6e.js
@@ -0,0 +1,36 @@
+import{S as w,r as D,f as v,c as i,d as V,y as S,l as R,a as e,j as t,V as b,k as F,b as a,T as g}from"./Space-ae3bdd0a.js";import{Q as O,B as u}from"./index-08ba51cc.js";import{F as T}from"./Fill-602d8958.js";import{L as z}from"./LakeHeading-5479ddf3.js";import{b as X,S as _,a as P}from"./_StoriesComponents-1b6767dc.js";import{S as B}from"./SwanLogo-1ade1854.js";import{W as q}from"./WithPartnerAccentColor-3fec4679.js";import{t as n}from"./i18n-6c697f11.js";import"./extends-98964cd2.js";import"./index-ebeaab24.js";import"./colors-298625c7.js";import"./Svg-29b8c088.js";import"./polished.esm-77d64d09.js";import"./rifm-2ea4b407.js";const C=26,U=200,x={objectFit:"contain",objectPosition:"left"},r=w.create({container:{width:470,borderRadius:D[8],backgroundColor:v.white,borderWidth:1,borderColor:i.gray[100]},part:{padding:V[24]},label:{...S,fontSize:10},labelPartner:{color:i.partner[500],fontWeight:"500"},mainInfo:{fontFamily:R.iban,fontSize:12,color:i.gray[900]},smallInfo:{fontFamily:R.iban,fontSize:10,color:i.gray[900]},addressInfo:{...S,fontSize:12,color:i.gray[900]},partnershipText:{...S,fontSize:8,color:i.gray[500]},defaultLogo:{height:C,width:115},swanLogo:{width:26,height:6}}),m=l=>O(l).with({version:"v1"},s=>e(W,{...s})).exhaustive(),W=({partnerColor:l,partnerLogoUrl:s,iban:I,bic:d,bank:f,agency:A,bankNumber:h,bankKey:k,bankAddress:c,accountHolderAddress:p})=>e(q,{color:l,children:t(b,{style:r.container,children:[t(b,{style:r.part,children:[t(u,{direction:"row",alignItems:"center",children:[F(s)?e("img",{src:s,width:U,height:C,style:x}):e(B,{style:r.defaultLogo}),e(T,{minWidth:24}),e(z,{level:2,variant:"h3",color:i.gray[900],children:n("rib.bankDetails")})]}),e(a,{height:24}),t(u,{direction:"row",alignItems:"center",children:[e(o,{type:"mainInfo",color:"partner",label:n("rib.iban"),value:I}),e(a,{width:24}),e(o,{type:"mainInfo",color:"partner",label:n("rib.bic"),value:d})]}),e(a,{height:8}),t(u,{direction:"row",alignItems:"center",children:[e(o,{type:"smallInfo",color:"gray",label:n("rib.bank"),value:f}),e(a,{width:24}),e(o,{type:"smallInfo",color:"gray",label:n("rib.agency"),value:A}),e(a,{width:24}),e(o,{type:"smallInfo",color:"gray",label:n("rib.number"),value:h}),e(a,{width:24}),e(o,{type:"smallInfo",color:"gray",label:n("rib.key"),value:k})]})]}),e(X,{}),t(b,{style:r.part,children:[t(u,{direction:"row",alignItems:"center",children:[e(o,{type:"addressInfo",color:"partner",label:n("rib.address"),value:[c.name,c.address,`${c.zipCode} ${c.city}`,c.country]}),e(a,{width:24}),e(o,{type:"addressInfo",color:"partner",label:n("rib.accountHolder"),value:[p.name,p.address,`${p.zipCode} ${p.city}`,p.country]})]}),e(a,{height:12}),t(u,{direction:"row",alignItems:"center",children:[e(g,{style:r.partnershipText,children:n("rib.partnership")}),e(a,{width:4}),e(B,{color:i.gray[900],style:r.swanLogo})]})]})]})}),H={mainInfo:r.mainInfo,smallInfo:r.smallInfo,addressInfo:r.addressInfo},o=({type:l,color:s,label:I,value:d})=>{const f=Array.isArray(d)?d:[d];return t(b,{children:[e(g,{style:[r.label,s==="partner"&&r.labelPartner],children:I}),e(a,{height:4}),f.map((A,h)=>e(g,{style:H[l],children:A},h))]})};try{m.displayName="RIB",m.__docgenInfo={description:"",displayName:"RIB",props:{version:{defaultValue:null,description:"",name:"version",required:!0,type:{name:'"v1"'}},partnerColor:{defaultValue:null,description:"",name:"partnerColor",required:!0,type:{name:"string"}},partnerLogoUrl:{defaultValue:null,description:"",name:"partnerLogoUrl",required:!1,type:{name:"string"}},iban:{defaultValue:null,description:"",name:"iban",required:!0,type:{name:"string"}},bic:{defaultValue:null,description:"",name:"bic",required:!0,type:{name:"string"}},bank:{defaultValue:null,description:"",name:"bank",required:!0,type:{name:"string"}},agency:{defaultValue:null,description:"",name:"agency",required:!0,type:{name:"string"}},bankNumber:{defaultValue:null,description:"",name:"bankNumber",required:!0,type:{name:"string"}},bankKey:{defaultValue:null,description:"",name:"bankKey",required:!0,type:{name:"string"}},bankAddress:{defaultValue:null,description:"",name:"bankAddress",required:!0,type:{name:"Address"}},accountHolderAddress:{defaultValue:null,description:"",name:"accountHolderAddress",required:!0,type:{name:"Address"}}}}}catch{}const oe={title:"Informations/RIB",component:m},y=()=>t(_,{title:"RIB",children:[e(P,{title:"Default",children:e(m,{version:"v1",partnerColor:"#0f6fde",partnerLogoUrl:"https://s3.eu-west-1.amazonaws.com/data.master.oina.ws/7a90851b-40f8-479d-9e0e-86d12fb001b5/SANDBOX/logo-749a2fb5-230b-4ae6-98bd-65bdb3c987c9.png",iban:"FR76 3000 1007 1600 0000 0000 123",bic:"BNPAFRPPXXX",bank:"BNP PARIBAS",agency:"PARIS 16 ETOILE",bankNumber:"00000000001",bankKey:"12",bankAddress:{name:"BNP PARIBAS",address:"16 BOULEVARD DES ITALIENS",zipCode:"75009",city:"PARIS",country:"FRANCE"},accountHolderAddress:{name:"John Doe",address:"16 BOULEVARD DES ITALIENS",zipCode:"75009",city:"PARIS",country:"FRANCE"}})}),e(P,{title:"Without logo",children:e(m,{version:"v1",partnerColor:"#0f6fde",iban:"FR76 3000 1007 1600 0000 0000 123",bic:"BNPAFRPPXXX",bank:"BNP PARIBAS",agency:"PARIS 16 ETOILE",bankNumber:"00000000001",bankKey:"12",bankAddress:{name:"BNP PARIBAS",address:"16 BOULEVARD DES ITALIENS",zipCode:"75009",city:"PARIS",country:"FRANCE"},accountHolderAddress:{name:"John Doe",address:"16 BOULEVARD DES ITALIENS",zipCode:"75009",city:"PARIS",country:"FRANCE"}})})]});var E,N,L;y.parameters={...y.parameters,docs:{...(E=y.parameters)==null?void 0:E.docs,source:{originalSource:`() => {
+  return <StoryBlock title="RIB">
+      <StoryPart title="Default">
+        <RIB version="v1" partnerColor="#0f6fde" partnerLogoUrl="https://s3.eu-west-1.amazonaws.com/data.master.oina.ws/7a90851b-40f8-479d-9e0e-86d12fb001b5/SANDBOX/logo-749a2fb5-230b-4ae6-98bd-65bdb3c987c9.png" iban="FR76 3000 1007 1600 0000 0000 123" bic="BNPAFRPPXXX" bank="BNP PARIBAS" agency="PARIS 16 ETOILE" bankNumber="00000000001" bankKey="12" bankAddress={{
+        name: "BNP PARIBAS",
+        address: "16 BOULEVARD DES ITALIENS",
+        zipCode: "75009",
+        city: "PARIS",
+        country: "FRANCE"
+      }} accountHolderAddress={{
+        name: "John Doe",
+        address: "16 BOULEVARD DES ITALIENS",
+        zipCode: "75009",
+        city: "PARIS",
+        country: "FRANCE"
+      }} />
+      </StoryPart>
+
+      <StoryPart title="Without logo">
+        <RIB version="v1" partnerColor="#0f6fde" iban="FR76 3000 1007 1600 0000 0000 123" bic="BNPAFRPPXXX" bank="BNP PARIBAS" agency="PARIS 16 ETOILE" bankNumber="00000000001" bankKey="12" bankAddress={{
+        name: "BNP PARIBAS",
+        address: "16 BOULEVARD DES ITALIENS",
+        zipCode: "75009",
+        city: "PARIS",
+        country: "FRANCE"
+      }} accountHolderAddress={{
+        name: "John Doe",
+        address: "16 BOULEVARD DES ITALIENS",
+        zipCode: "75009",
+        city: "PARIS",
+        country: "FRANCE"
+      }} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(L=(N=y.parameters)==null?void 0:N.docs)==null?void 0:L.source}}};const ie=["Colors"];export{y as Colors,ie as __namedExportsOrder,oe as default};
+//# sourceMappingURL=RIB.stories-dedc0f6e.js.map
---
+++ assets/2.3.1/ResponsiveContainer.stories-f30ff012.js
@@ -0,0 +1,35 @@
+import{S as y,c as k,j as l,a as e,V as c,b as s,p as u,L as r}from"./Space-ae3bdd0a.js";import{r as f}from"./index-ebeaab24.js";import{B as i}from"./index-08ba51cc.js";import{L as g}from"./LakeLabel-89ff2d26.js";import{R as m}from"./ResponsiveContainer-a9a6e0bf.js";import{S as x}from"./Switch-946e815a.js";import{S as w}from"./_StoriesComponents-1b6767dc.js";import"./extends-98964cd2.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Animated-697d7e8b.js";import"./index-3f1b99ef.js";import"./index-af6d3676.js";import"./index-30fb7a32.js";import"./math-76e7c16c.js";import"./Icon-77400ea4.js";import"./Svg-29b8c088.js";import"./index-56417baa.js";const t=y.create({container:{width:"100%"},containerMobile:{maxWidth:400},switchContainer:{alignSelf:"flex-start"},block:{width:120,height:60,backgroundColor:k.gray[0],borderRadius:8}}),O={title:"Layout/ResponsiveContainer",component:m},n=()=>{const[a,b]=f.useState(!1);return l(w,{title:"ResponsiveContainer",description:["ResponsiveContainer is a component that allows you to render different content depending on the screen size.",'You can try it by toggling the "Mobile mode" switch'],children:[e(g,{label:"Mobile mode",render:()=>e(c,{style:t.switchContainer,children:e(x,{value:a,onValueChange:b})})}),e(s,{height:16}),e(c,{style:a?t.containerMobile:t.container,children:e(m,{breakpoint:u.tiny,children:({small:o})=>l(i,{direction:o?"column":"row",children:[e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(i,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:o?"Mobile size":"Desktop size"})})]})})})]})};var p,h,d;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const [forceMobileWidth, setForceMobileWidth] = useState(false);
+  return <StoryBlock title="ResponsiveContainer" description={["ResponsiveContainer is a component that allows you to render different content depending on the screen size.", 'You can try it by toggling the "Mobile mode" switch']}>
+      <LakeLabel label="Mobile mode" render={() => <View style={styles.switchContainer}>
+            <Switch value={forceMobileWidth} onValueChange={setForceMobileWidth} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={forceMobileWidth ? styles.containerMobile : styles.container}>
+        <ResponsiveContainer breakpoint={breakpoints.tiny}>
+          {({
+          small
+        }) => <Box direction={small ? "column" : "row"}>
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+            </Box>}
+        </ResponsiveContainer>
+      </View>
+    </StoryBlock>;
+}`,...(d=(h=n.parameters)==null?void 0:h.docs)==null?void 0:d.source}}};const q=["Default"];export{n as Default,q as __namedExportsOrder,O as default};
+//# sourceMappingURL=ResponsiveContainer.stories-f30ff012.js.map
