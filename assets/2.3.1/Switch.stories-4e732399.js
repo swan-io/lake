8853d5e852eb218ff1964e84e039795ba960146a
Epimodev <Epimodev@users.noreply.github.com>
2023-06-26 08:21:06 +0000

    deploy: a8bca34d090f9d00f6d074bd843099a90e9a5978

---
+++ assets/2.3.1/Switch.stories-4e732399.js
@@ -0,0 +1,24 @@
+import{j as r,a as t,b as a,L as l,c as i}from"./Space-09b2011d.js";import{r as c}from"./index-ebeaab24.js";import{B as s}from"./index-35ad09cf.js";import{S as o}from"./Switch-138dbe69.js";import{S as g,a as n}from"./_StoriesComponents-d63fc2ec.js";import"./extends-98964cd2.js";import"./Animated-cca32fd9.js";import"./index-96e79e21.js";import"./index-c274a5be.js";import"./index-a04573ea.js";import"./math-6724607b.js";import"./Icon-6b24ae15.js";import"./Svg-acec65c8.js";import"./index-34acb216.js";const I={title:"Forms/Switch",component:o},e=()=>{const[p,h]=c.useState(!0),S=c.useCallback(()=>{h(w=>!w)},[]);return r(g,{title:"Switch",children:[t(n,{title:"Default",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:p,onValueChange:S}),t(a,{width:12}),t(l,{color:i.gray[700],children:"Allow physical cards"})]})}),t(n,{title:"Disabled",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:!0,disabled:!0}),t(a,{width:12}),t(l,{color:i.gray[700],children:"Allow physical cards"})]})})]})};var d,u,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  const [value, setValue] = useState<boolean>(true);
+  const toggle = useCallback(() => {
+    setValue(v => !v);
+  }, []);
+  return <StoryBlock title="Switch">
+      <StoryPart title="Default">
+        <Box direction="row" alignItems="center">
+          <Switch value={value} onValueChange={toggle} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Disabled">
+        <Box direction="row" alignItems="center">
+          <Switch value={true} disabled={true} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(m=(u=e.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};const j=["Default"];export{e as Default,j as __namedExportsOrder,I as default};
+//# sourceMappingURL=Switch.stories-4e732399.js.map
