6d4a52a038100ed1492dc7ab32152f47e6c6101f
Epimodev <Epimodev@users.noreply.github.com>
2023-06-26 11:55:29 +0000

    deploy: eab95428d61b264daa4babb4f2a6b0dd04edcbbf

---
+++ assets/2.4.0/TransitionView.stories-d8498a02.js
@@ -0,0 +1,21 @@
+import{S as p,c as l,j as h,a as e,V as i,b as w,L as y}from"./Space-e47e10d7.js";import{r as d}from"./index-ebeaab24.js";import{B as u}from"./index-4e51c3ea.js";import{L as S}from"./LakeLabel-ab785175.js";import{S as f}from"./Switch-c8b16260.js";import{T as c}from"./TransitionView-a977a46b.js";import{S as g}from"./_StoriesComponents-e5d38fee.js";import"./extends-98964cd2.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./Animated-e829a69f.js";import"./index-3deeecce.js";import"./index-5f43156e.js";import"./index-da9ddae5.js";import"./math-cb612628.js";import"./Icon-296adb1a.js";import"./Svg-7c62b3c4.js";import"./index-8c8b1adb.js";const t=p.create({switchContainer:{alignSelf:"flex-start"},container:{position:"relative",maxWidth:400},enterAnimation:{animationKeyframes:{"0%":{opacity:0,transform:"translateZ(0px) translateX(200px)"}},animationDuration:"300ms"},leaveAnimation:{animationKeyframes:{"100%":{opacity:0,transform:"translateZ(0px) translateX(200px)"}},animationDuration:"300ms"},block:{position:"absolute",top:0,left:0,width:"100%",height:60,backgroundColor:l.negative[400],borderRadius:8}}),X={title:"Animations/TransitionView",component:c},n=()=>{const[o,m]=d.useState(!1);return h(g,{title:"TransitionView",description:["TransitionView is component triggers a transition when an element enters or leaves the DOM.","You can try it by toggling the switch"],children:[e(S,{label:"Switch displayed content",render:()=>e(i,{style:t.switchContainer,children:e(f,{value:o,onValueChange:m})})}),e(w,{height:16}),e(i,{style:t.container,children:e(c,{style:t.container,enter:t.enterAnimation,leave:t.leaveAnimation,children:o?e(u,{justifyContent:"center",alignItems:"center",style:t.block,children:e(y,{color:l.live.contrast,variant:"semibold",children:"Second block"})}):null})})]})};var a,r,s;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  const [showBlock, setShowBlock] = useState(false);
+  return <StoryBlock title="TransitionView" description={["TransitionView is component triggers a transition when an element enters or leaves the DOM.", "You can try it by toggling the switch"]}>
+      <LakeLabel label="Switch displayed content" render={() => <View style={styles.switchContainer}>
+            <Switch value={showBlock} onValueChange={setShowBlock} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={styles.container}>
+        <TransitionView style={styles.container} enter={styles.enterAnimation} leave={styles.leaveAnimation}>
+          {showBlock ? <Box justifyContent="center" alignItems="center" style={styles.block}>
+              <LakeText color={colors.live.contrast} variant="semibold">
+                Second block
+              </LakeText>
+            </Box> : null}
+        </TransitionView>
+      </View>
+    </StoryBlock>;
+}`,...(s=(r=n.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const Y=["Default"];export{n as Default,Y as __namedExportsOrder,X as default};
+//# sourceMappingURL=TransitionView.stories-d8498a02.js.map
