e51a4881b7b26adf705a2a3ad8910b97db121a1a
Epimodev <Epimodev@users.noreply.github.com>
2023-06-27 08:32:01 +0000

    deploy: ac40253cfb3b5f6368e803fa40b6286c8199e934

---
+++ assets/2.4.0/Combobox.stories-2b5a385b.js
@@ -0,0 +1,34 @@
+import{S as P,j as E,a as t,L as y}from"./Space-57107f4e.js";import{A as n,P as k}from"./Boxed-b75d14a5.js";import{r as d}from"./index-8db94870.js";import{B as W}from"./index-7a357b54.js";import{L as S}from"./LakeCombobox-3e47e17c.js";import{S as B,a as r}from"./_StoriesComponents-f7676638.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-042e6b4d.js";import"./typography-6ad07e3d.js";import"./colors-298625c7.js";import"./useDisclosure-d57b00aa.js";import"./useMergeRefs-9ae9a1ea.js";import"./array-b3630b94.js";import"./Icon-cf4ea6a3.js";import"./Svg-c3c146c4.js";import"./LakeTextInput-a3d8f012.js";import"./commonStyles-d52fedae.js";import"./Fill-0b777f41.js";import"./index-c1bfc883.js";import"./LoadingView-23ab85ed.js";import"./Popover-22300165.js";import"./useResponsive-a7b20045.js";import"./BottomPanel-9aaf884d.js";import"./useBodyClassName-27d97f40.js";import"./math-b9695d7b.js";import"./FocusTrap-307c77b1.js";import"./Portal-8c029158.js";import"./index-8ce4a492.js";import"./WithCurrentColor-c53f695f.js";import"./TransitionView-07b011e8.js";import"./index-6e5c097a.js";import"./index-ab60b850.js";import"./index-58bb7859.js";const L=P.create({footer:{paddingHorizontal:12}}),lt={title:"Forms/Combobox",component:S},g=m=>{const a=fetch(`https://dummyjson.com/products/search?limit=10&q=${m}`).then(i=>i.json()).then(i=>i.products);return k.fromPromise(a)},e=m=>{const[a,i]=d.useState(null),[p,l]=d.useState(()=>n.NotAsked()),[s,x]=d.useState(""),f=a?a.title:s;return d.useEffect(()=>{if(!s){l(n.NotAsked());return}l(n.Loading());const o=g(s);return o.onResolve(C=>{l(n.Done(C))}),()=>{o.cancel()}},[s]),t(S,{value:f,items:p,onValueChange:o=>{i(null),x(o)},onSelectItem:o=>{l(n.NotAsked()),i(o)},keyExtractor:o=>o.id.toString(),renderItem:o=>t(y,{children:o.title}),emptyResultText:"No result",...m})},c=()=>E(B,{title:"Combobox",children:[t(r,{title:"By default",children:t(e,{})}),t(r,{title:"Is disabled",children:t(e,{disabled:!0})}),t(r,{title:"Is readOnly",children:t(e,{readOnly:!0})}),t(r,{title:"With icon",children:t(e,{icon:"add-circle-regular"})}),t(r,{title:"With placeholder",children:t(e,{placeholder:"Enter a product"})}),t(r,{title:"With error",children:t(e,{placeholder:"An error occurred"})}),t(r,{title:"With Footer Component",children:t(e,{ListFooterComponent:t(W,{direction:"row",justifyContent:"end",style:L.footer,children:t(y,{children:"With footer component"})})})})]});var u,b,h;c.parameters={...c.parameters,docs:{...(u=c.parameters)==null?void 0:u.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Combobox">
+      <StoryPart title="By default">
+        <EditableCombobox />
+      </StoryPart>
+
+      <StoryPart title="Is disabled">
+        <EditableCombobox disabled={true} />
+      </StoryPart>
+
+      <StoryPart title="Is readOnly">
+        <EditableCombobox readOnly={true} />
+      </StoryPart>
+
+      <StoryPart title="With icon">
+        <EditableCombobox icon={"add-circle-regular"} />
+      </StoryPart>
+
+      <StoryPart title="With placeholder">
+        <EditableCombobox placeholder={"Enter a product"} />
+      </StoryPart>
+
+      <StoryPart title="With error">
+        <EditableCombobox placeholder={"An error occurred"} />
+      </StoryPart>
+
+      <StoryPart title="With Footer Component">
+        <EditableCombobox ListFooterComponent={<Box direction="row" justifyContent="end" style={styles.footer}>
+              <LakeText>With footer component</LakeText>
+            </Box>} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(h=(b=c.parameters)==null?void 0:b.docs)==null?void 0:h.source}}};const st=["Variations"];export{c as Variations,st as __namedExportsOrder,lt as default};
+//# sourceMappingURL=Combobox.stories-2b5a385b.js.map
