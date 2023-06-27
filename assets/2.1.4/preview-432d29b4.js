b2a4ebb146014810eff217cbcf92394e1d69f893
bloodyowl <bloodyowl@users.noreply.github.com>
2023-06-27 07:04:14 +0000

    deploy: f2c62375215fb286f53e6dd592b40505c712e98a

---
+++ assets/2.1.4/preview-432d29b4.js
@@ -0,0 +1,2 @@
+const o={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{method:"alphabetical"}}};export{o as parameters};
+//# sourceMappingURL=preview-432d29b4.js.map
