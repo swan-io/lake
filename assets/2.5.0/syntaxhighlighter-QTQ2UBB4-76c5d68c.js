4cd583d8cac08cbd64d771ba2cf486bc71aa0287
sandrine-ds <sandrine-ds@users.noreply.github.com>
2023-06-28 12:52:57 +0000

    deploy: f54943e43dc68921ac5fd36c4101dbb17c858ecb

--- assets/2.5.0/syntaxhighlighter-QTQ2UBB4-76c5d68c.js
+++
@@ -1,2 +0,0 @@
-import{S as f,c as g,s as h}from"./DocsRenderer-EYKKDMVH-d93ca3ce.js";import"./iframe-68db4d26.js";import"../../sb-preview/runtime.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-49bc54d1.js";import"./index-8ce4a492.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./polished.esm-77d64d09.js";import"./extends-98964cd2.js";import"./index-356e4a49.js";export{f as SyntaxHighlighter,g as createCopyToClipboardFunction,h as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-76c5d68c.js.map
