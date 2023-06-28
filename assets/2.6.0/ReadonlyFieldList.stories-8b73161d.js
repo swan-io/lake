264f568351d1d58122ebd688865ca8c30d42452b
bloodyowl <bloodyowl@users.noreply.github.com>
2023-06-28 14:09:21 +0000

    deploy: cc9b962a3decbd02f6461aada824c207041722df

---
+++ assets/2.6.0/ReadonlyFieldList.stories-8b73161d.js
@@ -0,0 +1,30 @@
+import{a as e,F as m,i as k,j as y,V as b,L as r,c as o}from"./Space-57107f4e.js";import{L as T}from"./LakeCopyButton-b89ff022.js";import{L as a}from"./LakeLabel-9e0e0d60.js";import{r as g}from"./index-8db94870.js";import{b as x,S as h}from"./_StoriesComponents-f7676638.js";import{T as c}from"./Tag-55831b1b.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-042e6b4d.js";import"./Icon-26b2c7d1.js";import"./Svg-c3c146c4.js";import"./LakeTooltip-3cd12322.js";import"./index-8ce4a492.js";import"./index-7a357b54.js";import"./useMergeRefs-9ae9a1ea.js";import"./Portal-8c029158.js";import"./WithCurrentColor-c53f695f.js";import"./Pressable-ac4f9d8c.js";import"./index-ca925b5c.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./index-ab60b850.js";const t=({children:i})=>e(m,{children:g.Children.toArray(i).filter(k).map((L,n)=>y(b,{children:[n!==0&&e(x,{horizontal:!1,space:8}),L]},n))});try{t.displayName="ReadOnlyFieldList",t.__docgenInfo={description:"",displayName:"ReadOnlyFieldList",props:{}}}catch{}const q={title:"Forms/ReadOnlyFieldList",component:t},l=()=>e(h,{title:"Readonly field list",children:y(t,{children:[e(a,{label:"Card holder",type:"view",render:()=>e(r,{color:o.gray[900],children:"John Doe"})}),e(a,{label:"Account",type:"view",render:()=>e(r,{color:o.gray[900],children:"Compte courant"})}),e(a,{label:"Account holder",type:"view",render:()=>e(r,{color:o.gray[900],children:"John Doe"})}),e(a,{label:"Type",type:"view",render:()=>e(r,{color:o.gray[900],children:"Single use"})}),e(a,{label:"Name",type:"view",render:()=>e(r,{color:o.gray[900],children:"John Doe"})}),e(a,{label:"Status",type:"view",render:()=>e(r,{color:o.gray[900],children:e(c,{color:"positive",children:"Enabled"})})}),e(a,{label:"Contract expiry date",type:"view",render:()=>e(r,{color:o.gray[900],children:e(c,{color:"gray",children:"Unlimited"})})}),e(a,{label:"ID",type:"view",render:()=>e(r,{color:o.gray[900],children:"123456789"}),actions:e(T,{valueToCopy:"123456789",copyText:"Click to copy",copiedText:"Copied to clipboard!"})}),e(a,{label:"Created",type:"view",render:()=>e(r,{color:o.gray[900],children:"16-01-2023"})}),e(a,{label:"Updated",type:"view",render:()=>e(r,{color:o.gray[900],children:"17-01-2023"})})]})});var d,p,s;l.parameters={...l.parameters,docs:{...(d=l.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Readonly field list">
+      <ReadOnlyFieldList>
+        <LakeLabel label="Card holder" type="view" render={() => <LakeText color={colors.gray[900]}>John Doe</LakeText>} />
+
+        <LakeLabel label="Account" type="view" render={() => <LakeText color={colors.gray[900]}>Compte courant</LakeText>} />
+
+        <LakeLabel label="Account holder" type="view" render={() => <LakeText color={colors.gray[900]}>John Doe</LakeText>} />
+
+        <LakeLabel label="Type" type="view" render={() => <LakeText color={colors.gray[900]}>Single use</LakeText>} />
+
+        <LakeLabel label="Name" type="view" render={() => <LakeText color={colors.gray[900]}>John Doe</LakeText>} />
+
+        <LakeLabel label="Status" type="view" render={() => <LakeText color={colors.gray[900]}>
+              <Tag color="positive">Enabled</Tag>
+            </LakeText>} />
+
+        <LakeLabel label="Contract expiry date" type="view" render={() => <LakeText color={colors.gray[900]}>
+              <Tag color="gray">Unlimited</Tag>
+            </LakeText>} />
+
+        <LakeLabel label="ID" type="view" render={() => <LakeText color={colors.gray[900]}>123456789</LakeText>} actions={<LakeCopyButton valueToCopy="123456789" copyText="Click to copy" copiedText="Copied to clipboard!" />} />
+
+        <LakeLabel label="Created" type="view" render={() => <LakeText color={colors.gray[900]}>16-01-2023</LakeText>} />
+
+        <LakeLabel label="Updated" type="view" render={() => <LakeText color={colors.gray[900]}>17-01-2023</LakeText>} />
+      </ReadOnlyFieldList>
+    </StoryBlock>;
+}`,...(s=(p=l.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const G=["Default"];export{l as Default,G as __namedExportsOrder,q as default};
+//# sourceMappingURL=ReadonlyFieldList.stories-8b73161d.js.map
---
+++ assets/2.6.0/Switch.stories-84776c0c.js
@@ -0,0 +1,24 @@
+import{j as r,a as t,b as a,L as l,c as i}from"./Space-57107f4e.js";import{r as c}from"./index-8db94870.js";import{B as s}from"./index-7a357b54.js";import{S as o}from"./Switch-85943987.js";import{S as g,a as n}from"./_StoriesComponents-f7676638.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-042e6b4d.js";import"./Animated-232f86a6.js";import"./index-58bb7859.js";import"./index-6e5c097a.js";import"./index-b1dbc5b8.js";import"./math-b9695d7b.js";import"./Icon-26b2c7d1.js";import"./Svg-c3c146c4.js";import"./index-ab60b850.js";const j={title:"Forms/Switch",component:o},e=()=>{const[p,h]=c.useState(!0),S=c.useCallback(()=>{h(w=>!w)},[]);return r(g,{title:"Switch",children:[t(n,{title:"Default",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:p,onValueChange:S}),t(a,{width:12}),t(l,{color:i.gray[700],children:"Allow physical cards"})]})}),t(n,{title:"Disabled",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:!0,disabled:!0}),t(a,{width:12}),t(l,{color:i.gray[700],children:"Allow physical cards"})]})})]})};var d,m,u;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  const [value, setValue] = useState<boolean>(true);
+  const toggle = useCallback(() => {
+    setValue(v => !v);
+  }, []);
+  return <StoryBlock title="Switch">
+      <StoryPart title="Default">
+        <Box direction="row" alignItems="center">
+          <Switch value={value} onValueChange={toggle} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Disabled">
+        <Box direction="row" alignItems="center">
+          <Switch value={true} disabled={true} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(u=(m=e.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};const _=["Default"];export{e as Default,_ as __namedExportsOrder,j as default};
+//# sourceMappingURL=Switch.stories-84776c0c.js.map
