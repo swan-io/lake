f54943e43dc68921ac5fd36c4101dbb17c858ecb
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-06-28 14:50:03 +0200

    Add pressable text to tabview

--- packages/lake/src/components/TabView.tsx
+++ packages/lake/src/components/TabView.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "@swan-io/chicane";
 import {
   Fragment,
+  ReactNode,
   Ref,
   forwardRef,
   useCallback,
@@ -11,7 +12,16 @@ import {
   useRef,
   useState,
 } from "react";
-import { LayoutChangeEvent, NativeSyntheticEvent, StyleSheet, Text, View } from "react-native";
+import {
+  LayoutChangeEvent,
+  NativeSyntheticEvent,
+  PressableStateCallbackType,
+  StyleSheet,
+  Text,
+  TextProps,
+  View,
+  WebRole,
+} from "react-native";
 import { P, match } from "ts-pattern";
 import {
   animations,
@@ -32,20 +42,28 @@ import { FocusTrap } from "./FocusTrap";
 import { Icon, IconName } from "./Icon";
 import { LakeText } from "./LakeText";
 import { Link } from "./Link";
-import { PressableText } from "./Pressable";
+import { PressableText, PressableTextProps } from "./Pressable";
 import { Space, SpacingValue } from "./Space";
 import { TransitionView } from "./TransitionView";
 
 const TABS_HEIGHT = 40;
 export const tabsViewHeight = TABS_HEIGHT + 1;
 
-type Tab = {
-  label: string;
-  url: string;
-  icon?: IconName;
-  withSeparator?: boolean;
-  count?: number;
-};
+type Tab =
+  | {
+      label: string;
+      url: string;
+      icon?: IconName;
+      withSeparator?: boolean;
+      count?: number;
+    }
+  | {
+      label: string;
+      icon?: IconName;
+      withSeparator?: boolean;
+      count?: number;
+      id: string;
+    };
 
 const styles = StyleSheet.create({
   container: {
@@ -151,6 +169,50 @@ const styles = StyleSheet.create({
   },
 });
 
+type TabViewLinkProps = {
+  onChange: ((id: string) => void) | undefined;
+  tab: Tab;
+  children: ReactNode;
+  style: (state: PressableStateCallbackType & { active: boolean }) => TextProps["style"];
+  role?: WebRole;
+  activeTabId: string | undefined;
+  onFocus?: PressableTextProps["onFocus"];
+  onBlur?: PressableTextProps["onBlur"];
+  onPress?: () => void;
+  ariaCurrentValue?: "page" | "location";
+};
+
+const TabViewLink = forwardRef<Text, TabViewLinkProps>(
+  ({ children, style, tab, onChange, activeTabId, onBlur, onFocus, onPress }, ref) => {
+    return match(tab)
+      .with({ url: P.string }, ({ url }) => (
+        <Link ref={ref} to={url} style={style} onFocus={onFocus} onBlur={onBlur} onPress={onPress}>
+          {children}
+        </Link>
+      ))
+      .with({ id: P.string }, ({ id }) => {
+        const tabId = getTabId(tab);
+        const isActive = tabId === activeTabId;
+
+        return (
+          <PressableText
+            ref={ref}
+            style={state => style({ ...state, active: isActive })}
+            onPress={() => {
+              onChange?.(id);
+              onPress?.();
+            }}
+            onFocus={onFocus}
+            onBlur={onBlur}
+          >
+            {children}
+          </PressableText>
+        );
+      })
+      .exhaustive();
+  },
+);
+
 const Dropdown = ({
   tabs,
   onHoverStart,
@@ -158,6 +220,8 @@ const Dropdown = ({
   onLinkFocus,
   onLinkBlur,
   onLinkPress,
+  activeTabId,
+  onChange,
 }: {
   tabs: Tab[];
   onHoverStart: () => void;
@@ -165,6 +229,8 @@ const Dropdown = ({
   onLinkFocus: () => void;
   onLinkBlur: () => void;
   onLinkPress: () => void;
+  activeTabId: string | undefined;
+  onChange: ((id: string) => void) | undefined;
 }) => {
   const containerRef = useRef(null);
 
@@ -175,11 +241,15 @@ const Dropdown = ({
 
   return (
     <View role="menu" style={styles.dropdown} ref={containerRef}>
-      {tabs.map(({ url, label }) => {
+      {tabs.map(tab => {
+        const tabId = getTabId(tab);
+
         return (
-          <Link
-            to={url}
-            key={url}
+          <TabViewLink
+            onChange={onChange}
+            key={tabId}
+            activeTabId={activeTabId}
+            tab={tab}
             onFocus={onLinkFocus}
             onBlur={onLinkBlur}
             onPress={onLinkPress}
@@ -191,8 +261,8 @@ const Dropdown = ({
               hovered && styles.dropdownLinkTextHovered,
             ]}
           >
-            {label}
-          </Link>
+            {tab.label}
+          </TabViewLink>
         );
       })}
     </View>
@@ -213,8 +283,10 @@ const DropdownItems = forwardRef<
     tabs: Tab[];
     currentUrl: string;
     otherLabel: string;
+    activeTabId: string | undefined;
+    onChange: ((id: string) => void) | undefined;
   }
->(({ tabs, otherLabel, currentUrl }, ref) => {
+>(({ tabs, otherLabel, currentUrl, activeTabId, onChange }, ref) => {
   const [openingStatus, dispatch] = useReducer(
     (state: DropdownOpeningState, action: DropdownAction): DropdownOpeningState => {
       return match([action, state])
@@ -363,7 +435,9 @@ const DropdownItems = forwardRef<
 
   const mergedRef = useMergeRefs(containerRef, ref);
 
-  const activeTab = tabs.find(({ url }) => url === currentUrl);
+  const activeTab = tabs.find(tab =>
+    isTabActive({ activeTabId, currentLocationURL: currentUrl, tab }),
+  );
 
   return (
     <View style={styles.dropdownHandleContainer} ref={mergedRef}>
@@ -397,6 +471,8 @@ const DropdownItems = forwardRef<
             onEscapeKey={shouldLockFocus ? onEscapeKey : undefined}
           >
             <Dropdown
+              onChange={onChange}
+              activeTabId={activeTabId}
               tabs={tabs}
               onHoverStart={onHoverStart}
               onHoverEnd={onHoverEnd}
@@ -412,6 +488,8 @@ const DropdownItems = forwardRef<
 });
 
 type Props = {
+  activeTabId?: string;
+  onChange?: (id: string) => void;
   tabs: Tab[];
   otherLabel: string;
   hideIfSingleItem?: boolean;
@@ -419,12 +497,31 @@ type Props = {
   sticky?: boolean;
 };
 
+type IsTabActiveParams = {
+  tab: Tab;
+  activeTabId: string | undefined;
+  currentLocationURL: string;
+};
+const isTabActive = ({ tab, activeTabId, currentLocationURL }: IsTabActiveParams) =>
+  match(tab)
+    .with({ url: P.string }, ({ url }) => currentLocationURL.startsWith(url))
+    .with({ id: P.string }, ({ id }) => isNotNullish(activeTabId) && id === activeTabId)
+    .exhaustive();
+
+const getTabId = (tab: Tab) =>
+  match(tab)
+    .with({ url: P.string }, ({ url }) => url)
+    .with({ id: P.string }, ({ id }) => id)
+    .exhaustive();
+
 export const TabView = ({
   tabs,
   otherLabel,
   hideIfSingleItem = true,
   sticky = false,
   padding,
+  activeTabId,
+  onChange,
 }: Props) => {
   const containerRef = useRef<View | null>(null);
   const placeholderRef = useRef<View | null>(null);
@@ -445,8 +542,12 @@ export const TabView = ({
       const values = Object.entries(linksRefs.current);
       const container = containerRef.current;
 
-      for (const [link, node] of values) {
-        if ("/" + path.join("/") === link && isNotNullish(node) && isNotNullish(container)) {
+      for (const [tabId, node] of values) {
+        if (
+          (tabId === activeTabId || "/" + path.join("/") === tabId) &&
+          isNotNullish(node) &&
+          isNotNullish(container)
+        ) {
           node.measureLayout(
             container as unknown as number,
             (left, _, width) => {
@@ -461,7 +562,7 @@ export const TabView = ({
     }
 
     setUnderlinePosition({ left: 0, width: 0 });
-  }, [path, kept, collapsed, padding]);
+  }, [path, kept, collapsed, padding, activeTabId]);
 
   useEffect(() => {
     setHasRendered(width > 0);
@@ -471,7 +572,8 @@ export const TabView = ({
     ({ width }: { container: Element; width: number }) => {
       const items = tabs.map(tab => {
         if (placeholderLinkRef.current) {
-          const ref = placeholderLinkRef.current[tab.url];
+          const tabId = getTabId(tab);
+          const ref = placeholderLinkRef.current[tabId];
 
           if (isNotNullish(ref)) {
             const element = ref as unknown as Element;
@@ -497,7 +599,7 @@ export const TabView = ({
         if (cummulatedWidth < width) {
           kept.push({ ...item.tab, width: item.width });
         } else {
-          if (currentLocationURL.startsWith(item.tab.url)) {
+          if (isTabActive({ activeTabId, currentLocationURL, tab: item.tab })) {
             while (
               kept.length !== 0 &&
               kept.reduce((acc, item) => acc + item.width, 0) + (item.width + 16) >= width
@@ -523,7 +625,9 @@ export const TabView = ({
         otherLabelWidth = otherLabel.getBoundingClientRect().width;
       }
 
-      const activeInKeptIndex = kept.findIndex(item => currentLocationURL.startsWith(item.url));
+      const activeInKeptIndex = kept.findIndex(item =>
+        isTabActive({ activeTabId, currentLocationURL, tab: item }),
+      );
 
       if (activeInKeptIndex !== -1) {
         const activeInKept = kept[activeInKeptIndex];
@@ -556,7 +660,7 @@ export const TabView = ({
         setKeptCollapsed([kept, collapsed]);
       }
     },
-    [tabs, currentLocationURL],
+    [tabs, activeTabId, currentLocationURL],
   );
 
   const onLayout = useCallback(
@@ -593,20 +697,80 @@ export const TabView = ({
       style={[styles.container, sticky && styles.sticky, { paddingHorizontal: padding }]}
     >
       <View style={styles.placeholder} aria-hidden={true} ref={placeholderRef} onLayout={onLayout}>
-        {tabs.map(({ label, url, icon, count }) => (
-          <Fragment key={url}>
-            <Link
+        {tabs.map(tab => {
+          const { label, icon, count } = tab;
+          const tabId = getTabId(tab);
+
+          return (
+            <Fragment key={tabId}>
+              <TabViewLink
+                ref={ref => {
+                  if (placeholderLinkRef.current) {
+                    placeholderLinkRef.current[tabId] = ref;
+                  }
+                }}
+                activeTabId={activeTabId}
+                tab={tab}
+                onChange={onChange}
+                style={({ active, hovered }) => [
+                  styles.link,
+                  active ? styles.activeLink : hovered ? styles.hoveredLink : null,
+                ]}
+              >
+                {isNotNullish(icon) && (
+                  <>
+                    <Icon name={icon} size={16} color="currentColor" />
+                    <Space width={8} />
+                  </>
+                )}
+
+                <Text>{label}</Text>
+
+                {count != null ? (
+                  <>
+                    <Space width={8} />
+                    <Text style={styles.count}>{count}</Text>
+                  </>
+                ) : null}
+              </TabViewLink>
+
+              <Space width={32} />
+            </Fragment>
+          );
+        })}
+
+        <LakeText ref={otherPlaceholderRef} style={styles.link}>
+          <Text>{otherLabel}</Text>
+          <Space width={8} />
+          <Text style={styles.count}>{tabs.length}</Text>
+          <Space width={4} />
+          <Icon name="chevron-down-filled" size={12} />
+        </LakeText>
+      </View>
+
+      {kept.map(tab => {
+        const { label, icon, withSeparator, count } = tab;
+        const tabId = getTabId(tab);
+
+        return (
+          <Fragment key={tabId}>
+            <TabViewLink
               ref={ref => {
-                if (placeholderLinkRef.current) {
-                  placeholderLinkRef.current[url] = ref;
+                if (linksRefs.current) {
+                  linksRefs.current[tabId] = ref;
                 }
               }}
-              to={url}
+              onChange={onChange}
+              activeTabId={activeTabId}
+              tab={tab}
+              role="tab"
               style={({ active, hovered }) => [
                 styles.link,
                 active ? styles.activeLink : hovered ? styles.hoveredLink : null,
               ]}
             >
+              {withSeparator === true && <View style={styles.separator} role="none" />}
+
               {isNotNullish(icon) && (
                 <>
                   <Icon name={icon} size={16} color="currentColor" />
@@ -622,71 +786,28 @@ export const TabView = ({
                   <Text style={styles.count}>{count}</Text>
                 </>
               ) : null}
-            </Link>
+            </TabViewLink>
 
             <Space width={32} />
           </Fragment>
-        ))}
-
-        <LakeText ref={otherPlaceholderRef} style={styles.link}>
-          <Text>{otherLabel}</Text>
-          <Space width={8} />
-          <Text style={styles.count}>{tabs.length}</Text>
-          <Space width={4} />
-          <Icon name="chevron-down-filled" size={12} />
-        </LakeText>
-      </View>
-
-      {kept.map(({ label, url, icon, withSeparator, count }) => (
-        <Fragment key={url}>
-          <Link
-            role="tab"
-            ref={ref => {
-              if (linksRefs.current) {
-                linksRefs.current[url] = ref;
-              }
-            }}
-            to={url}
-            style={({ active, hovered }) => [
-              styles.link,
-              active ? styles.activeLink : hovered ? styles.hoveredLink : null,
-            ]}
-          >
-            {withSeparator === true && <View style={styles.separator} role="none" />}
-
-            {isNotNullish(icon) && (
-              <>
-                <Icon name={icon} size={16} color="currentColor" />
-                <Space width={8} />
-              </>
-            )}
-
-            <Text>{label}</Text>
-
-            {count != null ? (
-              <>
-                <Space width={8} />
-                <Text style={styles.count}>{count}</Text>
-              </>
-            ) : null}
-          </Link>
-
-          <Space width={32} />
-        </Fragment>
-      ))}
+        );
+      })}
 
       {collapsed.length > 0 ? (
         <DropdownItems
           ref={ref => {
             collapsed.forEach(item => {
               if (linksRefs.current) {
-                linksRefs.current[item.url] = ref;
+                const tabId = getTabId(item);
+                linksRefs.current[tabId] = ref;
               }
             });
           }}
+          onChange={onChange}
           tabs={collapsed}
           currentUrl={currentLocationURL}
           otherLabel={otherLabel}
+          activeTabId={activeTabId}
         />
       ) : null}
 
