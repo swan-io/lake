df9de8360cc04dba68fa436c77407b21b4f48d2f
Mathieu Acthernoene <zoontek@gmail.com>
2023-07-05 16:18:42 +0200

    Cleanup

--- packages/shared-business/src/components/RIB.tsx
+++ packages/shared-business/src/components/RIB.tsx
@@ -15,7 +15,7 @@ import {
 } from "@swan-io/lake/src/constants/design";
 import { isNotNullishOrEmpty } from "@swan-io/lake/src/utils/nullish";
 import { CSSProperties } from "react";
-import { StyleProp, StyleSheet, Text, TextStyle, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { match } from "ts-pattern";
 import { t } from "../utils/i18n";
 
@@ -84,34 +84,204 @@ type Address = {
   country: string;
 };
 
+type RibValueProps = {
+  color: "partner" | "gray";
+  label: string;
+  type: "mainInfo" | "smallInfo" | "addressInfo";
+  value: string | string[];
+};
+
+const typeStyles = {
+  addressInfo: styles.addressInfo,
+  mainInfo: styles.mainInfo,
+  smallInfo: styles.smallInfo,
+};
+
+const RibValue = ({ color, label, type, value }: RibValueProps) => (
+  <View>
+    <Text style={[styles.label, color === "partner" && styles.labelPartner]}>{label}</Text>
+    <Space height={4} />
+
+    {(typeof value === "string" ? [value] : value).map((line, index) => (
+      <Text key={index} style={typeStyles[type]}>
+        {line}
+      </Text>
+    ))}
+  </View>
+);
+
 type RIBv1Props = {
   version: "v1";
+  accountHolderAddress: Address;
+  bank: string;
+  bankAddress: Address;
+  bic: string;
+  iban: string;
   partnerColor: string;
   partnerLogoUrl?: string;
-  iban: string;
-  bic: string;
-  bankAddress: Address;
-  accountHolderAddress: Address;
 } & (
   | {
       accountCountry: "FRA";
-      bank: string;
       agency: string;
-      bankNumber: string;
       bankKey: string;
+      bankNumber: string;
     }
   | {
       accountCountry: "DEU";
-      bank: string;
       accountNumber: string;
     }
   | {
       accountCountry: "ESP";
-      bank: string;
       agency: string;
-      nationalCode: string;
       bankNumber: string;
+      nationalCode: string;
     }
+);
+
+const RIBv1 = ({
+  accountHolderAddress,
+  bank,
+  bankAddress,
+  bic,
+  iban,
+  partnerColor,
+  partnerLogoUrl,
+  ...props
+}: RIBv1Props) => (
+  <WithPartnerAccentColor color={partnerColor}>
+    <View style={styles.container}>
+      <View style={styles.part}>
+        <Box direction="row" alignItems="center">
+          {isNotNullishOrEmpty(partnerLogoUrl) ? (
+            <img
+              height={LOGO_MAX_HEIGHT}
+              src={partnerLogoUrl}
+              style={imageStyle}
+              width={LOGO_MAX_WIDTH}
+            />
+          ) : (
+            <SwanLogo style={styles.defaultLogo} />
+          )}
+
+          <Fill minWidth={24} />
+
+          <LakeHeading level={2} variant="h3" color={colors.gray[900]}>
+            {t("rib.bankDetails")}
+          </LakeHeading>
+        </Box>
+
+        <Space height={24} />
+
+        <Box direction="row" alignItems="center">
+          <RibValue type="mainInfo" color="partner" label={t("rib.iban")} value={iban} />
+          <Space width={24} />
+          <RibValue type="mainInfo" color="partner" label={t("rib.bic")} value={bic} />
+        </Box>
+
+        <Space height={8} />
+
+        <Box direction="row" alignItems="center">
+          {match(props)
+            .with({ accountCountry: "FRA" }, ({ agency, bankKey, bankNumber }) => (
+              <>
+                <RibValue type="smallInfo" color="gray" label={t("rib.bank")} value={bank} />
+                <Space width={24} />
+                <RibValue type="smallInfo" color="gray" label={t("rib.agency")} value={agency} />
+                <Space width={24} />
+
+                <RibValue
+                  type="smallInfo"
+                  color="gray"
+                  label={t("rib.number")}
+                  value={bankNumber}
+                />
+
+                <Space width={24} />
+                <RibValue type="smallInfo" color="gray" label={t("rib.key")} value={bankKey} />
+              </>
+            ))
+            .with({ accountCountry: "DEU" }, ({ accountNumber }) => (
+              <>
+                <RibValue type="smallInfo" color="gray" label={t("rib.bank")} value={bank} />
+                <Space width={24} />
+
+                <RibValue
+                  type="smallInfo"
+                  color="gray"
+                  label={t("rib.accountNumber")}
+                  value={accountNumber}
+                />
+              </>
+            ))
+            .with({ accountCountry: "ESP" }, ({ agency, bankNumber, nationalCode }) => (
+              <>
+                <RibValue type="smallInfo" color="gray" label={t("rib.bank")} value={bank} />
+                <Space width={24} />
+                <RibValue type="smallInfo" color="gray" label={t("rib.agency")} value={agency} />
+                <Space width={24} />
+
+                <RibValue
+                  type="smallInfo"
+                  color="gray"
+                  label={t("rib.nationalCode")}
+                  value={nationalCode}
+                />
+
+                <Space width={24} />
+
+                <RibValue
+                  type="smallInfo"
+                  color="gray"
+                  label={t("rib.number")}
+                  value={bankNumber}
+                />
+              </>
+            ))
+            .exhaustive()}
+        </Box>
+      </View>
+
+      <Separator />
+
+      <View style={styles.part}>
+        <Box direction="row" alignItems="center">
+          <RibValue
+            type="addressInfo"
+            color="partner"
+            label={t("rib.address")}
+            value={[
+              bankAddress.name,
+              bankAddress.address,
+              `${bankAddress.zipCode} ${bankAddress.city}`,
+              bankAddress.country,
+            ]}
+          />
+
+          <Space width={24} />
+
+          <RibValue
+            type="addressInfo"
+            color="partner"
+            label={t("rib.accountHolder")}
+            value={[
+              accountHolderAddress.name,
+              accountHolderAddress.address,
+              `${accountHolderAddress.zipCode} ${accountHolderAddress.city}`,
+              accountHolderAddress.country,
+            ]}
+          />
+        </Box>
+
+        <Space height={12} />
+
+        <Box direction="row" alignItems="center">
+          <Text style={styles.partnershipText}>{t("rib.partnership")}</Text>
+          <Space width={4} />
+          <SwanLogo color={colors.gray[900]} style={styles.swanLogo} />
+        </Box>
+      </View>
+    </View>
+  </WithPartnerAccentColor>
 );
 
 export type RIBProps = RIBv1Props;
@@ -120,174 +290,3 @@ export const RIB = (props: RIBProps) =>
   match(props)
     .with({ version: "v1" }, props => <RIBv1 {...props} />)
     .exhaustive();
-
-const RIBv1 = (props: RIBProps) => {
-  const { partnerColor, partnerLogoUrl, iban, bic, bankAddress, accountHolderAddress } = props;
-
-  return (
-    <WithPartnerAccentColor color={partnerColor}>
-      <View style={styles.container}>
-        <View style={styles.part}>
-          <Box direction="row" alignItems="center">
-            {isNotNullishOrEmpty(partnerLogoUrl) ? (
-              <img
-                src={partnerLogoUrl}
-                width={LOGO_MAX_WIDTH}
-                height={LOGO_MAX_HEIGHT}
-                style={imageStyle}
-              />
-            ) : (
-              <SwanLogo style={styles.defaultLogo} />
-            )}
-
-            <Fill minWidth={24} />
-
-            <LakeHeading level={2} variant="h3" color={colors.gray[900]}>
-              {t("rib.bankDetails")}
-            </LakeHeading>
-          </Box>
-
-          <Space height={24} />
-
-          <Box direction="row" alignItems="center">
-            <RibValue type="mainInfo" color="partner" label={t("rib.iban")} value={iban} />
-            <Space width={24} />
-            <RibValue type="mainInfo" color="partner" label={t("rib.bic")} value={bic} />
-          </Box>
-
-          <Space height={8} />
-
-          <Box direction="row" alignItems="center">
-            {match(props)
-              .with({ accountCountry: "FRA" }, ({ bank, agency, bankNumber, bankKey }) => (
-                <>
-                  <RibValue type="smallInfo" color="gray" label={t("rib.bank")} value={bank} />
-                  <Space width={24} />
-                  <RibValue type="smallInfo" color="gray" label={t("rib.agency")} value={agency} />
-                  <Space width={24} />
-
-                  <RibValue
-                    type="smallInfo"
-                    color="gray"
-                    label={t("rib.number")}
-                    value={bankNumber}
-                  />
-
-                  <Space width={24} />
-                  <RibValue type="smallInfo" color="gray" label={t("rib.key")} value={bankKey} />
-                </>
-              ))
-              .with({ accountCountry: "DEU" }, ({ bank, accountNumber }) => (
-                <>
-                  <RibValue type="smallInfo" color="gray" label={t("rib.bank")} value={bank} />
-                  <Space width={24} />
-
-                  <RibValue
-                    type="smallInfo"
-                    color="gray"
-                    label={t("rib.accountNumber")}
-                    value={accountNumber}
-                  />
-                </>
-              ))
-              .with({ accountCountry: "ESP" }, ({ bank, agency, nationalCode, bankNumber }) => (
-                <>
-                  <RibValue type="smallInfo" color="gray" label={t("rib.bank")} value={bank} />
-                  <Space width={24} />
-                  <RibValue type="smallInfo" color="gray" label={t("rib.agency")} value={agency} />
-                  <Space width={24} />
-
-                  <RibValue
-                    type="smallInfo"
-                    color="gray"
-                    label={t("rib.nationalCode")}
-                    value={nationalCode}
-                  />
-
-                  <Space width={24} />
-
-                  <RibValue
-                    type="smallInfo"
-                    color="gray"
-                    label={t("rib.number")}
-                    value={bankNumber}
-                  />
-                </>
-              ))
-              .exhaustive()}
-          </Box>
-        </View>
-
-        <Separator />
-
-        <View style={styles.part}>
-          <Box direction="row" alignItems="center">
-            <RibValue
-              type="addressInfo"
-              color="partner"
-              label={t("rib.address")}
-              value={[
-                bankAddress.name,
-                bankAddress.address,
-                `${bankAddress.zipCode} ${bankAddress.city}`,
-                bankAddress.country,
-              ]}
-            />
-
-            <Space width={24} />
-
-            <RibValue
-              type="addressInfo"
-              color="partner"
-              label={t("rib.accountHolder")}
-              value={[
-                accountHolderAddress.name,
-                accountHolderAddress.address,
-                `${accountHolderAddress.zipCode} ${accountHolderAddress.city}`,
-                accountHolderAddress.country,
-              ]}
-            />
-          </Box>
-
-          <Space height={12} />
-
-          <Box direction="row" alignItems="center">
-            <Text style={styles.partnershipText}>{t("rib.partnership")}</Text>
-            <Space width={4} />
-            <SwanLogo color={colors.gray[900]} style={styles.swanLogo} />
-          </Box>
-        </View>
-      </View>
-    </WithPartnerAccentColor>
-  );
-};
-
-type RibValueProps = {
-  type: "mainInfo" | "smallInfo" | "addressInfo";
-  color: "partner" | "gray";
-  label: string;
-  value: string | string[];
-};
-
-const ribValueStyle: Record<RibValueProps["type"], StyleProp<TextStyle>> = {
-  mainInfo: styles.mainInfo,
-  smallInfo: styles.smallInfo,
-  addressInfo: styles.addressInfo,
-};
-
-const RibValue = ({ type, color, label, value }: RibValueProps) => {
-  const lines = Array.isArray(value) ? value : [value];
-
-  return (
-    <View>
-      <Text style={[styles.label, color === "partner" && styles.labelPartner]}>{label}</Text>
-      <Space height={4} />
-
-      {lines.map((line, index) => (
-        <Text key={index} style={ribValueStyle[type]}>
-          {line}
-        </Text>
-      ))}
-    </View>
-  );
-};
