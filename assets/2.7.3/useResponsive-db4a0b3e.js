d6e13544fe0245a3eebfbe47f759e1602070fa83
ncomont <ncomont@users.noreply.github.com>
2023-07-24 16:03:02 +0000

    deploy: 8966470abee5adc66d03a88a5213eeba1675f035

--- assets/2.7.3/useResponsive-db4a0b3e.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./index-76fb7be0.js";import{D as n,i as a}from"./Space-cf4dfeb4.js";const m=992,p=({width:e},s)=>e>=s,l=(e=m)=>{const s=t.useSyncExternalStore(o=>{const r=n.addEventListener("change",({window:c})=>{a(c)&&o()});return()=>{r.remove()}},()=>p(n.get("window"),e)),i=t.useCallback(o=>s?o.desktop??o.mobile:o.mobile,[s]);return{desktop:s,media:i}};export{l as u};
-//# sourceMappingURL=useResponsive-db4a0b3e.js.map
