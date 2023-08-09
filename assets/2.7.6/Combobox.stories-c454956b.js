37ff8272c99eff798ab899a2fbf11f50b858cdd9
zoontek <zoontek@users.noreply.github.com>
2023-08-09 10:12:23 +0000

    deploy: 57f089b59c6c4926275027d07342876ddc6b10f9

---
+++ assets/2.7.6/Combobox.stories-c454956b.js
@@ -0,0 +1,34 @@
+import{S as P,a as E,j as t,L as y}from"./Space-5a0fc28b.js";import{R as i,M as k}from"./Boxed-885d7854.js";import{r as d}from"./index-b9a8c83f.js";import{B as W}from"./Box-9d75daf6.js";import{L as S}from"./LakeCombobox-4b02380e.js";import{S as B,a as e}from"./_StoriesComponents-940d18f8.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./typography-6ad07e3d.js";import"./colors-298625c7.js";import"./useMergeRefs-4d9c8227.js";import"./array-a4405b3e.js";import"./LakeTextInput-0bc76d97.js";import"./commonStyles-d52fedae.js";import"./Fill-04afbde0.js";import"./index-4069c9dc.js";import"./LoadingView-ec64e3b1.js";import"./Popover-bd08d630.js";import"./useResponsive-2990e972.js";import"./BottomPanel-abea799c.js";import"./useBodyClassName-4a67c631.js";import"./math-0dcad0c5.js";import"./FocusTrap-ddbe2196.js";import"./TransitionView-d18f766a.js";import"./index-080ed22c.js";import"./index-992a5cb1.js";import"./index-68401ae8.js";const L=P.create({footer:{paddingHorizontal:12}}),ot={title:"Forms/Combobox",component:S},g=m=>{const a=fetch(`https://dummyjson.com/products/search?limit=10&q=${m}`).then(n=>n.json()).then(n=>n.products);return k.fromPromise(a)},r=m=>{const[a,n]=d.useState(null),[p,l]=d.useState(()=>i.NotAsked()),[s,x]=d.useState(""),f=a?a.title:s;return d.useEffect(()=>{if(!s){l(i.NotAsked());return}l(i.Loading());const o=g(s);return o.onResolve(C=>{l(i.Done(C))}),()=>{o.cancel()}},[s]),t(S,{value:f,items:p,onValueChange:o=>{n(null),x(o)},onSelectItem:o=>{l(i.NotAsked()),n(o)},keyExtractor:o=>o.id.toString(),renderItem:o=>t(y,{children:o.title}),emptyResultText:"No result",...m})},c=()=>E(B,{title:"Combobox",children:[t(e,{title:"By default",children:t(r,{})}),t(e,{title:"Is disabled",children:t(r,{disabled:!0})}),t(e,{title:"Is readOnly",children:t(r,{readOnly:!0})}),t(e,{title:"With icon",children:t(r,{icon:"add-circle-regular"})}),t(e,{title:"With placeholder",children:t(r,{placeholder:"Enter a product"})}),t(e,{title:"With error",children:t(r,{placeholder:"An error occurred"})}),t(e,{title:"With Footer Component",children:t(r,{ListFooterComponent:t(W,{direction:"row",justifyContent:"end",style:L.footer,children:t(y,{children:"With footer component"})})})})]});var u,b,h;c.parameters={...c.parameters,docs:{...(u=c.parameters)==null?void 0:u.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Combobox">
+      <StoryPart title="By default">
+        <EditableCombobox />
+      </StoryPart>
+
+      <StoryPart title="Is disabled">
+        <EditableCombobox disabled={true} />
+      </StoryPart>
+
+      <StoryPart title="Is readOnly">
+        <EditableCombobox readOnly={true} />
+      </StoryPart>
+
+      <StoryPart title="With icon">
+        <EditableCombobox icon={"add-circle-regular"} />
+      </StoryPart>
+
+      <StoryPart title="With placeholder">
+        <EditableCombobox placeholder={"Enter a product"} />
+      </StoryPart>
+
+      <StoryPart title="With error">
+        <EditableCombobox placeholder={"An error occurred"} />
+      </StoryPart>
+
+      <StoryPart title="With Footer Component">
+        <EditableCombobox ListFooterComponent={<Box direction="row" justifyContent="end" style={styles.footer}>
+              <LakeText>With footer component</LakeText>
+            </Box>} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(h=(b=c.parameters)==null?void 0:b.docs)==null?void 0:h.source}}};const et=["Variations"];export{c as Variations,et as __namedExportsOrder,ot as default};
+//# sourceMappingURL=Combobox.stories-c454956b.js.map
--- assets/2.7.6/useDisclosure-97bee580.js
+++
@@ -1,2 +0,0 @@
-import{r}from"./index-b9a8c83f.js";const p=(s,e)=>{const[u,t]=r.useState(s);return[u,r.useMemo(()=>({open:()=>t(!0),close:()=>{t(!1),e==null||e()},toggle:()=>t(a=>!a)}),[])]};export{p as u};
-//# sourceMappingURL=useDisclosure-97bee580.js.map
