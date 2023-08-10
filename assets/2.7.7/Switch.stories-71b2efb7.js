645ea9a943603f8e14c4cd2e900f390b3d120529
ncomont <ncomont@users.noreply.github.com>
2023-08-10 08:36:42 +0000

    deploy: 030dfdb52843cecaf43f9b4a9feca97d65f2b23d

---
+++ assets/2.7.7/Switch.stories-71b2efb7.js
@@ -0,0 +1,24 @@
+import{a as r,j as t,b as a,L as l,c}from"./Space-f9f487dc.js";import{r as i}from"./index-b9a8c83f.js";import{B as s}from"./Box-46193f0f.js";import{S as o}from"./Switch-d9b8f146.js";import{S as g,a as n}from"./_StoriesComponents-8e4a6521.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./Animated-a995ffbd.js";import"./index-74278a41.js";import"./index-a9513de6.js";import"./index-5a198a62.js";import"./math-1423d981.js";import"./index-55031020.js";const C={title:"Forms/Switch",component:o},e=()=>{const[p,h]=i.useState(!0),S=i.useCallback(()=>{h(w=>!w)},[]);return r(g,{title:"Switch",children:[t(n,{title:"Default",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:p,onValueChange:S}),t(a,{width:12}),t(l,{color:c.gray[700],children:"Allow physical cards"})]})}),t(n,{title:"Disabled",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:!0,disabled:!0}),t(a,{width:12}),t(l,{color:c.gray[700],children:"Allow physical cards"})]})})]})};var d,u,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  const [value, setValue] = useState<boolean>(true);
+  const toggle = useCallback(() => {
+    setValue(v => !v);
+  }, []);
+  return <StoryBlock title="Switch">
+      <StoryPart title="Default">
+        <Box direction="row" alignItems="center">
+          <Switch value={value} onValueChange={toggle} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Disabled">
+        <Box direction="row" alignItems="center">
+          <Switch value={true} disabled={true} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(m=(u=e.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};const I=["Default"];export{e as Default,I as __namedExportsOrder,C as default};
+//# sourceMappingURL=Switch.stories-71b2efb7.js.map
---
+++ assets/2.7.7/WithPartnerAccentColor.stories-433b2c61.js
@@ -0,0 +1,14 @@
+import{S as p,j as o,c as t,V as d}from"./Space-f9f487dc.js";import{B as h}from"./Box-46193f0f.js";import{W as s}from"./WithPartnerAccentColor-905c96ef.js";import{S as m}from"./_StoriesComponents-8e4a6521.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./index-b9a8c83f.js";import"./polished.esm-77d64d09.js";const c=p.create({container:{flexWrap:"wrap"},colorBlock:{width:60,height:40,margin:8,borderRadius:4}}),B={title:"Utilities/WithPartnerAccentColor",component:s},r=({color:i="black"})=>o(m,{title:"WithPartnerAccentColor",description:["This component generate from a color palette with different shades and change the color of all its children using 'colors.partner[shade]'","You can edit the color in 'Controls' panel. (Press A to open it)"],children:o(s,{color:i,children:o(h,{direction:"row",style:c.container,children:Object.keys(t.partner).map(e=>o(d,{style:[c.colorBlock,{backgroundColor:t.partner[e]}]},e))})})});var n,a,l;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`({
+  color = "black"
+}: StoryArgs) => {
+  return <StoryBlock title="WithPartnerAccentColor" description={["This component generate from a color palette with different shades and change the color of all its children using 'colors.partner[shade]'", "You can edit the color in 'Controls' panel. (Press A to open it)"]}>
+      <WithPartnerAccentColor color={color}>
+        <Box direction="row" style={styles.container}>
+          {Object.keys(colors.partner).map(colorShade => <View key={colorShade} style={[styles.colorBlock, {
+          backgroundColor: colors.partner[(colorShade as keyof typeof colors.partner)]
+        }]} />)}
+        </Box>
+      </WithPartnerAccentColor>
+    </StoryBlock>;
+}`,...(l=(a=r.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const P=["Default"];export{r as Default,P as __namedExportsOrder,B as default};
+//# sourceMappingURL=WithPartnerAccentColor.stories-433b2c61.js.map
