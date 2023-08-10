ce54b767dd479e14dbc0bc0a7722aea30a67a2e7
zoontek <zoontek@users.noreply.github.com>
2023-08-10 09:40:07 +0000

    deploy: 6b99502d13e84e8ad0cdaefb512ff77a9fffceab

---
+++ assets/2.7.8/Slider.stories-717ca87e.js
@@ -0,0 +1,26 @@
+import{S as g,a as c,j as t,V as f}from"./Space-d5dd6734.js";import{r as R}from"./index-b9a8c83f.js";import{L as S,s as k}from"./LakeSlider-10c943f3.js";import{S as u,a as e}from"./_StoriesComponents-acae3b9f.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./Box-708697f1.js";import"./LakeLabel-4fef36d0.js";import"./commonStyles-d52fedae.js";import"./v4-a960c1f4.js";import"./LakeTextInput-19f7bc73.js";import"./useMergeRefs-1e14b057.js";import"./Fill-4d6c0bac.js";import"./index-5136ab3b.js";import"./ResponsiveContainer-3b9437f7.js";const o=g.create({desktop:{padding:10,maxWidth:800},mobile:{maxWidth:400}}),v={title:"Forms/Slider",component:S},r=({mobileSize:b=!1,...h})=>{const[y,x]=R.useState(0);return t(f,{style:b?o.mobile:o.desktop,children:t(S,{...h,value:y,onChange:x})})},i=()=>c(u,{title:"Ranges",children:[t(e,{title:"from 0 to 100",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$"})}),t(e,{title:"from -20 to 50",children:t(r,{label:"Range",min:-20,max:50,step:1,unit:"$"})}),t(e,{title:"with steps 10 by 10",children:t(r,{label:"Range",min:0,max:100,step:10,unit:"$"})})]}),n=()=>c(u,{title:"Slider sizes",description:`UI of slider depends on container width. If width is lower than ${k}, there is only a text input without slider`,children:[t(e,{title:"Mobile",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$",mobileSize:!0})}),t(e,{title:"Desktop",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$"})})]});var a,s,l;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Ranges">
+      <StoryPart title="from 0 to 100">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" />
+      </StoryPart>
+
+      <StoryPart title="from -20 to 50">
+        <EditableSlider label="Range" min={-20} max={50} step={1} unit="$" />
+      </StoryPart>
+
+      <StoryPart title="with steps 10 by 10">
+        <EditableSlider label="Range" min={0} max={100} step={10} unit="$" />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(l=(s=i.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var m,d,p;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Slider sizes" description={\`UI of slider depends on container width. If width is lower than \${sliderBreakpoint}, there is only a text input without slider\`}>
+      <StoryPart title="Mobile">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" mobileSize={true} />
+      </StoryPart>
+
+      <StoryPart title="Desktop">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(p=(d=n.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const C=["Ranges","Sizes"];export{i as Ranges,n as Sizes,C as __namedExportsOrder,v as default};
+//# sourceMappingURL=Slider.stories-717ca87e.js.map
