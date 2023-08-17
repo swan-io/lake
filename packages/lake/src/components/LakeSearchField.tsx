1b451c51585697ddd6f9d96ba72ac091f935eebe
bloodyowl <bloodyowl@icloud.com>
2023-08-17 11:15:45 +0200

    Add missing search field style

--- packages/lake/src/components/LakeSearchField.tsx
+++ packages/lake/src/components/LakeSearchField.tsx
@@ -36,6 +36,7 @@ const styles = StyleSheet.create({
   smallButtonContainer: {
     flexDirection: "row",
     justifyContent: "flex-end",
+    flexGrow: 1,
   },
   openSearchFieldContainer: {
     position: "absolute",
