72a69e486c1de942d4fb83fc7904e564f6fda6d4
bloodyowl <bloodyowl@icloud.com>
2023-08-21 16:21:14 +0200

    Add dutch TIN validation

---
+++ packages/shared-business/src/utils/__tests__/validation.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { validateIndividualTaxNumber } from "../validation";
+
+test("validateIndividualTaxNumber", () => {
+  expect(validateIndividualTaxNumber("NLD")("923456788")).toBe(undefined);
+  expect(validateIndividualTaxNumber("NLD")("999999999")).not.toBe(undefined);
+  expect(validateIndividualTaxNumber("NLD")("123456789")).not.toBe(undefined);
+});
