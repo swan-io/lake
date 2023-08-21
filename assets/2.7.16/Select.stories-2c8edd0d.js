617873b58da036795b9d5c843cb1d90cb1317f19
bloodyowl <bloodyowl@users.noreply.github.com>
2023-08-21 14:31:43 +0000

    deploy: 72a69e486c1de942d4fb83fc7904e564f6fda6d4

---
+++ assets/2.7.16/Select.stories-2c8edd0d.js
@@ -0,0 +1,42 @@
+import{S as P,c as p,a as f,j as e,L as k}from"./Space-6f4fd9a8.js";import{r as g}from"./index-b9a8c83f.js";import{L as u}from"./LakeSelect-1f28da6b.js";import{S,a as t}from"./_StoriesComponents-894316fe.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./commonStyles-d52fedae.js";import"./useDisclosure-2bfd5a49.js";import"./array-a4405b3e.js";import"./Box-822dece8.js";import"./Fill-71a3ee24.js";import"./Popover-f5d1f878.js";import"./useResponsive-e98256bb.js";import"./function-2aea345a.js";import"./Boxed-885d7854.js";import"./BottomPanel-d69a8989.js";import"./useBodyClassName-4a67c631.js";import"./math-0b3624af.js";import"./FocusTrap-21b11a5a.js";import"./LoadingView-d08e568b.js";import"./colors-298625c7.js";import"./index-f7a90e68.js";import"./TransitionView-f8f8750c.js";import"./index-5c298ca7.js";import"./index-69920879.js";import"./Pressable-180608bf.js";import"./useMergeRefs-b265f72b.js";import"./index-a637061e.js";const y=P.create({select:{maxWidth:250},footer:{paddingHorizontal:24,paddingVertical:8,backgroundColor:p.gray[0]}}),X={title:"Forms/Select",component:u},E=[{name:"Camille",value:1},{name:"Charlotte",value:2},{name:"Francis",value:3},{name:"Émilie",value:4},{name:"Marie-Laure-Natasha-Valérie-Émilie",value:17},{name:"François",value:5},{name:"Laura",value:6},{name:"Marie-Anne",value:7},{name:"Natasha",value:8},{name:"Nicolas",value:9},{name:"Pierre",value:10},{name:"Thomas",value:11},{name:"Valérie",value:12},{name:"Yann",value:13},{name:"Zoé",value:14},{name:"Jérôme",value:15},{name:"Thomas",value:16}],r=({initialValue:o,...b})=>{const[h,v]=g.useState(o);return e(u,{placeholder:"Select someone...",value:h,onValueChange:v,items:E,style:y.select,...b})},a=()=>f(S,{title:"Select variations",children:[e(t,{title:"Default",children:e(r,{})}),e(t,{title:"Small",children:e(r,{size:"small"})}),e(t,{title:"Borderless",children:e(r,{mode:"borderless",initialValue:1})}),e(t,{title:"With icon",children:e(r,{icon:"building-bank-regular"})}),e(t,{title:"With footer",children:e(r,{PopoverFooter:e(k,{style:y.footer,children:"List of random people"})})}),e(t,{title:"With error",children:e(r,{error:"Required field"})}),e(t,{title:"Disabled",children:e(r,{disabled:!0})}),e(t,{title:"Disabled with value",children:e(r,{disabled:!0,initialValue:2})})]}),l=()=>e(S,{title:"Select colors",description:"Color prop change only icon color",children:Object.keys(p).map(o=>e(t,{title:o,children:e(r,{icon:"building-bank-filled",color:o})},o))});var i,n,s;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Select variations">
+      <StoryPart title="Default">
+        <EditableSelect />
+      </StoryPart>
+
+      <StoryPart title="Small">
+        <EditableSelect size="small" />
+      </StoryPart>
+
+      <StoryPart title="Borderless">
+        <EditableSelect mode="borderless" initialValue={1} />
+      </StoryPart>
+
+      <StoryPart title="With icon">
+        <EditableSelect icon="building-bank-regular" />
+      </StoryPart>
+
+      <StoryPart title="With footer">
+        <EditableSelect PopoverFooter={<LakeText style={styles.footer}>List of random people</LakeText>} />
+      </StoryPart>
+
+      <StoryPart title="With error">
+        <EditableSelect error="Required field" />
+      </StoryPart>
+
+      <StoryPart title="Disabled">
+        <EditableSelect disabled={true} />
+      </StoryPart>
+
+      <StoryPart title="Disabled with value">
+        <EditableSelect disabled={true} initialValue={2} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(s=(n=a.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};var c,m,d;l.parameters={...l.parameters,docs:{...(c=l.parameters)==null?void 0:c.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Select colors" description="Color prop change only icon color">
+      {Object.keys(colors).map(color => <StoryPart key={color} title={color}>
+          <EditableSelect icon="building-bank-filled" color={(color as ColorVariants)} />
+        </StoryPart>)}
+    </StoryBlock>;
+}`,...(d=(m=l.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};const $=["Variations","Colors"];export{l as Colors,a as Variations,$ as __namedExportsOrder,X as default};
+//# sourceMappingURL=Select.stories-2c8edd0d.js.map
