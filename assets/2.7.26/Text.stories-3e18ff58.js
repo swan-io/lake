bf7af87422c3ca087a8fc2c45f46e98c66030dab
ncomont <ncomont@users.noreply.github.com>
2023-09-08 13:40:37 +0000

    deploy: 2cad460c6e5b09f049fecb529fe51c41b6920567

---
+++ assets/2.7.26/Text.stories-3e18ff58.js
@@ -0,0 +1,100 @@
+import{L as i,a as t,j as e,b as r,c as n}from"./Space-762dd32b.js";import{S as m,a as s}from"./_StoriesComponents-fc7e0bd8.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./index-b9a8c83f.js";import"./Box-11f99570.js";const k={title:"Typography/Text",component:i},o=()=>t(m,{title:"Text",children:[t(s,{title:"Variations",children:[e(i,{children:"Default: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(r,{height:16}),e(i,{variant:"light",children:"Light: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{variant:"regular",children:"Regular: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{variant:"medium",children:"Medium: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{variant:"semibold",children:"Semibold: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(r,{height:16}),e(i,{variant:"smallRegular",children:"SmallRegular: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{variant:"smallMedium",children:"SmallMedium: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{variant:"smallSemibold",children:"SmallSemibold: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"})]}),t(s,{title:"Colors",children:[e(i,{color:n.gray.primary,children:"gray: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.live.primary,children:"live: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.sandbox.primary,children:"sandbox: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.positive.primary,children:"positive: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.warning.primary,children:"warning: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.negative.primary,children:"negative: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.current.primary,children:"current: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.partner.primary,children:"partner: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.swan.primary,children:"swan: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.shakespear.primary,children:"shakespear: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.darkPink.primary,children:"darkPink: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.sunglow.primary,children:"sunglow: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"}),e(i,{color:n.mediumSladeBlue.primary,children:"mediumSladeBlue: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec"})]})]});var a,l,c;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Text">
+      <StoryPart title="Variations">
+        <LakeText>
+          Default: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <Space height={16} />
+
+        <LakeText variant="light">
+          Light: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText variant="regular">
+          Regular: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText variant="medium">
+          Medium: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText variant="semibold">
+          Semibold: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <Space height={16} />
+
+        <LakeText variant="smallRegular">
+          SmallRegular: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl
+          nec
+        </LakeText>
+
+        <LakeText variant="smallMedium">
+          SmallMedium: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl
+          nec
+        </LakeText>
+
+        <LakeText variant="smallSemibold">
+          SmallSemibold: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl
+          nec
+        </LakeText>
+      </StoryPart>
+
+      <StoryPart title="Colors">
+        <LakeText color={colors.gray.primary}>
+          gray: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.live.primary}>
+          live: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.sandbox.primary}>
+          sandbox: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.positive.primary}>
+          positive: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.warning.primary}>
+          warning: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.negative.primary}>
+          negative: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.current.primary}>
+          current: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.partner.primary}>
+          partner: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.swan.primary}>
+          swan: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.shakespear.primary}>
+          shakespear: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.darkPink.primary}>
+          darkPink: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.sunglow.primary}>
+          sunglow: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec
+        </LakeText>
+
+        <LakeText color={colors.mediumSladeBlue.primary}>
+          mediumSladeBlue: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod,
+          nisl nec
+        </LakeText>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(c=(l=o.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const x=["Variations"];export{o as Variations,x as __namedExportsOrder,k as default};
+//# sourceMappingURL=Text.stories-3e18ff58.js.map
