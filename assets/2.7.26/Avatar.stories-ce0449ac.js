3148de7efe3ef0b02e97d337c0be4b7ad377daf0
ncomont <ncomont@users.noreply.github.com>
2023-09-08 13:33:33 +0000

    deploy: bf8e0d2203632e2208d9aa646e6666526a82afdb

---
+++ assets/2.7.26/Avatar.stories-ce0449ac.js
@@ -0,0 +1,42 @@
+import{S as u,j as t,V as y,c as o,T as v,I as w,a as l,b as n}from"./Space-bc65526b.js";import{d as x}from"./index-356e4a49.js";import{r as z}from"./index-b9a8c83f.js";import{c as f}from"./commonStyles-d52fedae.js";import{t as c}from"./typography-6ad07e3d.js";import{B as d}from"./Box-517a827c.js";import{S as B,a as A,c as C}from"./_StoriesComponents-e06e319f.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./colors-298625c7.js";const p=u.create({text:{...c.bodyLarge,fontWeight:c.fontWeights.demi,textAlign:"center",userSelect:"none"},container:{borderWidth:1}}),g=i=>{switch((i.charCodeAt(0)+i.charCodeAt(1))%3){case 2:return"darkPink";case 1:return"live";default:return"shakespear"}},e=z.memo(({initials:i="",size:r})=>{const a=g(i);return t(y,{role:"img",style:[f.center,p.container,{backgroundColor:o[a][100],borderColor:o[a][200],height:r,width:r,borderRadius:r/2}],children:i!==""?t(v,{style:[p.text,{color:o[a][500],fontSize:r*.4}],children:i}):t(w,{name:"person-filled",size:r-8,color:o[a][50]})})});try{e.displayName="Avatar",e.__docgenInfo={description:"",displayName:"Avatar",props:{initials:{defaultValue:null,description:"",name:"initials",required:!1,type:{name:"string"}},size:{defaultValue:null,description:"",name:"size",required:!0,type:{name:"number"}}}}}catch{}const E={title:"Informations/Avatar",component:e},s=()=>l(B,{title:"Avatar",children:[t(A,{title:"Sizes",children:l(d,{direction:"row",children:[t(e,{initials:"AA",size:32}),t(n,{width:16}),t(e,{initials:"AA",size:48}),t(n,{width:16}),t(e,{initials:"AA",size:64})]})}),t(A,{title:"Colors",children:l(d,{direction:"row",children:[t(e,{initials:"AA",size:32}),t(n,{width:16}),t(e,{initials:"BA",size:32}),t(n,{width:16}),t(e,{initials:"CA",size:32})]})}),t(C,{title:"Code example",children:x`
+          <Box direction="row">
+            <Avatar initials="AA" size={32} />
+            <Space width={16} />
+            <Avatar initials="BA" size={32} />
+            <Space width={16} />
+            <Avatar initials="CA" size={32} />
+          </Box>`})]});var m,h,S;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Avatar">
+      <StoryPart title="Sizes">
+        <Box direction="row">
+          <Avatar initials="AA" size={32} />
+          <Space width={16} />
+          <Avatar initials="AA" size={48} />
+          <Space width={16} />
+          <Avatar initials="AA" size={64} />
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Colors">
+        <Box direction="row">
+          <Avatar initials="AA" size={32} />
+          <Space width={16} />
+          <Avatar initials="BA" size={32} />
+          <Space width={16} />
+          <Avatar initials="CA" size={32} />
+        </Box>
+      </StoryPart>
+
+      <StoryCodePart title="Code example">
+        {dedent\`
+          <Box direction="row">
+            <Avatar initials="AA" size={32} />
+            <Space width={16} />
+            <Avatar initials="BA" size={32} />
+            <Space width={16} />
+            <Avatar initials="CA" size={32} />
+          </Box>\`}
+      </StoryCodePart>
+    </StoryBlock>;
+}`,...(S=(h=s.parameters)==null?void 0:h.docs)==null?void 0:S.source}}};const N=["Variants"];export{s as Variants,N as __namedExportsOrder,E as default};
+//# sourceMappingURL=Avatar.stories-ce0449ac.js.map
---
+++ assets/2.7.26/SwanLogo.stories-4d68d1f6.js
@@ -0,0 +1,32 @@
+import{a as p,j as o,b as a,c as e}from"./Space-bc65526b.js";import{S as r}from"./SwanLogo-e9d931fa.js";import{S as l}from"./_StoriesComponents-e06e319f.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./index-b9a8c83f.js";import"./colors-298625c7.js";import"./Box-517a827c.js";const d={title:"Informations/SwanLogo",component:r},i=()=>p(l,{title:"SwanLogo",children:[o(r,{}),o(a,{height:16}),o(r,{color:e.gray.primary}),o(a,{height:16}),o(r,{color:e.live.primary}),o(a,{height:16}),o(r,{color:e.sandbox.primary}),o(a,{height:16}),o(r,{color:e.positive.primary}),o(a,{height:16}),o(r,{color:e.warning.primary}),o(a,{height:16}),o(r,{color:e.negative.primary}),o(a,{height:16}),o(r,{color:e.current.primary}),o(a,{height:16}),o(r,{color:e.partner.primary}),o(a,{height:16}),o(r,{color:e.swan.primary}),o(a,{height:16}),o(r,{color:e.shakespear.primary}),o(a,{height:16}),o(r,{color:e.darkPink.primary}),o(a,{height:16}),o(r,{color:e.sunglow.primary}),o(a,{height:16}),o(r,{color:e.mediumSladeBlue.primary})]});var n,c,t;i.parameters={...i.parameters,docs:{...(n=i.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  return <StoryBlock title="SwanLogo">
+      <SwanLogo />
+      <Space height={16} />
+      <SwanLogo color={colors.gray.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.live.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.sandbox.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.positive.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.warning.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.negative.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.current.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.partner.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.swan.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.shakespear.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.darkPink.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.sunglow.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.mediumSladeBlue.primary} />
+    </StoryBlock>;
+}`,...(t=(c=i.parameters)==null?void 0:c.docs)==null?void 0:t.source}}};const u=["Colors"];export{i as Colors,u as __namedExportsOrder,d as default};
+//# sourceMappingURL=SwanLogo.stories-4d68d1f6.js.map
