881b3012861aefd280e5bff790f40500eccd60c7
ncomont <ncomont@users.noreply.github.com>
2023-09-19 12:45:58 +0000

    deploy: 9a20f0de7d317423038f93d9510e83e7cfcf8494

---
+++ assets/2.7.29/Combobox.stories-ec56f3df.js
@@ -0,0 +1,34 @@
+import{S as P,a as E,j as t,L as y}from"./Space-288ad8ab.js";import{R as i,A as k}from"./Boxed-86465006.js";import{r as d}from"./index-1e572255.js";import{B as W}from"./Box-c9a54aff.js";import{L as S}from"./LakeCombobox-6b95edb6.js";import{S as B,a as e}from"./_StoriesComponents-8cbef4fb.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./typography-6ad07e3d.js";import"./colors-298625c7.js";import"./useMergeRefs-466806cb.js";import"./array-a4405b3e.js";import"./LakeTextInput-df8f0b24.js";import"./commonStyles-d52fedae.js";import"./Fill-d6410a1f.js";import"./index-89fe783b.js";import"./LoadingView-597456e8.js";import"./Popover-a9b0710a.js";import"./useResponsive-94793b75.js";import"./function-a81f264e.js";import"./BottomPanel-c2aaf03a.js";import"./useBodyClassName-915b7711.js";import"./math-7e27de18.js";import"./FocusTrap-bba16465.js";import"./TransitionView-1b04c2cb.js";import"./index-d3a60108.js";import"./index-01123cb9.js";import"./index-bdc946b6.js";const L=P.create({footer:{paddingHorizontal:12}}),et={title:"Forms/Combobox",component:S},g=m=>{const a=fetch(`https://dummyjson.com/products/search?limit=10&q=${m}`).then(n=>n.json()).then(n=>n.products);return k.fromPromise(a)},r=m=>{const[a,n]=d.useState(null),[p,l]=d.useState(()=>i.NotAsked()),[s,x]=d.useState(""),f=a?a.title:s;return d.useEffect(()=>{if(!s){l(i.NotAsked());return}l(i.Loading());const o=g(s);return o.onResolve(C=>{l(i.Done(C))}),()=>{o.cancel()}},[s]),t(S,{value:f,items:p,onValueChange:o=>{n(null),x(o)},onSelectItem:o=>{l(i.NotAsked()),n(o)},keyExtractor:o=>o.id.toString(),renderItem:o=>t(y,{children:o.title}),emptyResultText:"No result",...m})},c=()=>E(B,{title:"Combobox",children:[t(e,{title:"By default",children:t(r,{})}),t(e,{title:"Is disabled",children:t(r,{disabled:!0})}),t(e,{title:"Is readOnly",children:t(r,{readOnly:!0})}),t(e,{title:"With icon",children:t(r,{icon:"add-circle-regular"})}),t(e,{title:"With placeholder",children:t(r,{placeholder:"Enter a product"})}),t(e,{title:"With error",children:t(r,{placeholder:"An error occurred"})}),t(e,{title:"With Footer Component",children:t(r,{ListFooterComponent:t(W,{direction:"row",justifyContent:"end",style:L.footer,children:t(y,{children:"With footer component"})})})})]});var u,b,h;c.parameters={...c.parameters,docs:{...(u=c.parameters)==null?void 0:u.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Combobox">
+      <StoryPart title="By default">
+        <EditableCombobox />
+      </StoryPart>
+
+      <StoryPart title="Is disabled">
+        <EditableCombobox disabled={true} />
+      </StoryPart>
+
+      <StoryPart title="Is readOnly">
+        <EditableCombobox readOnly={true} />
+      </StoryPart>
+
+      <StoryPart title="With icon">
+        <EditableCombobox icon={"add-circle-regular"} />
+      </StoryPart>
+
+      <StoryPart title="With placeholder">
+        <EditableCombobox placeholder={"Enter a product"} />
+      </StoryPart>
+
+      <StoryPart title="With error">
+        <EditableCombobox placeholder={"An error occurred"} />
+      </StoryPart>
+
+      <StoryPart title="With Footer Component">
+        <EditableCombobox ListFooterComponent={<Box direction="row" justifyContent="end" style={styles.footer}>
+              <LakeText>With footer component</LakeText>
+            </Box>} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(h=(b=c.parameters)==null?void 0:b.docs)==null?void 0:h.source}}};const rt=["Variations"];export{c as Variations,rt as __namedExportsOrder,et as default};
+//# sourceMappingURL=Combobox.stories-ec56f3df.js.map
--- assets/2.7.29/useResponsive-9458a9f4.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./index-1e572255.js";import{D as n,i as a}from"./Space-2d20569e.js";const m=992,p=({width:e},s)=>e>=s,l=(e=m)=>{const s=t.useSyncExternalStore(o=>{const r=n.addEventListener("change",({window:c})=>{a(c)&&o()});return()=>{r.remove()}},()=>p(n.get("window"),e)),i=t.useCallback(o=>s?o.desktop??o.mobile:o.mobile,[s]);return{desktop:s,media:i}};export{l as u};
-//# sourceMappingURL=useResponsive-9458a9f4.js.map
