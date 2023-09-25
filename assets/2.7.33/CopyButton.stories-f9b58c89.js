dc5b32f928a50fedf8c98dd5daac2ee9faf00e32
zoontek <zoontek@users.noreply.github.com>
2023-09-25 13:03:38 +0000

    deploy: d07129aeaeffb7a7a774fbf729c23b9727ae080b

---
+++ assets/2.7.33/CopyButton.stories-f9b58c89.js
@@ -0,0 +1,34 @@
+import{S,a as c,j as e,b as l}from"./Space-28ce6199.js";import{r as u}from"./index-1e572255.js";import{B as v}from"./commonStyles-7d9713c2.js";import{L as r,c as x}from"./LakeCopyButton-199ff5d1.js";import{L as a}from"./LakeLabel-c5b76a15.js";import{L}from"./LakeTextInput-dcfc48e3.js";import{S as b,a as y}from"./_StoriesComponents-230f7087.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./Pressable-f0a2867d.js";import"./useMergeRefs-6c78374f.js";import"./index-ca925b5c.js";import"./v4-4a60fe23.js";import"./Fill-d666892b.js";import"./index-dd888261.js";const h=S.create({inputPart:{maxWidth:400}}),H={title:"Interactivity/CopyButton",component:r},d=()=>{const o=new Date,i=o.getHours(),t=o.getMinutes(),s=o.getSeconds();return`${i}:${t}:${s}`},n=()=>{const[o,i]=u.useState(""),[t,s]=u.useState(()=>d());return u.useEffect(()=>{const p=setInterval(()=>{s(d())},1e3);return()=>clearInterval(p)},[]),c(b,{title:"CopyButton",children:[e(y,{title:"Sizes",children:c(v,{direction:"row",alignItems:"start",children:[e(a,{label:`Default size (${x})`,render:()=>e(r,{copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 16",render:()=>e(r,{size:16,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 21",render:()=>e(r,{size:21,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 26",render:()=>e(r,{size:26,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})})]})}),e(y,{title:"You can test if value is well copied by paste it in this input",style:h.inputPart,children:e(a,{label:"Paste here",render:()=>e(L,{value:o,onChange:p=>i(p.currentTarget.value)})})})]})};var C,T,m;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`() => {
+  const [pastedValue, setPastedValue] = useState("");
+  const [valueToCopy, setValueToCopy] = useState(() => getTime());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValueToCopy(getTime());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return <StoryBlock title="CopyButton">
+      <StoryPart title="Sizes">
+        <Box direction="row" alignItems="start">
+          <LakeLabel label={\`Default size (\${copyButtondefaultSize})\`} render={() => <LakeCopyButton copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 16" render={() => <LakeCopyButton size={16} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 21" render={() => <LakeCopyButton size={21} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 26" render={() => <LakeCopyButton size={26} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="You can test if value is well copied by paste it in this input" style={styles.inputPart}>
+        <LakeLabel label="Paste here" render={() => <LakeTextInput value={pastedValue} onChange={event => setPastedValue(event.currentTarget.value)} />} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(m=(T=n.parameters)==null?void 0:T.docs)==null?void 0:m.source}}};const M=["Variations"];export{n as Variations,M as __namedExportsOrder,H as default};
+//# sourceMappingURL=CopyButton.stories-f9b58c89.js.map
---
+++ assets/2.7.33/SwanLogo.stories-e4f4785d.js
@@ -0,0 +1,32 @@
+import{a as p,j as o,b as a,c as e}from"./Space-28ce6199.js";import{S as r}from"./SwanLogo-493ae883.js";import{S as l}from"./_StoriesComponents-230f7087.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./index-1e572255.js";import"./colors-298625c7.js";import"./commonStyles-7d9713c2.js";const d={title:"Informations/SwanLogo",component:r},i=()=>p(l,{title:"SwanLogo",children:[o(r,{}),o(a,{height:16}),o(r,{color:e.gray.primary}),o(a,{height:16}),o(r,{color:e.live.primary}),o(a,{height:16}),o(r,{color:e.sandbox.primary}),o(a,{height:16}),o(r,{color:e.positive.primary}),o(a,{height:16}),o(r,{color:e.warning.primary}),o(a,{height:16}),o(r,{color:e.negative.primary}),o(a,{height:16}),o(r,{color:e.current.primary}),o(a,{height:16}),o(r,{color:e.partner.primary}),o(a,{height:16}),o(r,{color:e.swan.primary}),o(a,{height:16}),o(r,{color:e.shakespear.primary}),o(a,{height:16}),o(r,{color:e.darkPink.primary}),o(a,{height:16}),o(r,{color:e.sunglow.primary}),o(a,{height:16}),o(r,{color:e.mediumSladeBlue.primary})]});var n,c,t;i.parameters={...i.parameters,docs:{...(n=i.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  return <StoryBlock title="SwanLogo">
+      <SwanLogo />
+      <Space height={16} />
+      <SwanLogo color={colors.gray.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.live.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.sandbox.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.positive.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.warning.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.negative.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.current.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.partner.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.swan.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.shakespear.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.darkPink.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.sunglow.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.mediumSladeBlue.primary} />
+    </StoryBlock>;
+}`,...(t=(c=i.parameters)==null?void 0:c.docs)==null?void 0:t.source}}};const u=["Colors"];export{i as Colors,u as __namedExportsOrder,d as default};
+//# sourceMappingURL=SwanLogo.stories-e4f4785d.js.map
