32a8678b25fae42ad525551311e547e7295ee295
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-10-24 13:56:25 +0200

    Update logic: buttons below each other in mobile view

--- packages/lake/src/components/SegmentedControl.tsx
+++ packages/lake/src/components/SegmentedControl.tsx
@@ -1,25 +1,54 @@
-import { ReactNode, useRef, useState } from "react";
+import { ReactNode } from "react";
 import { StyleSheet, View } from "react-native";
-import { commonStyles } from "../constants/commonStyles";
-import { backgroundColor, colors, radii, spacings } from "../constants/design";
+import { backgroundColor, colors, radii, spacings, texts } from "../constants/design";
 import { isNotNullish } from "../utils/nullish";
 import { Box } from "./Box";
-import { Icon } from "./Icon";
 import { LakeText } from "./LakeText";
 import { Pressable } from "./Pressable";
 import { Space } from "./Space";
 
-const HEIGHT = 64;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.gray[50],
+    borderRadius: radii[8],
+    padding: spacings[4],
+  },
+  selectedItem: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    flexGrow: 1,
+    flexShrink: 1,
+    transitionProperty: "transform",
+    transitionDuration: "250ms",
+    transitionTimingFunction: "ease",
+    padding: spacings[4],
+    borderRadius: radii[4],
+    backgroundColor: backgroundColor.accented,
+  },
+  item: {
+    flexBasis: "0%",
+    flexGrow: 1,
+    flexShrink: 1,
+    alignItems: "center",
+    padding: spacings[12],
+    flexDirection: "row",
+  },
+  itemMobile: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  textItem: {
+    userSelect: "none",
+    lineHeight: texts.regular.fontSize,
+  },
+});
 
 export type Item<T extends string> = {
   name: string;
   id: T;
   icon?: ReactNode;
 };
-
-const Item = () => {};
 
 type Props<T extends string> = {
   mode?: "desktop" | "mobile";
@@ -34,171 +63,57 @@ export const SegmentedControl = <T extends string>({
   items,
   onValueChange,
 }: Props<T>) => {
-  const indicatorWidth = useRef(0);
-  const itemWidths = useRef<number[]>([]);
-  const [itemsToDisplay, setItemsToDisplay] = useState(items);
   const selectedItemIndex = items.findIndex(item => item.id === selected);
 
-  const maybeHideItems = () => {
-    const maxWidth = itemsToDisplay.length * indicatorWidth.current;
-    const slots = Math.floor(maxWidth / Math.max(...itemWidths.current));
-    if (slots !== itemsToDisplay.length) {
-      setItemsToDisplay(items.slice(0, Math.max(1, slots)));
-    }
-  };
-
   return (
-    <Box
-      direction="row"
-      style={{
-        backgroundColor: colors.gray[50],
-        height: HEIGHT,
-        borderRadius: radii[8],
-      }}
-    >
-      <Box
-        role="none"
-        aria-hidden={true}
-        direction="row"
-        style={{
-          position: "absolute",
-          visibility: "hidden",
-        }}
-      >
-        {itemsToDisplay.map((item, index) => {
-          return (
-            <Box
-              key={`${item.id}-hidden`}
-              direction="row"
-              alignItems="center"
-              onLayout={({ nativeEvent: { layout } }) => {
-                itemWidths.current[index] = layout.width;
-              }}
-              style={{
-                padding: spacings[12],
-                backgroundColor: "green",
-              }}
-            >
-              {isNotNullish(item.icon) && (
-                <>
-                  {item.icon}
-
-                  <Space width={12} />
-                </>
-              )}
-
-              <LakeText
-                color={colors.gray[900]}
-                numberOfLines={1}
-                style={{
-                  userSelect: "none",
-                }}
-              >
-                {item.name}
-              </LakeText>
-            </Box>
-          );
-        })}
-      </Box>
-
-      <View
-        style={{
-          flexGrow: 1,
-          flexShrink: 1,
-          padding: spacings[4],
-        }}
-      >
-        <Box direction="row" style={commonStyles.fill}>
-          <View
-            role="none"
-            style={{
-              position: "absolute",
-              left: 0,
-              top: 0,
+    <Box style={styles.container}>
+      <Box direction={mode === "mobile" ? "column" : "row"}>
+        <View
+          role="none"
+          style={[
+            styles.selectedItem,
+            // eslint-disable-next-line react-native/no-inline-styles
+            mode === "mobile" && {
+              right: 0,
+              height: `${(1 / items.length) * 100}%`,
+              transform: `translateY(${selectedItemIndex * 100}%)`,
+            },
+            // eslint-disable-next-line react-native/no-inline-styles
+            mode === "desktop" && {
               bottom: 0,
-              flexGrow: 1,
-              flexShrink: 1,
-              width: `${(1 / itemsToDisplay.length) * 100}%`,
-              transitionProperty: "transform",
-              transitionDuration: "250ms",
-              transitionTimingFunction: "ease",
+              width: `${(1 / items.length) * 100}%`,
               transform: `translateX(${selectedItemIndex * 100}%)`,
-            }}
-            onLayout={({ nativeEvent: { layout } }) => {
-              indicatorWidth.current = layout.width;
-              maybeHideItems();
+            },
+          ]}
+        />
+
+        {items.map(item => (
+          <Pressable
+            key={item.id}
+            style={[styles.item, mode === "desktop" && styles.itemMobile]}
+            onPress={() => {
+              onValueChange(item.id);
             }}
           >
-            <View
-              style={{
-                flexGrow: 1,
-                flexShrink: 1,
-                borderRadius: radii[4],
-                backgroundColor: backgroundColor.accented,
-              }}
-            />
-          </View>
+            {isNotNullish(item.icon) && (
+              <>
+                {item.icon}
 
-          {itemsToDisplay.map((item, i) => (
-            <Pressable
-              key={item.id}
-              style={{
-                flexBasis: "0%",
-                flexGrow: 1,
-                flexShrink: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "row",
-              }}
-              onPress={() => {
-                onValueChange(item.id);
-              }}
+                <Space height={8} width={12} />
+              </>
+            )}
+
+            <LakeText
+              color={colors.gray[900]}
+              numberOfLines={1}
+              variant="regular"
+              style={styles.textItem}
             >
-              <Box
-                direction="row"
-                alignItems="center"
-                onLayout={({ nativeEvent: { layout } }) => {
-                  // itemsWidths.current[index] = layout.width;
-                  // maybeUpdateItems();
-                }}
-                style={{
-                  padding: spacings[12],
-                  // backgroundColor: "hotpink",
-                }}
-              >
-                {isNotNullish(item.icon) && (
-                  <>
-                    {item.icon}
-
-                    <Space width={12} />
-                  </>
-                )}
-
-                <LakeText
-                  color={colors.gray[900]}
-                  numberOfLines={1}
-                  style={{
-                    userSelect: "none",
-                  }}
-                >
-                  {item.name}
-                </LakeText>
-              </Box>
-            </Pressable>
-          ))}
-        </Box>
-      </View>
-
-      <Pressable
-        role="button"
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          width: HEIGHT,
-        }}
-      >
-        <Icon size={24} name="more-horizontal-filled" color={colors.gray[500]} />
-      </Pressable>
+              {item.name}
+            </LakeText>
+          </Pressable>
+        ))}
+      </Box>
     </Box>
   );
 };
