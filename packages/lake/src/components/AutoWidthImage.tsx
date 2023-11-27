556808530de697cda88ceca816efa76033ad9c67
Mathieu Acthernoene <zoontek@gmail.com>
2023-11-27 17:57:58 +0100

    Add a onSizeComputed prop on AutoWidthImage

--- packages/lake/src/components/AutoWidthImage.tsx
+++ packages/lake/src/components/AutoWidthImage.tsx
@@ -3,19 +3,30 @@ import { Image, ImageProps } from "react-native";
 import { Except } from "type-fest";
 import { isNotNullish } from "../utils/nullish";
 
+type Size = {
+  height: number;
+  width: number;
+};
+
 type Props = Except<ImageProps, "source"> & {
   ariaLabel?: string;
-  maxWidth?: number;
   height: number;
+  maxWidth?: number;
   sourceUri: string;
+  onSizeComputed?: (size: Size) => void;
+};
+
+type State = {
+  fetched: boolean;
+  size: Size;
 };
 
 export const AutoWidthImage = memo(
   forwardRef<Image, Props>(
-    ({ ariaLabel, maxWidth, height, sourceUri, style, ...props }, forwardedRef) => {
-      const [size, setSize] = useState<{ height: number; width: number }>({
-        height,
-        width: 0,
+    ({ ariaLabel, maxWidth, height, sourceUri, onSizeComputed, style, ...props }, forwardedRef) => {
+      const [state, setState] = useState<State>({
+        fetched: false,
+        size: { height, width: 0 },
       });
 
       useEffect(() => {
@@ -23,21 +34,27 @@ export const AutoWidthImage = memo(
           const ratio = fetchedWidth / fetchedHeight;
           const width = ratio * height;
 
-          if (isNotNullish(maxWidth) && maxWidth < width) {
-            const height = maxWidth / ratio;
-            setSize({ height, width: maxWidth });
-          } else {
-            setSize({ height, width });
-          }
+          const size =
+            isNotNullish(maxWidth) && maxWidth < width
+              ? { height: maxWidth / ratio, width: maxWidth }
+              : { height, width };
+
+          setState({ fetched: true, size });
         });
       }, [maxWidth, height, sourceUri]);
+
+      useEffect(() => {
+        if (state.fetched) {
+          onSizeComputed?.(state.size);
+        }
+      }, [state]); // eslint-disable-line react-hooks/exhaustive-deps
 
       return (
         <Image
           aria-label={ariaLabel}
           ref={forwardedRef}
           source={{ uri: sourceUri }}
-          style={[style, size]}
+          style={[style, state.size]}
           {...props}
         />
       );
