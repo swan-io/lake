a6ff36d0e6c722bf3c95979c9f1e6017bd96639f
zoontek <zoontek@users.noreply.github.com>
2023-11-28 10:03:11 +0000

    deploy: c0d148949cf547b3597c3d84eb1ca8be92f73b06

---
+++ assets/4.0.1/CopyButton.stories-17aa8c49.js
@@ -0,0 +1,34 @@
+import{S,a as c,j as e,b as l}from"./Space-8ec20221.js";import{r as u}from"./index-f1286426.js";import{B as v}from"./commonStyles-46fa1855.js";import{L as r,c as x}from"./LakeCopyButton-b65484e1.js";import{L as a}from"./LakeLabel-e425aa7c.js";import{L}from"./LakeTextInput-2e62cd80.js";import{S as b,a as y}from"./_StoriesComponents-1ed2bb38.js";import"./extends-98964cd2.js";import"./_commonjsHelpers-de833af9.js";import"./Pressable-dbd81015.js";import"./index-ca925b5c.js";import"./v4-4a60fe23.js";import"./Fill-33307717.js";import"./LakeSelect-382a7f75.js";import"./useDisclosure-a0ce2208.js";import"./a11y-e7c88e69.js";import"./Popover-71e98e36.js";import"./useResponsive-84457823.js";import"./function-a81f264e.js";import"./Boxed-86465006.js";import"./BottomPanel-bcf3c00c.js";import"./useBodyClassName-4b4df53c.js";import"./math-534c7348.js";import"./FocusTrap-ef098164.js";import"./array-4dd4a51c.js";import"./LoadingView-2056983b.js";import"./colors-298625c7.js";import"./index-88f32c52.js";import"./TransitionView-72f82458.js";import"./index-d01a13d5.js";import"./index-ff079f0e.js";import"./index-23a462bd.js";const h=S.create({inputPart:{maxWidth:400}}),te={title:"Interactivity/CopyButton",component:r},d=()=>{const o=new Date,n=o.getHours(),t=o.getMinutes(),p=o.getSeconds();return`${n}:${t}:${p}`},i=()=>{const[o,n]=u.useState(""),[t,p]=u.useState(()=>d());return u.useEffect(()=>{const s=setInterval(()=>{p(d())},1e3);return()=>clearInterval(s)},[]),c(b,{title:"CopyButton",children:[e(y,{title:"Sizes",children:c(v,{direction:"row",alignItems:"start",children:[e(a,{label:`Default size (${x})`,render:()=>e(r,{copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 16",render:()=>e(r,{size:16,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 21",render:()=>e(r,{size:21,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})}),e(l,{width:32}),e(a,{label:"Size 26",render:()=>e(r,{size:26,copyText:"Copy",copiedText:`Copied ${t}`,valueToCopy:t})})]})}),e(y,{title:"You can test if value is well copied by paste it in this input",style:h.inputPart,children:e(a,{label:"Paste here",render:()=>e(L,{value:o,onChange:s=>n(s.currentTarget.value)})})})]})};var m,C,T;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  const [pastedValue, setPastedValue] = useState("");
+  const [valueToCopy, setValueToCopy] = useState(() => getTime());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValueToCopy(getTime());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return <StoryBlock title="CopyButton">
+      <StoryPart title="Sizes">
+        <Box direction="row" alignItems="start">
+          <LakeLabel label={\`Default size (\${copyButtondefaultSize})\`} render={() => <LakeCopyButton copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 16" render={() => <LakeCopyButton size={16} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 21" render={() => <LakeCopyButton size={21} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+
+          <Space width={32} />
+
+          <LakeLabel label="Size 26" render={() => <LakeCopyButton size={26} copyText="Copy" copiedText={\`Copied \${valueToCopy}\`} valueToCopy={valueToCopy} />} />
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="You can test if value is well copied by paste it in this input" style={styles.inputPart}>
+        <LakeLabel label="Paste here" render={() => <LakeTextInput value={pastedValue} onChange={event => setPastedValue(event.currentTarget.value)} />} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(T=(C=i.parameters)==null?void 0:C.docs)==null?void 0:T.source}}};const oe=["Variations"];export{i as Variations,oe as __namedExportsOrder,te as default};
+//# sourceMappingURL=CopyButton.stories-17aa8c49.js.map
