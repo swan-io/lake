e4edc395941b456caad63144d8bbb81fe57bba70
zoontek <zoontek@users.noreply.github.com>
2023-12-05 10:05:56 +0000

    deploy: a25001b4323d7221854e7eb18cd4e0b4321b4bf5

---
+++ assets/4.0.4/Switch.stories-d0pF6fj0.js
@@ -0,0 +1,24 @@
+import{j as r,a as t,b as a,L as l,c as i}from"./Space-ctE3Ujv8.js";import{r as c}from"./index-4g5l5LRQ.js";import{B as s}from"./commonStyles-k1NzZ0t0.js";import{S as o}from"./Switch-4L6hePnB.js";import{S as g,a as n}from"./_StoriesComponents-Q1lyUHJJ.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./Animated-GsDuS39A.js";import"./index-V6xpc-xJ.js";import"./index-u5y7qc50.js";import"./index-HFdjeFuE.js";import"./math-1_WMFTMs.js";import"./index-i4ZwlJCJ.js";const I={title:"Forms/Switch",component:o},e=()=>{const[p,h]=c.useState(!0),S=c.useCallback(()=>{h(w=>!w)},[]);return r(g,{title:"Switch",children:[t(n,{title:"Default",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:p,onValueChange:S}),t(a,{width:12}),t(l,{color:i.gray[700],children:"Allow physical cards"})]})}),t(n,{title:"Disabled",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:!0,disabled:!0}),t(a,{width:12}),t(l,{color:i.gray[700],children:"Allow physical cards"})]})})]})};var d,u,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  const [value, setValue] = useState<boolean>(true);
+  const toggle = useCallback(() => {
+    setValue(v => !v);
+  }, []);
+  return <StoryBlock title="Switch">
+      <StoryPart title="Default">
+        <Box direction="row" alignItems="center">
+          <Switch value={value} onValueChange={toggle} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Disabled">
+        <Box direction="row" alignItems="center">
+          <Switch value={true} disabled={true} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(m=(u=e.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};const j=["Default"];export{e as Default,j as __namedExportsOrder,I as default};
+//# sourceMappingURL=Switch.stories-d0pF6fj0.js.map
