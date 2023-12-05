832852e228a62acfe1e7bebed6534901b8a940e9
zoontek <zoontek@users.noreply.github.com>
2023-12-05 10:39:49 +0000

    deploy: a4af00fd4ce67da9268a5da5f42b02bda69fc49f

---
+++ assets/4.0.4/TransitionView.stories-RsM4w25W.js
@@ -0,0 +1,21 @@
+import{S as p,c as l,j as h,a as e,V as i,b as w,L as y}from"./Space-vhw16axB.js";import{r as d}from"./index-4g5l5LRQ.js";import{B as u}from"./commonStyles-hHFaj2sN.js";import{L as S}from"./LakeLabel-P3eCsZG1.js";import{S as f}from"./Switch-P5I0kA3U.js";import{T as c}from"./TransitionView-7vfQsz7V.js";import{S as g}from"./_StoriesComponents-v4PKqYFA.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./v4-yQnnJER4.js";import"./Animated-NOGApXGm.js";import"./index-Jc684VSD.js";import"./index-7eTo7gIZ.js";import"./index-ryxw3pZ_.js";import"./math-qTTbeRnF.js";import"./index-dJhxpsRa.js";const t=p.create({switchContainer:{alignSelf:"flex-start"},container:{position:"relative",maxWidth:400},enterAnimation:{animationKeyframes:{"0%":{opacity:0,transform:"translateZ(0px) translateX(200px)"}},animationDuration:"300ms"},leaveAnimation:{animationKeyframes:{"100%":{opacity:0,transform:"translateZ(0px) translateX(200px)"}},animationDuration:"300ms"},block:{position:"absolute",top:0,left:0,width:"100%",height:60,backgroundColor:l.negative[400],borderRadius:8}}),M={title:"Animations/TransitionView",component:c},n=()=>{const[o,m]=d.useState(!1);return h(g,{title:"TransitionView",description:["TransitionView is component triggers a transition when an element enters or leaves the DOM.","You can try it by toggling the switch"],children:[e(S,{label:"Switch displayed content",render:()=>e(i,{style:t.switchContainer,children:e(f,{value:o,onValueChange:m})})}),e(w,{height:16}),e(i,{style:t.container,children:e(c,{style:t.container,enter:t.enterAnimation,leave:t.leaveAnimation,children:o?e(u,{justifyContent:"center",alignItems:"center",style:t.block,children:e(y,{color:l.live.contrast,variant:"semibold",children:"Second block"})}):null})})]})};var a,r,s;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  const [showBlock, setShowBlock] = useState(false);
+  return <StoryBlock title="TransitionView" description={["TransitionView is component triggers a transition when an element enters or leaves the DOM.", "You can try it by toggling the switch"]}>
+      <LakeLabel label="Switch displayed content" render={() => <View style={styles.switchContainer}>
+            <Switch value={showBlock} onValueChange={setShowBlock} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={styles.container}>
+        <TransitionView style={styles.container} enter={styles.enterAnimation} leave={styles.leaveAnimation}>
+          {showBlock ? <Box justifyContent="center" alignItems="center" style={styles.block}>
+              <LakeText color={colors.live.contrast} variant="semibold">
+                Second block
+              </LakeText>
+            </Box> : null}
+        </TransitionView>
+      </View>
+    </StoryBlock>;
+}`,...(s=(r=n.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const X=["Default"];export{n as Default,X as __namedExportsOrder,M as default};
+//# sourceMappingURL=TransitionView.stories-RsM4w25W.js.map
--- assets/4.0.4/preview-Hs6NOSCB.js
+++
@@ -1,8 +0,0 @@
-import{_ as t}from"./iframe-5mujcl4N.js";import"../../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-cD3lzD-q.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]));return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-Hs6NOSCB.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/4.0.4/DocsRenderer-NNNQARDV-cD3lzD-q.js","assets/4.0.4/iframe-5mujcl4N.js","assets/4.0.4/index-4g5l5LRQ.js","assets/4.0.4/_commonjsHelpers-4gQjN7DL.js","assets/4.0.4/react-18-ba7OOUbL.js","assets/4.0.4/index-jmm5gWkb.js","assets/4.0.4/index-ogXoivrg.js","assets/4.0.4/polished.esm-SyeXkIL0.js","assets/4.0.4/extends-dGVwEr9R.js","assets/4.0.4/_getPrototype-bjD8Yebc.js","assets/4.0.4/index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
