132eb2fd969e4dceb00ca63c5c3936a21bced86a
zoontek <zoontek@users.noreply.github.com>
2023-12-06 17:46:49 +0000

    deploy: 6dc9ec6825488221a2acdcf41e1070f727add2e7

---
+++ assets/4.0.5/Slider.stories-pKd48i3s.js
@@ -0,0 +1,26 @@
+import{S as g,j as c,a as t,V as f}from"./Space-7lspbpUo.js";import{r as R}from"./index-4g5l5LRQ.js";import{L as S,s as k}from"./LakeSlider-OZLfIDZf.js";import{S as u,a as e}from"./_StoriesComponents-L4mJvqYX.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./commonStyles-piWKWeC_.js";import"./LakeLabel-GVHZH4nq.js";import"./v4-yQnnJER4.js";import"./LakeTextInput-4yND1fE6.js";import"./Pressable-04ofnHT4.js";import"./Fill-AGAWiBb2.js";import"./LakeSelect-Q2SeKIEi.js";import"./useDisclosure-Cnlvt24p.js";import"./a11y-2bSo9Xxj.js";import"./Popover-aKHzLc5O.js";import"./useResponsive-1n1KmGPw.js";import"./function-sfCq9YxC.js";import"./Boxed-cdFaaAUo.js";import"./BottomPanel-mt-cJW5J.js";import"./useBodyClassName-b5H8Q92H.js";import"./math-Yb1W9pf1.js";import"./FocusTrap-ruNqcYyC.js";import"./array-_tkD71ox.js";import"./LoadingView-wKJKg2x4.js";import"./colors-34oHE9gw.js";import"./index-d6rpU6_z.js";import"./TransitionView-165qQn2Z.js";import"./index-76v31l2V.js";import"./index-TKjlCM9E.js";import"./index-BYzh770C.js";import"./ResponsiveContainer-QMgJ-SnX.js";const n=g.create({desktop:{padding:10,maxWidth:800},mobile:{maxWidth:400}}),rt={title:"Forms/Slider",component:S},r=({mobileSize:b=!1,...h})=>{const[y,x]=R.useState(0);return t(f,{style:b?n.mobile:n.desktop,children:t(S,{...h,value:y,onChange:x})})},i=()=>c(u,{title:"Ranges",children:[t(e,{title:"from 0 to 100",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$"})}),t(e,{title:"from -20 to 50",children:t(r,{label:"Range",min:-20,max:50,step:1,unit:"$"})}),t(e,{title:"with steps 10 by 10",children:t(r,{label:"Range",min:0,max:100,step:10,unit:"$"})})]}),o=()=>c(u,{title:"Slider sizes",description:`UI of slider depends on container width. If width is lower than ${k}, there is only a text input without slider`,children:[t(e,{title:"Mobile",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$",mobileSize:!0})}),t(e,{title:"Desktop",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$"})})]});var a,s,l;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Ranges">
+      <StoryPart title="from 0 to 100">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" />
+      </StoryPart>
+
+      <StoryPart title="from -20 to 50">
+        <EditableSlider label="Range" min={-20} max={50} step={1} unit="$" />
+      </StoryPart>
+
+      <StoryPart title="with steps 10 by 10">
+        <EditableSlider label="Range" min={0} max={100} step={10} unit="$" />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(l=(s=i.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var m,p,d;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Slider sizes" description={\`UI of slider depends on container width. If width is lower than \${sliderBreakpoint}, there is only a text input without slider\`}>
+      <StoryPart title="Mobile">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" mobileSize={true} />
+      </StoryPart>
+
+      <StoryPart title="Desktop">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(d=(p=o.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const it=["Ranges","Sizes"];export{i as Ranges,o as Sizes,it as __namedExportsOrder,rt as default};
+//# sourceMappingURL=Slider.stories-pKd48i3s.js.map
--- assets/4.0.5/preview-BB3gIzDW.js
+++
@@ -1,8 +0,0 @@
-import{_ as t}from"./iframe-NjVPc9Gj.js";import"../../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-piO2FPWt.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]));return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-BB3gIzDW.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/4.0.5/DocsRenderer-NNNQARDV-piO2FPWt.js","assets/4.0.5/iframe-NjVPc9Gj.js","assets/4.0.5/index-4g5l5LRQ.js","assets/4.0.5/_commonjsHelpers-4gQjN7DL.js","assets/4.0.5/react-18-ba7OOUbL.js","assets/4.0.5/index-jmm5gWkb.js","assets/4.0.5/index-ogXoivrg.js","assets/4.0.5/polished.esm-SyeXkIL0.js","assets/4.0.5/extends-dGVwEr9R.js","assets/4.0.5/_getPrototype-bjD8Yebc.js","assets/4.0.5/index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
