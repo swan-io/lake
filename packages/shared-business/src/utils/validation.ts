7d6a087917c69d94ef355717e87341fe62c08094
Nicolas Comont <contact@nicolasc.eu>
2023-12-11 10:20:36 +0100

    feat: add mail validation regex

--- packages/shared-business/src/utils/validation.ts
+++ packages/shared-business/src/utils/validation.ts
@@ -4,6 +4,8 @@ import { match } from "ts-pattern";
 import { t } from "./i18n";
 import { AccountCountry } from "./templateTranslations";
 
+const EMAIL_REGEX =
+  /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i;
 const VAT_NUMBER_REGEX =
   /^((AT)?U[0-9]{8}|(BE)?0[0-9]{9}|(BG)?[0-9]{9,10}|(CY)?[0-9]{8}L|(CZ)?[0-9]{8,10}|(DE)?[0-9]{9}|(DK)?[0-9]{8}|(EE)?[0-9]{9}|(EL|GR)?[0-9]{9}|(ES)?[0-9A-Z][0-9]{7}[0-9A-Z]|(FI)?[0-9]{8}|(FR)?[0-9A-Z]{2}[0-9]{9}|(GB)?([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})|(HU)?[0-9]{8}|(IE)?[0-9]S[0-9]{5}L|(IT)?[0-9]{11}|(LT)?([0-9]{9}|[0-9]{12})|(LU)?[0-9]{8}|(LV)?[0-9]{11}|(MT)?[0-9]{8}|(NL)?[0-9]{9}B[0-9]{2}|(PL)?[0-9]{10}|(PT)?[0-9]{9}|(RO)?[0-9]{2,10}|(SE)?[0-9]{12}|(SI)?[0-9]{8}|(SK)?[0-9]{10})$/;
 
@@ -37,6 +39,10 @@ const VAT_NUMBER_REGEX =
 
 export const isValidVatNumber = (maybeVat: string) => {
   return VAT_NUMBER_REGEX.test(maybeVat);
+};
+
+export const isValidEmail = (maybeEmail: string) => {
+  return EMAIL_REGEX.test(maybeEmail);
 };
 
 export const validateNullableRequired: Validator<string | undefined> = value => {
