8bc3269ec569fe991be2bd86caa0713bf9d88731
bloodyowl <bloodyowl@users.noreply.github.com>
2023-12-13 10:04:25 +0000

    deploy: 394cbd01ddd29d89105b3c0b2b3ce84b8b837e05

---
+++ assets/4.1.1/TabView.stories-EZVt8gnP.js
@@ -0,0 +1,27 @@
+import{a as e}from"./Space-BwM1A8gD.js";import{T as l}from"./TabView-ZlShyfHt.js";import{S as i}from"./_StoriesComponents-MIuOnDu8.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-4g5l5LRQ.js";import"./index-jmm5gWkb.js";import"./index-zwxHXgzQ.js";import"./index-uFi_wnQu.js";import"./with-selector-b0n9FJ4x.js";import"./Pressable-7YvvRDTG.js";import"./function-sfCq9YxC.js";import"./Boxed-cdFaaAUo.js";import"./commonStyles-5ZoAAfTY.js";import"./FocusTrap-3Jblvv_o.js";import"./a11y-2bSo9Xxj.js";import"./array-_tkD71ox.js";import"./Link-H2f-xmQQ.js";import"./TransitionView-SSKovmLI.js";const E={title:"Interactivity/TabView",component:l},r=()=>e(i,{title:"TabView",description:"You can reduce window's width to see behavior when there isn't enough space to display all tabs",children:e(l,{tabs:[{label:"SEPA Credit Transfers",url:"/simulator/sct",count:2},{label:"SEPA Direct Debits",url:"/simulator/sdd",icon:"building-regular"},{label:"Cards",url:"/simulator/cards",withSeparator:!0},{label:"Apple Pay/Google Pay",url:"/simulator/apple-pay-google-pay"},{label:"Account holders",url:"/simulator/account-holders"},{label:"Merchant acquiring",url:"/simulator/merchant-acquiring"}],otherLabel:"More"})});var t,a,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  return <StoryBlock title="TabView" description="You can reduce window's width to see behavior when there isn't enough space to display all tabs">
+      <TabView tabs={[{
+      label: "SEPA Credit Transfers",
+      url: "/simulator/sct",
+      count: 2
+    }, {
+      label: "SEPA Direct Debits",
+      url: "/simulator/sdd",
+      icon: "building-regular"
+    }, {
+      label: "Cards",
+      url: "/simulator/cards",
+      withSeparator: true
+    }, {
+      label: "Apple Pay/Google Pay",
+      url: "/simulator/apple-pay-google-pay"
+    }, {
+      label: "Account holders",
+      url: "/simulator/account-holders"
+    }, {
+      label: "Merchant acquiring",
+      url: "/simulator/merchant-acquiring"
+    }]} otherLabel="More" />
+    </StoryBlock>;
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const q=["Default"];export{r as Default,q as __namedExportsOrder,E as default};
+//# sourceMappingURL=TabView.stories-EZVt8gnP.js.map
---
+++ assets/4.1.1/preview-62Q5vhyX.js
@@ -0,0 +1,8 @@
+import{_ as t}from"./iframe-oK96jVo4.js";import"../../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-ET6tXdTs.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]));return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-62Q5vhyX.js.map
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/4.1.1/DocsRenderer-NNNQARDV-ET6tXdTs.js","assets/4.1.1/iframe-oK96jVo4.js","assets/4.1.1/index-4g5l5LRQ.js","assets/4.1.1/_commonjsHelpers-4gQjN7DL.js","assets/4.1.1/react-18-ba7OOUbL.js","assets/4.1.1/index-jmm5gWkb.js","assets/4.1.1/index-ogXoivrg.js","assets/4.1.1/polished.esm-SyeXkIL0.js","assets/4.1.1/extends-dGVwEr9R.js","assets/4.1.1/_getPrototype-bjD8Yebc.js","assets/4.1.1/index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
