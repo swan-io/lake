43195e33abec3b6eaecd4f919e281936b783a26e
zoontek <zoontek@users.noreply.github.com>
2024-01-16 13:47:42 +0000

    deploy: eb256f44ac2453fda4367afedeb3180348e7f2d4

--- assets/4.4.1/preview-_QNU7bQ8.js
+++
@@ -1,8 +0,0 @@
-import{_ as t}from"./iframe-Xc9CmwVP.js";import"../../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-eoCJTXnM.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]));return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/4.4.1/DocsRenderer-NNNQARDV-eoCJTXnM.js","assets/4.4.1/iframe-Xc9CmwVP.js","assets/4.4.1/index-4g5l5LRQ.js","assets/4.4.1/_commonjsHelpers-4gQjN7DL.js","assets/4.4.1/react-18-ba7OOUbL.js","assets/4.4.1/index-jmm5gWkb.js","assets/4.4.1/index-ogXoivrg.js","assets/4.4.1/polished.esm-SyeXkIL0.js","assets/4.4.1/extends-dGVwEr9R.js","assets/4.4.1/_getPrototype-bjD8Yebc.js","assets/4.4.1/index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-//# sourceMappingURL=preview-_QNU7bQ8.js.map
