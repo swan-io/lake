c1d4f443264899ce1d5494e893f4100e22b8a48e
Mathieu Acthernoene <zoontek@gmail.com>
2024-01-16 12:30:16 +0100

    Improve the useResponsive hook

--- packages/lake/src/hooks/useResponsive.ts
+++ packages/lake/src/hooks/useResponsive.ts
@@ -1,27 +1,30 @@
 // Based on https://github.com/necolas/react-native-web/blob/0.17.7/packages/react-native-web/src/exports/useWindowDimensions/index.js
 
 import { useCallback, useSyncExternalStore } from "react";
-import { Dimensions, ScaledSize } from "react-native";
+import { Dimensions } from "react-native";
 import { isNotNullish } from "../utils/nullish";
 
 const BREAKPOINT = 992;
-const isDesktop = ({ width }: ScaledSize, breakpoint: number) => width >= breakpoint;
 
 export const useResponsive = (breakpoint: number | undefined = BREAKPOINT) => {
-  const desktop = useSyncExternalStore(
-    onStoreChange => {
-      const subscription = Dimensions.addEventListener("change", ({ window }) => {
-        if (isNotNullish(window)) {
-          onStoreChange();
-        }
-      });
+  const subscribe = useCallback((onStoreChange: () => void) => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      if (isNotNullish(window)) {
+        onStoreChange();
+      }
+    });
 
-      return () => {
-        subscription.remove();
-      };
-    },
-    () => isDesktop(Dimensions.get("window"), breakpoint),
-  );
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const getSnapshot = useCallback(() => {
+    const { width } = Dimensions.get("window");
+    return width >= breakpoint;
+  }, [breakpoint]);
+
+  const desktop = useSyncExternalStore(subscribe, getSnapshot);
 
   const media = useCallback(
     <T>(values: { mobile: T; desktop?: T }): T =>
