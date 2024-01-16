8fa33e2e80aaff29f534539d65a2ae61736a4975
Mathieu Acthernoene <zoontek@gmail.com>
2024-01-16 12:55:11 +0100

    Update comment

--- packages/lake/src/hooks/useBreakpoint.ts
+++ packages/lake/src/hooks/useBreakpoint.ts
@@ -1,4 +1,4 @@
-// Based on https://github.com/necolas/react-native-web/blob/0.17.7/packages/react-native-web/src/exports/useWindowDimensions/index.js
+// Based on https://github.com/necolas/react-native-web/blob/0.19.10/packages/react-native-web/src/exports/useWindowDimensions/index.js
 
 import { useCallback, useSyncExternalStore } from "react";
 import { Dimensions } from "react-native";
