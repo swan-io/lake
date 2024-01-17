6b03f82b1a5f6537250c95a13f56ead39f4f6bfd
zoontek <zoontek@users.noreply.github.com>
2024-01-17 15:14:28 +0000

    deploy: 75ddb5aa281b1f5c881228b5ad8fbc05b70b14a4

---
+++ assets/4.4.1/ResponsiveContainer.stories-yl6fmyjC.js
@@ -0,0 +1,35 @@
+import{s as y,c as k,j as l,a as e,V as c,S as s,i as u,L as r}from"./Space-hNF9Ny6x.js";import{r as f}from"./index-4g5l5LRQ.js";import{B as o}from"./commonStyles-gOY62Af5.js";import{L as g}from"./LakeLabel-caf4i9Sm.js";import{R as m}from"./ResponsiveContainer-ibcj1W6i.js";import{S as x}from"./Switch-8iJHlP-k.js";import{S as w}from"./_StoriesComponents-pnxyufw6.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./v4-yQnnJER4.js";import"./Animated-eq9J1oIm.js";import"./index-5qHXuZG5.js";import"./index-N4gQQcxK.js";import"./index-VP1RVCSg.js";import"./math-fvXOWgDf.js";import"./index-5rR6xXD5.js";const t=y.create({container:{width:"100%"},containerMobile:{maxWidth:400},switchContainer:{alignSelf:"flex-start"},block:{width:120,height:60,backgroundColor:k.gray[0],borderRadius:8}}),Y={title:"Layout/ResponsiveContainer",component:m},i=()=>{const[a,b]=f.useState(!1);return l(w,{title:"ResponsiveContainer",description:["ResponsiveContainer is a component that allows you to render different content depending on the screen size.",'You can try it by toggling the "Mobile mode" switch'],children:[e(g,{label:"Mobile mode",render:()=>e(c,{style:t.switchContainer,children:e(x,{value:a,onValueChange:b})})}),e(s,{height:16}),e(c,{style:a?t.containerMobile:t.container,children:e(m,{breakpoint:u.tiny,children:({small:n})=>l(o,{direction:n?"column":"row",children:[e(o,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:n?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(o,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:n?"Mobile size":"Desktop size"})}),e(s,{width:16,height:16}),e(o,{justifyContent:"center",alignItems:"center",style:t.block,children:e(r,{children:n?"Mobile size":"Desktop size"})})]})})})]})};var h,p,d;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`() => {
+  const [forceMobileWidth, setForceMobileWidth] = useState(false);
+  return <StoryBlock title="ResponsiveContainer" description={["ResponsiveContainer is a component that allows you to render different content depending on the screen size.", 'You can try it by toggling the "Mobile mode" switch']}>
+      <LakeLabel label="Mobile mode" render={() => <View style={styles.switchContainer}>
+            <Switch value={forceMobileWidth} onValueChange={setForceMobileWidth} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={forceMobileWidth ? styles.containerMobile : styles.container}>
+        <ResponsiveContainer breakpoint={breakpoints.tiny}>
+          {({
+          small
+        }) => <Box direction={small ? "column" : "row"}>
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+            </Box>}
+        </ResponsiveContainer>
+      </View>
+    </StoryBlock>;
+}`,...(d=(p=i.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const O=["Default"];export{i as Default,O as __namedExportsOrder,Y as default};
+//# sourceMappingURL=ResponsiveContainer.stories-yl6fmyjC.js.map
--- assets/4.4.1/useResponsive-h0eqFfzV.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./index-4g5l5LRQ.js";import{D as n,l as a}from"./Space-JJ6M6GgY.js";const m=992,p=({width:e},s)=>e>=s,u=(e=m)=>{const s=t.useSyncExternalStore(o=>{const i=n.addEventListener("change",({window:c})=>{a(c)&&o()});return()=>{i.remove()}},()=>p(n.get("window"),e)),r=t.useCallback(o=>s?o.desktop??o.mobile:o.mobile,[s]);return{desktop:s,media:r}};export{u};
-//# sourceMappingURL=useResponsive-h0eqFfzV.js.map
