9c365e7160359766885a2f3f943cbea03ae27460
zoontek <zoontek@users.noreply.github.com>
2024-01-17 15:07:25 +0000

    deploy: 378c46169a0463bde625644a094047a48d0eee4f

---
+++ assets/4.4.1/Icon.stories-X-qogG6T.js
@@ -0,0 +1,66 @@
+import{s as O,j as a,a as e,I as g,c as y,S as m,L as d,f as k,b}from"./Space-JJ6M6GgY.js";import{r as T}from"./index-4g5l5LRQ.js";import{B as u}from"./BorderedIcon-jITweTcI.js";import{B as o}from"./commonStyles-OnNy6GEI.js";import{L as P}from"./LakeLabel-WUDLqNda.js";import{L as w}from"./LakeTextInput-y4kwUyj4.js";import{S as C,a as p}from"./_StoriesComponents-km_N9ysr.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./v4-yQnnJER4.js";import"./Pressable-DxIBDVke.js";import"./Fill-S4WmrzQK.js";import"./LakeSelect-q1o47QtZ.js";import"./useDisclosure-Cnlvt24p.js";import"./a11y-2bSo9Xxj.js";import"./Popover-9Yn71nID.js";import"./useResponsive-h0eqFfzV.js";import"./function-dZk570d0.js";import"./Boxed-X5xMwKRs.js";import"./BottomPanel-S0V00OSQ.js";import"./useBodyClassName-b5H8Q92H.js";import"./math-djgq5oNU.js";import"./FocusTrap-5CyNT8FS.js";import"./array-_tkD71ox.js";import"./LoadingView-epv6saAA.js";import"./colors-34oHE9gw.js";import"./index-FiiknEBW.js";import"./TransitionView-8gy0MpUH.js";import"./index-CqA_kT0m.js";import"./index-sGirFPfH.js";import"./index-u3DDKVQn.js";const i=O.create({container:{flexWrap:"wrap"},icon:{flexBasis:"0%",flexGrow:1,minWidth:250,paddingVertical:16}}),ce={title:"Informations/Icon",component:u},h=r=>Object.keys(r),c=()=>{const[r,s]=T.useState("");return a(C,{title:"Icon",children:[e(P,{label:"Search",render:()=>e(w,{icon:"search-filled",value:r,onChangeText:s})}),e(p,{title:"Fluent icons",children:e(o,{direction:"row",alignItems:"center",style:i.container,children:h(k).filter(n=>n.includes(r)).map(n=>a(o,{alignItems:"center",style:i.icon,children:[e(g,{name:n,size:30,color:y.gray[800]}),e(m,{height:8}),e(d,{align:"center",numberOfLines:1,children:n})]},n))})}),e(p,{title:"Custom icons",children:e(o,{direction:"row",alignItems:"center",style:i.container,children:h(b).filter(n=>n.includes(r)).map(n=>a(o,{alignItems:"center",style:i.icon,children:[e(g,{name:n,size:30,color:y.gray[800]}),e(m,{height:8}),e(d,{align:"center",numberOfLines:1,children:n})]},n))})})]})},l=({color:r})=>{const[s,n]=T.useState("");return a(C,{title:"BorderedIcon",description:"You can change the color in 'Controls' panel (Press A to open it)",children:[e(P,{label:"Search",render:()=>e(w,{icon:"search-filled",value:s,onChangeText:n})}),e(p,{title:"Fluent icons",children:e(o,{direction:"row",alignItems:"center",style:i.container,children:h(k).filter(t=>t.includes(s)).map(t=>a(o,{alignItems:"center",style:i.icon,children:[e(u,{name:t,color:r,size:40,padding:8}),e(m,{height:8}),e(d,{align:"center",numberOfLines:1,children:t})]},t))})}),e(p,{title:"Custom icons",children:e(o,{direction:"row",alignItems:"center",style:i.container,children:h(b).filter(t=>t.includes(s)).map(t=>a(o,{alignItems:"center",style:i.icon,children:[e(u,{name:t,color:r,size:100,padding:8}),e(m,{height:8}),e(d,{align:"center",numberOfLines:1,children:t})]},t))})})]})};var x,I,f;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`() => {
+  const [search, setSearch] = useState("");
+  return <StoryBlock title="Icon">
+      <LakeLabel label="Search" render={() => <LakeTextInput icon="search-filled" value={search} onChangeText={setSearch} />} />
+
+      <StoryPart title="Fluent icons">
+        <Box direction="row" alignItems="center" style={styles.container}>
+          {getKeys(fluentIcons).filter(name => name.includes(search)).map(name => <Box key={name} alignItems="center" style={styles.icon}>
+                <Icon name={name} size={30} color={colors.gray[800]} />
+                <Space height={8} />
+
+                <LakeText align="center" numberOfLines={1}>
+                  {name}
+                </LakeText>
+              </Box>)}
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Custom icons">
+        <Box direction="row" alignItems="center" style={styles.container}>
+          {getKeys(customIcons).filter(name => name.includes(search)).map(name => <Box key={name} alignItems="center" style={styles.icon}>
+                <Icon name={name} size={30} color={colors.gray[800]} />
+                <Space height={8} />
+
+                <LakeText align="center" numberOfLines={1}>
+                  {name}
+                </LakeText>
+              </Box>)}
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(f=(I=c.parameters)==null?void 0:I.docs)==null?void 0:f.source}}};var S,B,L;l.parameters={...l.parameters,docs:{...(S=l.parameters)==null?void 0:S.docs,source:{originalSource:`({
+  color
+}: BorderedArgs) => {
+  const [search, setSearch] = useState("");
+  return <StoryBlock title="BorderedIcon" description="You can change the color in 'Controls' panel (Press A to open it)">
+      <LakeLabel label="Search" render={() => <LakeTextInput icon="search-filled" value={search} onChangeText={setSearch} />} />
+
+      <StoryPart title="Fluent icons">
+        <Box direction="row" alignItems="center" style={styles.container}>
+          {getKeys(fluentIcons).filter(name => name.includes(search)).map(name => <Box key={name} alignItems="center" style={styles.icon}>
+                <BorderedIcon name={name} color={color} size={40} padding={8} />
+                <Space height={8} />
+
+                <LakeText align="center" numberOfLines={1}>
+                  {name}
+                </LakeText>
+              </Box>)}
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Custom icons">
+        <Box direction="row" alignItems="center" style={styles.container}>
+          {getKeys(customIcons).filter(name => name.includes(search)).map(name => <Box key={name} alignItems="center" style={styles.icon}>
+                <BorderedIcon name={name} color={color} size={100} padding={8} />
+                <Space height={8} />
+
+                <LakeText align="center" numberOfLines={1}>
+                  {name}
+                </LakeText>
+              </Box>)}
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(L=(B=l.parameters)==null?void 0:B.docs)==null?void 0:L.source}}};const le=["Default","Bordered"];export{l as Bordered,c as Default,le as __namedExportsOrder,ce as default};
+//# sourceMappingURL=Icon.stories-X-qogG6T.js.map
--- assets/4.4.1/preview-wEfcrXNn.js
+++
@@ -1,8 +0,0 @@
-import{_ as t}from"./iframe-XznUukUj.js";import"../../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-OiIq831_.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]));return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/4.4.1/DocsRenderer-NNNQARDV-OiIq831_.js","assets/4.4.1/iframe-XznUukUj.js","assets/4.4.1/index-4g5l5LRQ.js","assets/4.4.1/_commonjsHelpers-4gQjN7DL.js","assets/4.4.1/react-18-ba7OOUbL.js","assets/4.4.1/index-jmm5gWkb.js","assets/4.4.1/index-ogXoivrg.js","assets/4.4.1/polished.esm-SyeXkIL0.js","assets/4.4.1/extends-dGVwEr9R.js","assets/4.4.1/_getPrototype-bjD8Yebc.js","assets/4.4.1/index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-//# sourceMappingURL=preview-wEfcrXNn.js.map
