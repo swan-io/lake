f37eae025b3040071239fd8c9ac7cecbe0e4a154
zoontek <zoontek@users.noreply.github.com>
2024-01-18 20:44:16 +0000

    deploy: a934a99cc8436fe4c4a906ca32199b9f05c1f7ee

---
+++ assets/4.5.1/TabView.stories-3aDEe_it.js
@@ -0,0 +1,27 @@
+import{a as e}from"./Space-JGGAzEwT.js";import{T as l}from"./TabView-WFfZ8OQE.js";import{S as i}from"./_StoriesComponents-5J5Fu6L_.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-4g5l5LRQ.js";import"./index-jmm5gWkb.js";import"./index-zwxHXgzQ.js";import"./index-uFi_wnQu.js";import"./with-selector-b0n9FJ4x.js";import"./Pressable-N3IKB27h.js";import"./function-dZk570d0.js";import"./Boxed-X5xMwKRs.js";import"./commonStyles-1HXsRt6a.js";import"./FocusTrap-uXm4CYle.js";import"./a11y-2bSo9Xxj.js";import"./array-_tkD71ox.js";import"./Link-2ZOxjlG6.js";import"./TransitionView-Mku9T4ol.js";const E={title:"Interactivity/TabView",component:l},r=()=>e(i,{title:"TabView",description:"You can reduce window's width to see behavior when there isn't enough space to display all tabs",children:e(l,{tabs:[{label:"SEPA Credit Transfers",url:"/simulator/sct",count:2},{label:"SEPA Direct Debits",url:"/simulator/sdd",icon:"building-regular"},{label:"Cards",url:"/simulator/cards",withSeparator:!0},{label:"Apple Pay/Google Pay",url:"/simulator/apple-pay-google-pay"},{label:"Account holders",url:"/simulator/account-holders"},{label:"Merchant acquiring",url:"/simulator/merchant-acquiring"}],otherLabel:"More"})});var t,a,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  return <StoryBlock title="TabView" description="You can reduce window's width to see behavior when there isn't enough space to display all tabs">
+      <TabView tabs={[{
+      label: "SEPA Credit Transfers",
+      url: "/simulator/sct",
+      count: 2
+    }, {
+      label: "SEPA Direct Debits",
+      url: "/simulator/sdd",
+      icon: "building-regular"
+    }, {
+      label: "Cards",
+      url: "/simulator/cards",
+      withSeparator: true
+    }, {
+      label: "Apple Pay/Google Pay",
+      url: "/simulator/apple-pay-google-pay"
+    }, {
+      label: "Account holders",
+      url: "/simulator/account-holders"
+    }, {
+      label: "Merchant acquiring",
+      url: "/simulator/merchant-acquiring"
+    }]} otherLabel="More" />
+    </StoryBlock>;
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const q=["Default"];export{r as Default,q as __namedExportsOrder,E as default};
+//# sourceMappingURL=TabView.stories-3aDEe_it.js.map
