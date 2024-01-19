cb7307aefbdc44d99f7615ba8404ac0eb626c66b
zoontek <zoontek@users.noreply.github.com>
2024-01-19 12:16:57 +0000

    deploy: fd6a0c6b5e75d3019263dd4cedca14c227ef1317

---
+++ assets/4.5.1/useResponsive-92TvZuqI.js
@@ -0,0 +1,2 @@
+import{r as o}from"./index-4g5l5LRQ.js";import{E as r}from"./Space-UKiLa9-i.js";const i=t=>o.useSyncExternalStore(o.useCallback(e=>r.addEventListener("change",({window:s})=>{s!=null&&e()}).remove,[]),o.useCallback(()=>r.get("window").width>=t,[t])),u=(t=992)=>{const e=i(t),n=o.useCallback(s=>e?s.desktop??s.mobile:s.mobile,[e]);return{desktop:e,media:n}};export{u};
+//# sourceMappingURL=useResponsive-92TvZuqI.js.map
