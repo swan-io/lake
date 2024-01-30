f1e4cd2db274246725eb064f125e446883a44df8
zoontek <zoontek@users.noreply.github.com>
2024-01-30 13:48:41 +0000

    deploy: 26a20f48262146d071abc595a9f9c67f346295e7

---
+++ assets/4.7.0/DatePicker.stories-SJX326Cp.js
@@ -0,0 +1,81 @@
+import{s as A,a as e,j as c,V as i,L as V,S as I}from"./Space-0DxCdC-u.js";import{D as C,i as w,a as T,b as x,c as _,v as N,d as j}from"./DatePicker-iOJIblGl.js";import{r as l}from"./index-4g5l5LRQ.js";import{L as B}from"./LakeButton-r1cwJr7-.js";import{W as y}from"./WithPartnerAccentColor-N1CvJnr6.js";import{S,a as n}from"./_StoriesComponents-O9JMPPz2.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./Boxed-X5xMwKRs.js";import"./BottomPanel-CXci5Bqh.js";import"./commonStyles-pAxPsQfm.js";import"./useBodyClassName-b5H8Q92H.js";import"./math-nfNbDm2Q.js";import"./FocusTrap-Tw0zQcOU.js";import"./a11y-2bSo9Xxj.js";import"./array-_tkD71ox.js";import"./LoadingView-wq0gbzf5.js";import"./index-QroVw1hG.js";import"./TransitionView-V3ZRP1YW.js";import"./index-DuWDHCOT.js";import"./index-0zEMs7G_.js";import"./Fill-tTmDCrbH.js";import"./LakeLabel-8pQKkCRZ.js";import"./v4-yQnnJER4.js";import"./LakeSelect-0TWRKBhE.js";import"./useDisclosure-Cnlvt24p.js";import"./Pressable-GQHczMeE.js";import"./Popover-ARUKTJds.js";import"./useResponsive-8EV6BqVv.js";import"./function-dZk570d0.js";import"./index-_SfXEbEv.js";import"./LakeTextInput-hlcE6u7o.js";import"./rifm-Wpvf-Oho.js";import"./utc-y0zBq2ZQ.js";import"./index-uFi_wnQu.js";import"./rifm.esm-RM0KNYGD.js";import"./i18n-0jI7tR9X.js";import"./LakeModal-VCD96qo2.js";import"./LakeHeading-x0NHO0OG.js";import"./ResponsiveContainer-PqoaviG2.js";import"./polished.esm-KhTUF-P5.js";const o=A.create({container:{maxWidth:430},button:{alignSelf:"flex-start"}}),f=new Date;f.setDate(f.getDate()-15);const k=new Date;k.setDate(k.getDate()+15);const Ve={title:"Forms/DatePicker",component:C},d=({...t})=>{const[a,r]=l.useState();return e(C,{...t,value:a,onChange:r})},G=({...t})=>{const[a,r]=l.useState({start:"",end:""}),s=N(a,t.format);return e(j,{...t,value:a,error:s?void 0:"End date can't be before start date",onChange:r})},u=()=>e(y,{color:"#0F6FDE",children:c(S,{title:"DatePicker",children:[e(n,{title:"Default",children:e(i,{style:o.container,children:e(d,{label:"Select a date",firstWeekDay:"monday",format:"DD/MM/YYYY"})})}),e(n,{title:"Week starting sunday",children:e(i,{style:o.container,children:e(d,{label:"Select a date",firstWeekDay:"sunday",format:"DD/MM/YYYY"})})}),e(n,{title:"Selectable only in the future",children:e(i,{style:o.container,children:e(d,{label:"Select a date",firstWeekDay:"sunday",format:"DD/MM/YYYY",isSelectable:w})})}),e(n,{title:"Selectable only in a range (15 days before or after today)",children:e(i,{style:o.container,children:e(d,{label:"Select a date",firstWeekDay:"sunday",format:"DD/MM/YYYY",isSelectable:T(f,k)})})})]})}),D=()=>{const t=l.useRef(null),[a,r]=l.useState(""),[s,Y]=l.useState(!1);return e(y,{color:"#0F6FDE",children:e(S,{title:"DatePicker Modal",children:c(n,{title:"Default",children:[c(V,{children:["Selected date: ",a]}),e(I,{height:20}),e(B,{ref:t,style:o.button,onPress:()=>Y(!0),children:"Open date picker"}),e(x,{visible:s,value:a,firstWeekDay:"monday",format:"DD/MM/YYYY",label:"Select a date",confirmLabel:"Select",cancelLabel:"Cancel",onChange:r,onDissmiss:()=>Y(!1)})]})})})},m=()=>e(y,{color:"#0F6FDE",children:e(S,{title:"DateRangePicker",children:e(n,{title:"Default",children:e(i,{style:o.container,children:e(G,{startLabel:"Start date",endLabel:"End date",firstWeekDay:"monday",format:"DD/MM/YYYY"})})})})}),p=()=>{const[t,a]=l.useState({start:"",end:""}),[r,s]=l.useState(!1);return e(y,{color:"#0F6FDE",children:e(S,{title:"DatePicker Popover",children:c(n,{title:"Default",children:[c(V,{children:["Selected date: ",t.start," - ",t.end]}),e(I,{height:20}),e(B,{style:o.button,onPress:()=>s(!0),children:"Open date range picker"}),e(_,{visible:r,value:t,firstWeekDay:"monday",format:"DD/MM/YYYY",startLabel:"Start date",endLabel:"End date",confirmLabel:"Select",cancelLabel:"Cancel",onChange:a,onDissmiss:()=>s(!1)})]})})})};var h,P,b;u.parameters={...u.parameters,docs:{...(h=u.parameters)==null?void 0:h.docs,source:{originalSource:`() => {
+  return <WithPartnerAccentColor color="#0F6FDE">
+      <StoryBlock title="DatePicker">
+        <StoryPart title="Default">
+          <View style={styles.container}>
+            <InteractiveDatePicker label="Select a date" firstWeekDay="monday" format="DD/MM/YYYY" />
+          </View>
+        </StoryPart>
+
+        <StoryPart title="Week starting sunday">
+          <View style={styles.container}>
+            <InteractiveDatePicker label="Select a date" firstWeekDay="sunday" format="DD/MM/YYYY" />
+          </View>
+        </StoryPart>
+
+        <StoryPart title="Selectable only in the future">
+          <View style={styles.container}>
+            <InteractiveDatePicker label="Select a date" firstWeekDay="sunday" format="DD/MM/YYYY" isSelectable={isTodayOrFutureDate} />
+          </View>
+        </StoryPart>
+
+        <StoryPart title="Selectable only in a range (15 days before or after today)">
+          <View style={styles.container}>
+            <InteractiveDatePicker label="Select a date" firstWeekDay="sunday" format="DD/MM/YYYY" isSelectable={isDateInRange(FIFTEEN_DAYS_AGO, FIFTEEN_DAYS_LATER)} />
+          </View>
+        </StoryPart>
+      </StoryBlock>
+    </WithPartnerAccentColor>;
+}`,...(b=(P=u.parameters)==null?void 0:P.docs)==null?void 0:b.source}}};var g,M,v;D.parameters={...D.parameters,docs:{...(g=D.parameters)==null?void 0:g.docs,source:{originalSource:`() => {
+  const buttonRef = useRef<View | null>(null);
+  const [value, setValue] = useState("");
+  const [isOpened, setIsOpened] = useState(false);
+  return <WithPartnerAccentColor color="#0F6FDE">
+      <StoryBlock title="DatePicker Modal">
+        <StoryPart title="Default">
+          <LakeText>Selected date: {value}</LakeText>
+          <Space height={20} />
+
+          <LakeButton ref={buttonRef} style={styles.button} onPress={() => setIsOpened(true)}>
+            Open date picker
+          </LakeButton>
+
+          <DatePickerModal visible={isOpened} value={value} firstWeekDay="monday" format="DD/MM/YYYY" label="Select a date" confirmLabel="Select" cancelLabel="Cancel" onChange={setValue} onDissmiss={() => setIsOpened(false)} />
+        </StoryPart>
+      </StoryBlock>
+    </WithPartnerAccentColor>;
+}`,...(v=(M=D.parameters)==null?void 0:M.docs)==null?void 0:v.source}}};var W,L,F;m.parameters={...m.parameters,docs:{...(W=m.parameters)==null?void 0:W.docs,source:{originalSource:`() => {
+  return <WithPartnerAccentColor color="#0F6FDE">
+      <StoryBlock title="DateRangePicker">
+        <StoryPart title="Default">
+          <View style={styles.container}>
+            <InteractiveDateRangePicker startLabel="Start date" endLabel="End date" firstWeekDay="monday" format="DD/MM/YYYY" />
+          </View>
+        </StoryPart>
+      </StoryBlock>
+    </WithPartnerAccentColor>;
+}`,...(F=(L=m.parameters)==null?void 0:L.docs)==null?void 0:F.source}}};var E,O,R;p.parameters={...p.parameters,docs:{...(E=p.parameters)==null?void 0:E.docs,source:{originalSource:`() => {
+  const [value, setValue] = useState({
+    start: "",
+    end: ""
+  });
+  const [isOpened, setIsOpened] = useState(false);
+  return <WithPartnerAccentColor color="#0F6FDE">
+      <StoryBlock title="DatePicker Popover">
+        <StoryPart title="Default">
+          <LakeText>
+            Selected date: {value.start} - {value.end}
+          </LakeText>
+
+          <Space height={20} />
+
+          <LakeButton style={styles.button} onPress={() => setIsOpened(true)}>
+            Open date range picker
+          </LakeButton>
+
+          <DateRangePickerModal visible={isOpened} value={value} firstWeekDay="monday" format="DD/MM/YYYY" startLabel="Start date" endLabel="End date" confirmLabel="Select" cancelLabel="Cancel" onChange={setValue} onDissmiss={() => setIsOpened(false)} />
+        </StoryPart>
+      </StoryBlock>
+    </WithPartnerAccentColor>;
+}`,...(R=(O=p.parameters)==null?void 0:O.docs)==null?void 0:R.source}}};const Ie=["Default","ButtonWithModal","Range","ButtonWithRangePopover"];export{D as ButtonWithModal,p as ButtonWithRangePopover,u as Default,m as Range,Ie as __namedExportsOrder,Ve as default};
+//# sourceMappingURL=DatePicker.stories-SJX326Cp.js.map
