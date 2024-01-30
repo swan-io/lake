ff917a44f54b2f4fccb5ccb41c167c4ec84fd37b
zoontek <zoontek@users.noreply.github.com>
2024-01-30 16:02:02 +0000

    deploy: 2f10ee3f4b65cbb6a442a12c69467db7d93cfb32

---
+++ assets/4.7.0/SwanLogo.stories-cI34rXHD.js
@@ -0,0 +1,32 @@
+import{j as p,a as o,S as a,c as e}from"./Space-fpWcly8i.js";import{S as r}from"./SwanLogo-H0DdcJki.js";import{S as l}from"./_StoriesComponents-50vo5dIe.js";import"./extends-dGVwEr9R.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-4g5l5LRQ.js";import"./index-jmm5gWkb.js";import"./commonStyles-9xZlVg2d.js";const d={title:"Informations/SwanLogo",component:r},i=()=>p(l,{title:"SwanLogo",children:[o(r,{}),o(a,{height:16}),o(r,{color:e.gray.primary}),o(a,{height:16}),o(r,{color:e.live.primary}),o(a,{height:16}),o(r,{color:e.sandbox.primary}),o(a,{height:16}),o(r,{color:e.positive.primary}),o(a,{height:16}),o(r,{color:e.warning.primary}),o(a,{height:16}),o(r,{color:e.negative.primary}),o(a,{height:16}),o(r,{color:e.current.primary}),o(a,{height:16}),o(r,{color:e.partner.primary}),o(a,{height:16}),o(r,{color:e.swan.primary}),o(a,{height:16}),o(r,{color:e.shakespear.primary}),o(a,{height:16}),o(r,{color:e.darkPink.primary}),o(a,{height:16}),o(r,{color:e.sunglow.primary}),o(a,{height:16}),o(r,{color:e.mediumSladeBlue.primary})]});var n,c,t;i.parameters={...i.parameters,docs:{...(n=i.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  return <StoryBlock title="SwanLogo">
+      <SwanLogo />
+      <Space height={16} />
+      <SwanLogo color={colors.gray.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.live.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.sandbox.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.positive.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.warning.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.negative.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.current.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.partner.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.swan.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.shakespear.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.darkPink.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.sunglow.primary} />
+      <Space height={16} />
+      <SwanLogo color={colors.mediumSladeBlue.primary} />
+    </StoryBlock>;
+}`,...(t=(c=i.parameters)==null?void 0:c.docs)==null?void 0:t.source}}};const u=["Colors"];export{i as Colors,u as __namedExportsOrder,d as default};
+//# sourceMappingURL=SwanLogo.stories-cI34rXHD.js.map
