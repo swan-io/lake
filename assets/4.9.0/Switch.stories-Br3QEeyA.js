0e5eae6fa4d0b7b455977711b1e07bbc5f84f076
zoontek <zoontek@users.noreply.github.com>
2024-02-13 15:01:12 +0000

    deploy: 8a1f3558617be28f5e389dc6f05889dfc4b80d11

---
+++ assets/4.9.0/Switch.stories-Br3QEeyA.js
@@ -0,0 +1,24 @@
+import{j as r,a as t,S as a,L as l,c as i}from"./Space-CgCWV-Mq.js";import{r as c}from"./index-CBqU2yxZ.js";import{B as s}from"./commonStyles-btEIBsfg.js";import{S as o}from"./Switch-BvdbpUZ2.js";import{S as g,a as n}from"./_StoriesComponents-B1tuaLOg.js";import"./extends-CCbyfPlC.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./Animated-BiG8z48t.js";import"./index-Bi7lHBnd.js";import"./index-MXBIjStY.js";import"./index-HUYp5Xnp.js";import"./math-BkFYzAYb.js";import"./index-BScOByK1.js";const I={title:"Forms/Switch",component:o},e=()=>{const[p,h]=c.useState(!0),S=c.useCallback(()=>{h(w=>!w)},[]);return r(g,{title:"Switch",children:[t(n,{title:"Default",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:p,onValueChange:S}),t(a,{width:12}),t(l,{color:i.gray[700],children:"Allow physical cards"})]})}),t(n,{title:"Disabled",children:r(s,{direction:"row",alignItems:"center",children:[t(o,{value:!0,disabled:!0}),t(a,{width:12}),t(l,{color:i.gray[700],children:"Allow physical cards"})]})})]})};var d,u,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`() => {
+  const [value, setValue] = useState<boolean>(true);
+  const toggle = useCallback(() => {
+    setValue(v => !v);
+  }, []);
+  return <StoryBlock title="Switch">
+      <StoryPart title="Default">
+        <Box direction="row" alignItems="center">
+          <Switch value={value} onValueChange={toggle} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Disabled">
+        <Box direction="row" alignItems="center">
+          <Switch value={true} disabled={true} />
+          <Space width={12} />
+          <LakeText color={colors.gray[700]}>Allow physical cards</LakeText>
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(m=(u=e.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};const j=["Default"];export{e as Default,j as __namedExportsOrder,I as default};
+//# sourceMappingURL=Switch.stories-Br3QEeyA.js.map
---
+++ assets/4.9.0/TabView.stories-Bzk3ruRE.js
@@ -0,0 +1,27 @@
+import{a as e}from"./Space-CgCWV-Mq.js";import{T as l}from"./TabView-CV4KBwkv.js";import{S as i}from"./_StoriesComponents-B1tuaLOg.js";import"./extends-CCbyfPlC.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-CBqU2yxZ.js";import"./index-BtM5VmRH.js";import"./index-qXo7R5Vv.js";import"./index-CmNHga1A.js";import"./with-selector-D-a6ftZl.js";import"./Pressable-DFt-3toh.js";import"./function-DNthd_7Y.js";import"./Boxed-zu2eTygX.js";import"./commonStyles-btEIBsfg.js";import"./FocusTrap-D_xjippm.js";import"./a11y-DXvpDVrG.js";import"./array-rjEBP2Ry.js";import"./Link-ByGG0EgJ.js";import"./TransitionView-DYUYBhiu.js";const E={title:"Interactivity/TabView",component:l},r=()=>e(i,{title:"TabView",description:"You can reduce window's width to see behavior when there isn't enough space to display all tabs",children:e(l,{tabs:[{label:"SEPA Credit Transfers",url:"/simulator/sct",count:2},{label:"SEPA Direct Debits",url:"/simulator/sdd",icon:"building-regular"},{label:"Cards",url:"/simulator/cards",withSeparator:!0},{label:"Apple Pay/Google Pay",url:"/simulator/apple-pay-google-pay"},{label:"Account holders",url:"/simulator/account-holders"},{label:"Merchant acquiring",url:"/simulator/merchant-acquiring"}],otherLabel:"More"})});var t,a,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  return <StoryBlock title="TabView" description="You can reduce window's width to see behavior when there isn't enough space to display all tabs">
+      <TabView tabs={[{
+      label: "SEPA Credit Transfers",
+      url: "/simulator/sct",
+      count: 2
+    }, {
+      label: "SEPA Direct Debits",
+      url: "/simulator/sdd",
+      icon: "building-regular"
+    }, {
+      label: "Cards",
+      url: "/simulator/cards",
+      withSeparator: true
+    }, {
+      label: "Apple Pay/Google Pay",
+      url: "/simulator/apple-pay-google-pay"
+    }, {
+      label: "Account holders",
+      url: "/simulator/account-holders"
+    }, {
+      label: "Merchant acquiring",
+      url: "/simulator/merchant-acquiring"
+    }]} otherLabel="More" />
+    </StoryBlock>;
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const q=["Default"];export{r as Default,q as __namedExportsOrder,E as default};
+//# sourceMappingURL=TabView.stories-Bzk3ruRE.js.map
