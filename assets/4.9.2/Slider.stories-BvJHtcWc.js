d300b162e795be89c28d39c5324a44b559194adb
bloodyowl <bloodyowl@users.noreply.github.com>
2024-02-26 11:00:26 +0000

    deploy: 079f8bca086abee212a5f01e582f228f126c3675

---
+++ assets/4.9.2/Slider.stories-BvJHtcWc.js
@@ -0,0 +1,26 @@
+import{s as g,j as c,a as t,V as f}from"./Space-C1EmkcD0.js";import{r as R}from"./index-CBqU2yxZ.js";import{L as S,s as k}from"./LakeSlider-Bbha2nRB.js";import{S as u,a as e}from"./_StoriesComponents-Bo_MKYK6.js";import"./extends-CCbyfPlC.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BtM5VmRH.js";import"./commonStyles-CXm1Z20J.js";import"./LakeLabel-BP83E4Yx.js";import"./v4-D8aEg3BZ.js";import"./LakeTextInput-D3bmZZ2n.js";import"./Pressable-C3gAtVtk.js";import"./Fill-DIsAaKNX.js";import"./LakeSelect-CVn_QhMF.js";import"./useDisclosure-CS9t68_Y.js";import"./a11y-DXvpDVrG.js";import"./Popover-D9ZxnFEz.js";import"./useResponsive-CVJYEjW2.js";import"./function-DNthd_7Y.js";import"./Boxed-zu2eTygX.js";import"./BottomPanel-nWg0b-WC.js";import"./useBodyClassName-FAgY5CgW.js";import"./math-CZD-I5pP.js";import"./FocusTrap-CYNp_oyj.js";import"./array-rjEBP2Ry.js";import"./LoadingView-D3u299aR.js";import"./index-BHq18-ig.js";import"./TransitionView-C8Eyh8nE.js";import"./index-wMq3vUMK.js";import"./index-QFdr-F11.js";import"./index-CTeprn-w.js";import"./ResponsiveContainer-CInNOwy6.js";const n=g.create({desktop:{padding:10,maxWidth:800},mobile:{maxWidth:400}}),et={title:"Forms/Slider",component:S},r=({mobileSize:b=!1,...h})=>{const[y,x]=R.useState(0);return t(f,{style:b?n.mobile:n.desktop,children:t(S,{...h,value:y,onChange:x})})},i=()=>c(u,{title:"Ranges",children:[t(e,{title:"from 0 to 100",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$"})}),t(e,{title:"from -20 to 50",children:t(r,{label:"Range",min:-20,max:50,step:1,unit:"$"})}),t(e,{title:"with steps 10 by 10",children:t(r,{label:"Range",min:0,max:100,step:10,unit:"$"})})]}),o=()=>c(u,{title:"Slider sizes",description:`UI of slider depends on container width. If width is lower than ${k}, there is only a text input without slider`,children:[t(e,{title:"Mobile",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$",mobileSize:!0})}),t(e,{title:"Desktop",children:t(r,{label:"Range",min:0,max:100,step:1,unit:"$"})})]});var a,s,l;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Ranges">
+      <StoryPart title="from 0 to 100">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" />
+      </StoryPart>
+
+      <StoryPart title="from -20 to 50">
+        <EditableSlider label="Range" min={-20} max={50} step={1} unit="$" />
+      </StoryPart>
+
+      <StoryPart title="with steps 10 by 10">
+        <EditableSlider label="Range" min={0} max={100} step={10} unit="$" />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(l=(s=i.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};var m,p,d;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Slider sizes" description={\`UI of slider depends on container width. If width is lower than \${sliderBreakpoint}, there is only a text input without slider\`}>
+      <StoryPart title="Mobile">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" mobileSize={true} />
+      </StoryPart>
+
+      <StoryPart title="Desktop">
+        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(d=(p=o.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const rt=["Ranges","Sizes"];export{i as Ranges,o as Sizes,rt as __namedExportsOrder,et as default};
+//# sourceMappingURL=Slider.stories-BvJHtcWc.js.map
---
+++ assets/4.9.2/TabView.stories-BS5AZk-k.js
@@ -0,0 +1,27 @@
+import{a as e}from"./Space-C1EmkcD0.js";import{T as l}from"./TabView-BNYQyKwn.js";import{S as i}from"./_StoriesComponents-Bo_MKYK6.js";import"./extends-CCbyfPlC.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-CBqU2yxZ.js";import"./index-BtM5VmRH.js";import"./index-qXo7R5Vv.js";import"./index-CmNHga1A.js";import"./with-selector-D-a6ftZl.js";import"./Pressable-C3gAtVtk.js";import"./function-DNthd_7Y.js";import"./Boxed-zu2eTygX.js";import"./commonStyles-CXm1Z20J.js";import"./FocusTrap-CYNp_oyj.js";import"./a11y-DXvpDVrG.js";import"./array-rjEBP2Ry.js";import"./Link-DtjinynN.js";import"./TransitionView-C8Eyh8nE.js";const E={title:"Interactivity/TabView",component:l},r=()=>e(i,{title:"TabView",description:"You can reduce window's width to see behavior when there isn't enough space to display all tabs",children:e(l,{tabs:[{label:"SEPA Credit Transfers",url:"/simulator/sct",count:2},{label:"SEPA Direct Debits",url:"/simulator/sdd",icon:"building-regular"},{label:"Cards",url:"/simulator/cards",withSeparator:!0},{label:"Apple Pay/Google Pay",url:"/simulator/apple-pay-google-pay"},{label:"Account holders",url:"/simulator/account-holders"},{label:"Merchant acquiring",url:"/simulator/merchant-acquiring"}],otherLabel:"More"})});var t,a,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  return <StoryBlock title="TabView" description="You can reduce window's width to see behavior when there isn't enough space to display all tabs">
+      <TabView tabs={[{
+      label: "SEPA Credit Transfers",
+      url: "/simulator/sct",
+      count: 2
+    }, {
+      label: "SEPA Direct Debits",
+      url: "/simulator/sdd",
+      icon: "building-regular"
+    }, {
+      label: "Cards",
+      url: "/simulator/cards",
+      withSeparator: true
+    }, {
+      label: "Apple Pay/Google Pay",
+      url: "/simulator/apple-pay-google-pay"
+    }, {
+      label: "Account holders",
+      url: "/simulator/account-holders"
+    }, {
+      label: "Merchant acquiring",
+      url: "/simulator/merchant-acquiring"
+    }]} otherLabel="More" />
+    </StoryBlock>;
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const q=["Default"];export{r as Default,q as __namedExportsOrder,E as default};
+//# sourceMappingURL=TabView.stories-BS5AZk-k.js.map
