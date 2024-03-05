2226ef42be5b65d95ceafa7fccc7dec607c05363
Mathieu Acthernoene <zoontek@gmail.com>
2024-03-05 16:25:16 +0100

    Simplify LakeSlider component (#278)

--- packages/lake/__stories__/Slider.stories.tsx
+++ packages/lake/__stories__/Slider.stories.tsx
@@ -1,17 +1,18 @@
 import { Meta } from "@storybook/react";
-import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { ComponentProps, useState } from "react";
+import { StyleSheet } from "react-native";
 import { Except } from "type-fest";
-import { LakeSlider, SliderProps, sliderBreakpoint } from "../src/components/LakeSlider";
+import { Box } from "../src/components/Box";
+import { LakeSlider } from "../src/components/LakeSlider";
+import { LakeText } from "../src/components/LakeText";
+import { Space } from "../src/components/Space";
+import { commonStyles } from "../src/constants/commonStyles";
 import { StoryBlock, StoryPart } from "./_StoriesComponents";
 
 const styles = StyleSheet.create({
-  desktop: {
-    padding: 10, // adds some padding to avoid crop slider handler caused by storybook canvas
+  slider: {
+    ...commonStyles.fill,
     maxWidth: 800,
-  },
-  mobile: {
-    maxWidth: 400,
   },
 });
 
@@ -20,50 +21,30 @@ export default {
   component: LakeSlider,
 } as Meta<typeof LakeSlider>;
 
-const EditableSlider = ({
-  mobileSize = false,
-  ...props
-}: Except<SliderProps, "value" | "onChange"> & { mobileSize?: boolean }) => {
+const EditableSlider = (props: Except<ComponentProps<typeof LakeSlider>, "value" | "onChange">) => {
   const [value, setValue] = useState(0);
 
   return (
-    <View style={mobileSize ? styles.mobile : styles.desktop}>
-      <LakeSlider {...props} value={value} onChange={setValue} />
-    </View>
+    <Box direction="row" alignItems="center">
+      <LakeSlider {...props} value={value} onChange={setValue} style={styles.slider} />
+      <Space width={8} />
+      <LakeText>{value}</LakeText>
+    </Box>
   );
 };
 
-export const Ranges = () => {
-  return (
-    <StoryBlock title="Ranges">
-      <StoryPart title="from 0 to 100">
-        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" />
-      </StoryPart>
+export const Ranges = () => (
+  <StoryBlock title="Ranges">
+    <StoryPart title="from 0 to 100">
+      <EditableSlider min={0} max={100} step={1} />
+    </StoryPart>
 
-      <StoryPart title="from -20 to 50">
-        <EditableSlider label="Range" min={-20} max={50} step={1} unit="$" />
-      </StoryPart>
+    <StoryPart title="from -20 to 50">
+      <EditableSlider min={-20} max={50} step={1} />
+    </StoryPart>
 
-      <StoryPart title="with steps 10 by 10">
-        <EditableSlider label="Range" min={0} max={100} step={10} unit="$" />
-      </StoryPart>
-    </StoryBlock>
-  );
-};
-
-export const Sizes = () => {
-  return (
-    <StoryBlock
-      title="Slider sizes"
-      description={`UI of slider depends on container width. If width is lower than ${sliderBreakpoint}, there is only a text input without slider`}
-    >
-      <StoryPart title="Mobile">
-        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" mobileSize={true} />
-      </StoryPart>
-
-      <StoryPart title="Desktop">
-        <EditableSlider label="Range" min={0} max={100} step={1} unit="$" />
-      </StoryPart>
-    </StoryBlock>
-  );
-};
+    <StoryPart title="with steps 10 by 10">
+      <EditableSlider min={0} max={100} step={10} />
+    </StoryPart>
+  </StoryBlock>
+);
