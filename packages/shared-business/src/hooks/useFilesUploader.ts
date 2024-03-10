1249666766fe4bbd30a3a7e92a22f100987fc5ae
bloodyowl <bloodyowl@icloud.com>
2024-03-10 13:05:54 +0100

    Add `useFilesUploader` hook.
    This hook provides a generic interface for our document upload use-cases
    (SupportingDocumentCollection & CapitalDepositCase), where you can
    upload multiple files.
    
    For those, the process is the following:
    -> Call a mutation to generate the upload "configuration" (url, fields
    to send in the body)
    -> Effectively upload the file to the storage service
    
    At the moment, the storage service doesn't notify us when the file is
    effectively processed, meaning we need to rely on the local information.
    
    This hooks takes as config:
    -> `initialFiles`, which we receive from the API
    -> `generateUpload`, to get the upload configuration (its type is
    generic to account for all cases). Note that the document has an ID only
    once this operation is successful.
    -> `uploadFile`, to send the file to the storage service
    -> `onError`, called if one of the two steps fails
    
    It then manages the file lifecycle within its state.
    
    Known limitation: if new `files` are received from the API, we don't
    reconcile. This shouldn't be an issue.

---
+++ packages/shared-business/src/hooks/useFilesUploader.ts
@@ -0,0 +1,106 @@
+import { Future, Result } from "@swan-io/boxed";
+import { useCallback, useState } from "react";
+import { SwanFile } from "../utils/SwanFile";
+
+export type UploadFileInput<UploadOutput> = {
+  id: string;
+  upload: UploadOutput;
+  file: File;
+  onLoadStart: (event: ProgressEvent<XMLHttpRequestEventTarget>) => void;
+  onProgress: (event: ProgressEvent<XMLHttpRequestEventTarget>) => void;
+};
+
+export type UploadOutputWithId<UploadOutput> = { id: string; upload: UploadOutput };
+
+export type Config<UploadInput, UploadOutput, GenerateUploadError, UploadFileError> = {
+  initialFiles: SwanFile[];
+  // Takes a configuration and outputs the needed input for the actual upload
+  // It also provides an ID that's required to keep track of the file
+  generateUpload: (
+    config: UploadInput,
+  ) => Future<Result<UploadOutputWithId<UploadOutput>, GenerateUploadError>>;
+  // Effectively uploads the file
+  uploadFile: (config: UploadFileInput<UploadOutput>) => Future<Result<unknown, UploadFileError>>;
+  onError?: (error: GenerateUploadError | UploadFileError) => void;
+};
+
+export const useFilesUploader = <UploadInput, UploadOutput, GenerateUploadError, UploadFileError>(
+  config: Config<UploadInput, UploadOutput, GenerateUploadError, UploadFileError>,
+) => {
+  const [files, setFiles] = useState(config.initialFiles);
+
+  const upload = useCallback(
+    (uploadInput: UploadInput, file: File) => {
+      return config
+        .generateUpload(uploadInput)
+        .tapError(error => {
+          if (config.onError != undefined) {
+            config.onError(error);
+          }
+        })
+        .flatMapOk(({ upload, id }) => {
+          setFiles(files => [
+            ...files.filter(file => file.id !== id),
+            {
+              id,
+              statusInfo: { status: "Uploading", progress: 0 },
+              name: file.name,
+            },
+          ]);
+
+          const onProgress = (event: ProgressEvent<XMLHttpRequestEventTarget>) => {
+            setFiles(files =>
+              files.map(file => {
+                if (file.id !== id) {
+                  return file;
+                }
+                return {
+                  ...file,
+                  statusInfo: { status: "Uploading", progress: event.loaded / event.total },
+                };
+              }),
+            );
+          };
+
+          return config
+            .uploadFile({
+              id,
+              upload,
+              file,
+              onLoadStart: onProgress,
+              onProgress,
+            })
+            .tapOk(() => {
+              setFiles(files =>
+                files.map(file => {
+                  if (file.id !== id) {
+                    return file;
+                  }
+                  return {
+                    ...file,
+                    statusInfo: { status: "Uploaded" },
+                  };
+                }),
+              );
+            })
+            .tapError(error => {
+              if (config.onError != undefined) {
+                config.onError(error);
+              }
+              setFiles(files => files.filter(file => file.id !== id));
+            });
+        });
+    },
+    [config],
+  );
+
+  const remove = useCallback((id: string) => {
+    setFiles(files => files.filter(file => file.id !== id));
+  }, []);
+
+  return {
+    files,
+    upload,
+    remove,
+  };
+};
