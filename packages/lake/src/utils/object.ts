0ff57f86af17345f958d445fb61294cef4c68afd
Mathieu Acthernoene <zoontek@gmail.com>
2024-03-13 15:39:13 +0100

    Fix pick helper

--- packages/lake/src/utils/object.ts
+++ packages/lake/src/utils/object.ts
@@ -2,11 +2,12 @@ export const isPlainObject = (value: unknown): boolean => {
   return Object.prototype.toString.call(value) === "[object Object]";
 };
 
-export const pick = <T extends Record<K, unknown>, K extends keyof T>(
+export const pick = <T extends Record<K, unknown>, const K extends keyof T>(
   object: T,
   keys: K[],
 ): Pick<T, K> => {
   const allowedKeys = new Set<K>(keys);
+
   return Object.fromEntries(
     Object.entries(object).filter(([key]) => allowedKeys.has(key as K)),
   ) as Pick<T, K>;
