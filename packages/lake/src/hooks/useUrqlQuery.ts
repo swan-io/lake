61f739295bf3ef8d3db06364b8231d26beda2c39
bloodyowl <bloodyowl@icloud.com>
2024-03-14 18:16:55 +0100

    Add useDeferredUrqlQuery

--- packages/lake/src/hooks/useUrqlQuery.ts
+++ packages/lake/src/hooks/useUrqlQuery.ts
@@ -1,6 +1,14 @@
-import { AsyncData, Result } from "@swan-io/boxed";
+import { AsyncData, Future, Result } from "@swan-io/boxed";
 import { DependencyList, useCallback, useEffect, useMemo, useState } from "react";
-import { AnyVariables, CombinedError, UseQueryArgs, useQuery } from "urql";
+import {
+  AnyVariables,
+  CombinedError,
+  DocumentInput,
+  OperationResult,
+  UseQueryArgs,
+  useClient,
+  useQuery,
+} from "urql";
 import { isNotNullish, isNullish } from "../utils/nullish";
 
 type Query<Data> = {
@@ -132,4 +140,35 @@ export const useUrqlPaginatedQuery = <Data, Variables extends AnyVariables>(
     reload,
     setAfter,
   };
+};
+
+type DeferredQuery<Data, Variables extends AnyVariables> = [
+  data: AsyncData<Result<Data, CombinedError>>,
+  query: (args: Variables) => Future<Result<Data, CombinedError>>,
+];
+
+export const useDeferredUrqlQuery = <Data, Variables extends AnyVariables>(
+  document: DocumentInput<Data, Variables>,
+): DeferredQuery<Data, Variables> => {
+  const [data, setData] = useState<AsyncData<Result<Data, CombinedError>>>(AsyncData.NotAsked());
+
+  const client = useClient();
+
+  const query = useCallback(
+    (args: Variables) => {
+      setData(AsyncData.Loading());
+      return Future.fromPromise<OperationResult<Data, Variables>, CombinedError>(
+        client.query(document, args).toPromise(),
+      )
+        .mapOkToResult(res =>
+          res.data != null ? Result.Ok(res.data) : Result.Error(res.error as CombinedError),
+        )
+        .tap(data => {
+          setData(AsyncData.Done(data));
+        });
+    },
+    [client, document],
+  );
+
+  return [data, query];
 };
