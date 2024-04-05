e924365a077073faacdcc002cb827c0bb2c086ca
bloodyowl <bloodyowl@icloud.com>
2024-04-05 12:04:10 +0200

    Tweak suspendable to only suspend on first render

--- packages/lake/src/components/Suspendable.tsx
+++ packages/lake/src/components/Suspendable.tsx
@@ -1,10 +1,15 @@
-import { Suspense, SuspenseProps, createContext, useContext } from "react";
+import { Suspense, SuspenseProps, createContext, useContext, useEffect, useRef } from "react";
 
 const SuspendableContext = createContext(false);
 
-export const Suspendable = (props: SuspenseProps) => {
+export const Suspendable = ({ ...props }: SuspenseProps) => {
+  const isFirstRenderRef = useRef(true);
+  useEffect(() => {
+    isFirstRenderRef.current = false;
+  }, []);
+
   return (
-    <SuspendableContext.Provider value={true}>
+    <SuspendableContext.Provider value={isFirstRenderRef.current}>
       <Suspense {...props} />
     </SuspendableContext.Provider>
   );
