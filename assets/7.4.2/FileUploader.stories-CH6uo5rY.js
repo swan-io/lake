b168f32978af588ecb8e50ef45666c04534a291f
ncomont <ncomont@users.noreply.github.com>
2024-04-09 15:35:09 +0000

    deploy: 300bf7de90569cce48d2d72edfe49de2d345cfa1

---
+++ assets/7.4.2/FileUploader.stories-CH6uo5rY.js
@@ -0,0 +1,34 @@
+import{s as u,j as o,E as r,l as n}from"./Space-B-tuzGSB.js";import{F as d}from"./FilesUploader-BHAzas-h.js";import{S as p,a as c}from"./_StoriesComponents-qw3Cph9e.js";import"./extends-CCbyfPlC.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-CBqU2yxZ.js";import"./index-BtM5VmRH.js";import"./FileInput-BLam1_px.js";import"./LakeButton-B_Hql0KV.js";import"./commonStyles-t4XfA7cz.js";import"./Pressable-D4CfqCYA.js";import"./index-CP-wDJ33.js";import"./LakeHeading-CLNxGxoz.js";import"./useBoolean-B5BCxMq_.js";import"./index-D3ylJrlI.js";import"./i18n-DA5rVMx_.js";import"./rifm-BUW2jT7k.js";import"./utc-DJvBpWoA.js";import"./array-rjEBP2Ry.js";import"./LakeAlert-skcJtdOq.js";import"./Tag-BH47Yzno.js";import"./index-BWWcrcy6.js";import"./index-DGaDRGzj.js";const g=u.create({storyPart:{maxWidth:600}}),U=["application/pdf","image/png","image/jpeg","image/svg+xml"],W={title:"Forms/FilesUploader",component:d},y=[{id:crypto.randomUUID(),name:"first-document.pdf",url:"https://swan.io",statusInfo:{status:"Validated"}},{id:crypto.randomUUID(),name:"second-document.png",statusInfo:{status:"Pending"}},{id:crypto.randomUUID(),name:"third-document.jpg",statusInfo:{status:"Refused",reason:"Quality of the document was too low"}},{id:crypto.randomUUID(),name:"fourth-document.xls",statusInfo:{status:"Uploaded"}},{id:crypto.randomUUID(),name:"last-document.png",statusInfo:{status:"Uploaded"}}],t=()=>o.jsx(p,{title:"UploadArea with several documents",children:o.jsx(c,{title:"",style:g.storyPart,children:o.jsx(d,{maxSize:2e7,icon:"document-regular",accept:U,initialFiles:y,getUploadConfig:()=>{},generateUpload:()=>r.value(n.Ok({id:crypto.randomUUID(),upload:{}})),onRemoveFile:()=>r.make(e=>{setTimeout(()=>e(n.Ok(void 0)),1e3)}),uploadFile:({onLoadStart:e,onProgress:a})=>r.make(l=>{e({loaded:0,total:1e3}),setTimeout(()=>a({loaded:100,total:1e3}),100),setTimeout(()=>a({loaded:600,total:1e3}),500),setTimeout(()=>{l(n.Ok(void 0))},1e3)}),formatAndSizeDescription:"20MB max"})})});t.__docgenInfo={description:"",methods:[],displayName:"WithSeveralDocuments"};var s,i,m;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  return <StoryBlock title="UploadArea with several documents">
+      <StoryPart title="" style={styles.storyPart}>
+        <FilesUploader maxSize={20_000_000} icon="document-regular" accept={ACCEPTED_FORMATS} initialFiles={documents} getUploadConfig={() => {}} generateUpload={() => Future.value(Result.Ok({
+        id: crypto.randomUUID(),
+        upload: {}
+      }))} onRemoveFile={() => Future.make<Result<unknown, unknown>>(resolve => {
+        setTimeout(() => resolve(Result.Ok(undefined)), 1_000);
+      })} uploadFile={({
+        onLoadStart,
+        onProgress
+      }) => {
+        return Future.make<Result<unknown, unknown>>(resolve => {
+          onLoadStart(({
+            loaded: 0,
+            total: 1000
+          } as ProgressEvent<XMLHttpRequestEventTarget>));
+          setTimeout(() => onProgress(({
+            loaded: 100,
+            total: 1000
+          } as ProgressEvent<XMLHttpRequestEventTarget>)), 100);
+          setTimeout(() => onProgress(({
+            loaded: 600,
+            total: 1000
+          } as ProgressEvent<XMLHttpRequestEventTarget>)), 500);
+          setTimeout(() => {
+            resolve(Result.Ok(undefined));
+          }, 1_000);
+        });
+      }} formatAndSizeDescription={"20MB max"} />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const q=["WithSeveralDocuments"];export{t as WithSeveralDocuments,q as __namedExportsOrder,W as default};
+//# sourceMappingURL=FileUploader.stories-CH6uo5rY.js.map
