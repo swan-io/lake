387e21763e6028badd8e8fae9ef709d30f175f5f
Mathieu Acthernoene <zoontek@gmail.com>
2024-05-29 18:07:14 +0200

    Add comment

--- packages/lake/src/utils/userAgent.ts
+++ packages/lake/src/utils/userAgent.ts
@@ -26,6 +26,7 @@ export const isChromeMobile = isChrome && isMobile;
 export const isSafari =
   isNullish(navigator.userAgentData) && SAFARI_REGEXP.test(navigator.userAgent) && !isChrome;
 
+// from https://stackoverflow.com/questions/21741841/detecting-ios-android-operating-system
 export const isSupportedMobileDevice = ((): boolean => {
   // Windows Phone must come first because its UA also contains "Android"
   if (/windows phone/i.test(navigator.userAgent)) {
