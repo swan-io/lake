d345b7d0c3fcf7813805842b33d902721cce8e5a
Mathieu Acthernoene <zoontek@gmail.com>
2024-05-29 18:03:27 +0200

    Add isSupportedMobileDevice

--- packages/lake/src/utils/userAgent.ts
+++ packages/lake/src/utils/userAgent.ts
@@ -4,10 +4,40 @@ const CHROME_REGEXP = /Chrom(e|ium)\//;
 const MOBI_REGEXP = /Mobi/;
 const SAFARI_REGEXP = /Safari\//;
 
+/**
+ * @deprecated
+ */
 export const isChrome = CHROME_REGEXP.test(navigator.userAgent);
+
+/**
+ * @deprecated
+ */
 export const isMobile = MOBI_REGEXP.test(navigator.userAgent);
+
+/**
+ * @deprecated
+ */
 export const isChromeMobile = isChrome && isMobile;
 
-// https://github.com/jakearchibald/safari-14-idb-fix/blob/v3.0.0/src/index.ts#L7
+/**
+ * @see https://github.com/jakearchibald/safari-14-idb-fix/blob/v3.0.0/src/index.ts#L7
+ * @deprecated
+ */
 export const isSafari =
   isNullish(navigator.userAgentData) && SAFARI_REGEXP.test(navigator.userAgent) && !isChrome;
+
+export const isSupportedMobileDevice = ((): boolean => {
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(navigator.userAgent)) {
+    return false;
+  }
+  if (/android/i.test(navigator.userAgent)) {
+    return true;
+  }
+  // MSStream must be checked to avoid identify IE11 as iOS
+  if (/iPad|iPhone/.test(navigator.userAgent) && !("MSStream" in window)) {
+    return true;
+  }
+
+  return false;
+})();
