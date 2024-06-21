49debcd824bba2b570d99698d326bbf801cdc796
Mathieu Acthernoene <zoontek@gmail.com>
2024-06-21 16:04:04 +0200

    Add ScrollView

--- packages/lake/src/components/ScrollView.tsx
+++ packages/lake/src/components/ScrollView.tsx
@@ -1,7 +1,14 @@
-import { Ref, forwardRef, useCallback, useImperativeHandle, useRef } from "react";
+import {
+  Ref,
+  SyntheticEvent,
+  UIEvent,
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+  useRef,
+} from "react";
 import { StyleProp, StyleSheet, View, ViewProps, ViewStyle } from "react-native";
 import { useMergeRefs } from "../hooks/useMergeRefs";
-import { ScrollEvent, ScrollViewBase } from "./ScrollViewBase";
 
 const styles = StyleSheet.create({
   base: {
@@ -21,10 +28,56 @@ const styles = StyleSheet.create({
   contentHorizontal: {
     flexDirection: "row",
   },
+  hideScrollbars: {
+    scrollbarWidth: "none",
+  },
 });
+
+const normalizeScrollEvent = (event: SyntheticEvent<UIEvent>) => {
+  const target = event.target as HTMLElement;
+
+  const contentOffset = {
+    get x() {
+      return target.scrollLeft;
+    },
+    get y() {
+      return target.scrollTop;
+    },
+  };
+
+  const contentSize = {
+    get height() {
+      return target.scrollHeight;
+    },
+    get width() {
+      return target.scrollWidth;
+    },
+  };
+
+  const layoutMeasurement = {
+    get height() {
+      return target.offsetHeight;
+    },
+    get width() {
+      return target.offsetWidth;
+    },
+  };
+
+  return {
+    nativeEvent: { contentOffset, contentSize, layoutMeasurement },
+    timeStamp: Date.now(),
+  };
+};
+
+export type ScrollEvent = ReturnType<typeof normalizeScrollEvent>;
 
 export type ScrollViewRef = {
   scrollTo: (event: { x?: number; y?: number; animated?: boolean }) => void;
+};
+
+const shouldEmitScrollEvent = (lastTick: number, eventThrottle: number) => {
+  const timeSinceLastTick = Date.now() - lastTick;
+  return eventThrottle > 0 && timeSinceLastTick >= eventThrottle;
 };
 
 type Props = ViewProps & {
@@ -32,6 +85,7 @@ type Props = ViewProps & {
   horizontal?: boolean;
   onScroll?: (event: ScrollEvent) => void;
   scrollEventThrottle?: number;
+  showsScrollIndicators?: boolean;
 };
 
 export const ScrollView = forwardRef<ScrollViewRef, Props>(
@@ -42,15 +96,53 @@ export const ScrollView = forwardRef<ScrollViewRef, Props>(
       horizontal = false,
       onScroll,
       scrollEventThrottle = 16,
+      showsScrollIndicators = true,
       style,
-      ...props
+      ...viewProps
     },
     forwardedRef,
   ) => {
-    const ref = useRef<ScrollViewRef>(null);
+    const innerRef = useRef<ScrollViewRef>(null);
+    const mergedRef = useMergeRefs(innerRef, forwardedRef) as Ref<View>;
+    const state = useRef({ isScrolling: false, scrollLastTick: 0 });
+    const timeout = useRef<NodeJS.Timeout | null>(null);
+
+    const handleOnScroll = useCallback(
+      (event: SyntheticEvent<UIEvent>) => {
+        event.stopPropagation();
+
+        if (event.target === (innerRef.current as HTMLElement)) {
+          event.persist();
+
+          // A scroll happened, so the scroll resets the scrollend timeout.
+          if (timeout.current != null) {
+            clearTimeout(timeout.current);
+          }
+
+          timeout.current = setTimeout(() => {
+            state.current.isScrolling = false;
+            onScroll?.(normalizeScrollEvent(event));
+          }, 100);
+
+          if (state.current.isScrolling) {
+            // Scroll last tick may have changed, check if we need to notify
+            if (shouldEmitScrollEvent(state.current.scrollLastTick, scrollEventThrottle)) {
+              state.current.scrollLastTick = Date.now();
+              onScroll?.(normalizeScrollEvent(event));
+            }
+          } else {
+            // Weren't scrolling, so we must have just started
+            state.current.isScrolling = true;
+            state.current.scrollLastTick = Date.now();
+            onScroll?.(normalizeScrollEvent(event));
+          }
+        }
+      },
+      [onScroll, scrollEventThrottle],
+    );
 
     const scrollTo = useCallback<ScrollViewRef["scrollTo"]>(({ x = 0, y = 0, animated = true }) => {
-      const element = ref.current as unknown as Element;
+      const element = innerRef.current as Element;
 
       if (element != null) {
         if (typeof element.scroll === "function") {
@@ -62,20 +154,24 @@ export const ScrollView = forwardRef<ScrollViewRef, Props>(
       }
     }, []);
 
-    useImperativeHandle(ref, () => ({ scrollTo }));
+    useImperativeHandle(innerRef, () => ({ scrollTo }));
 
     return (
-      <ScrollViewBase
-        {...props}
-        ref={useMergeRefs(ref, forwardedRef) as Ref<View>}
-        onScroll={onScroll}
-        scrollEventThrottle={scrollEventThrottle}
-        style={[styles.base, horizontal && styles.horizontal, style]}
+      <View
+        {...viewProps}
+        ref={mergedRef}
+        onScroll={handleOnScroll}
+        style={[
+          styles.base,
+          style,
+          horizontal && styles.horizontal,
+          !showsScrollIndicators && styles.hideScrollbars,
+        ]}
       >
         <View style={[horizontal && styles.contentHorizontal, contentContainerStyle]}>
           {children}
         </View>
-      </ScrollViewBase>
+      </View>
     );
   },
 );
