2f96c4952a8da26f3e755802e4289090a37c7945
zoontek <zoontek@users.noreply.github.com>
2024-06-21 12:59:08 +0000

    deploy: af9c75564bc7a1e57c052ff972a7b7a7c2a618d0

---
+++ assets/8.6.0/BottomPanel.stories-jZi_yZMD.js
@@ -0,0 +1,23 @@
+import{s as y,c as a,j as t,B as b,V as l,S as i}from"./Space-vOSkumTC.js";import{r as u}from"./index-uCp2LrAq.js";import{B as d}from"./BottomPanel-CQMdPCyR.js";import{L as k}from"./LakeButton-CKRXkx-m.js";import{L as g}from"./LakeTextInput-CXIrFBWW.js";import{S as x}from"./_StoriesComponents-jOR6vAMI.js";import"./extends-CF3RwP-h.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BOkhicXD.js";import"./commonStyles-t4XfA7cz.js";import"./useBodyClassName-CiTylq2A.js";import"./FocusTrap-BMYOn2gx.js";import"./a11y-DXvpDVrG.js";import"./array-rjEBP2Ry.js";import"./Suspendable-DAkP2ur3.js";import"./index-B_CfE0f5.js";import"./TransitionView-jc08-ng2.js";import"./index-ByctqrIc.js";import"./Pressable-C1mxBRS_.js";import"./Fill-Cf1X0Mdz.js";import"./LakeSelect-iEaqsXpi.js";import"./useDisclosure-X_l1NENI.js";import"./Popover-C64WgQme.js";import"./useResponsive-CWYKQO6x.js";import"./function-De4Cecxu.js";const e=y.create({button:{alignSelf:"flex-start"},panelContent:{padding:16},block1:{width:120,height:60,backgroundColor:a.gray[200],borderRadius:8},block2:{width:200,height:60,backgroundColor:a.gray[200],borderRadius:8},block3:{width:180,height:60,backgroundColor:a.gray[200],borderRadius:8}}),K={title:"Layout/BottomPanel",component:d},f=n=>{const[s,r]=u.useState(n.defaultValue??"");return t.jsx(g,{...n,value:s,onChange:h=>r(h.currentTarget.value)})},o=()=>{const[n,s]=u.useState(!1),r=()=>{s(!0)};return t.jsxs(x,{title:"BottomPanel",description:["BottomPanel is a component that allows you to render a panel on the bottom of the screen.","To make this component work, you need a root element with the id `full-page-layer-root` below your #root-app element","You can open it by clicking on the button below."],children:[t.jsx(k,{onPress:r,size:"small",style:e.button,children:"Open bottom panel"}),t.jsx(d,{visible:n,onPressClose:()=>s(!1),children:t.jsxs(b,{style:e.panelContent,children:[t.jsx(f,{}),t.jsx(l,{style:e.block1}),t.jsx(i,{height:16}),t.jsx(l,{style:e.block2}),t.jsx(i,{height:16}),t.jsx(l,{style:e.block3})]})})]})};o.__docgenInfo={description:"",methods:[],displayName:"Default"};var p,m,c;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const [isOpen, setIsOpen] = useState(false);
+  const open = () => {
+    setIsOpen(true);
+  };
+  return <StoryBlock title="BottomPanel" description={["BottomPanel is a component that allows you to render a panel on the bottom of the screen.", "To make this component work, you need a root element with the id \`full-page-layer-root\` below your #root-app element", "You can open it by clicking on the button below."]}>
+      <LakeButton onPress={open} size="small" style={styles.button}>
+        Open bottom panel
+      </LakeButton>
+
+      <BottomPanel visible={isOpen} onPressClose={() => setIsOpen(false)}>
+        <Box style={styles.panelContent}>
+          <EditableInputText />
+          <View style={styles.block1} />
+          <Space height={16} />
+          <View style={styles.block2} />
+          <Space height={16} />
+          <View style={styles.block3} />
+        </Box>
+      </BottomPanel>
+    </StoryBlock>;
+}`,...(c=(m=o.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const M=["Default"];export{o as Default,M as __namedExportsOrder,K as default};
+//# sourceMappingURL=BottomPanel.stories-jZi_yZMD.js.map
