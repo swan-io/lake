782d721583e7098bdb9029e533fa99d41de24cf2
zoontek <zoontek@users.noreply.github.com>
2024-06-27 11:17:17 +0000

    deploy: 68491a2036c9c7ae81a5ad81995d29694b529f47

---
+++ assets/8.8.2/Button.stories-DTuBuKIg.js
@@ -0,0 +1,172 @@
+import{s as R,j as e,B as s,c as u,S as h,L as S}from"./ScrollView-Boi5CiBf.js";import{r as L}from"./index-uCp2LrAq.js";import{L as g}from"./LakeButton-k04cYsGm.js";import{S as d}from"./Stack-JrM_SJbJ.js";import{S as k,a as r}from"./_StoriesComponents-6_K3irzY.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BOkhicXD.js";import"./commonStyles-t4XfA7cz.js";import"./Pressable-67_mt1oY.js";import"./index-DSWO_SAH.js";import"./extends-CF3RwP-h.js";const t=R.create({buttons:{flexWrap:"wrap"},buttonWrapper:{marginRight:32,marginBottom:16},button:{minWidth:200},countValue:{width:24}}),T={title:"Interactivity/Button",component:g},y=n=>n.charAt(0).toUpperCase()+n.slice(1),i=n=>{const[m,c]=L.useState(0);return e.jsxs(s,{direction:"row",alignItems:"center",style:t.buttonWrapper,children:[e.jsx(g,{...n,style:t.button,onPress:()=>c(z=>z+1)}),e.jsx(S,{align:"right",style:t.countValue,children:m})]})},o=n=>{const[m,c]=L.useState(!1);return e.jsx(g,{color:"live",...n,loading:m,onPress:()=>{c(!0),setTimeout(()=>c(!1),2e3)}})},l=()=>e.jsxs(k,{title:"Button colors",children:[e.jsx(r,{title:"Primary",children:e.jsx(s,{direction:"row",style:t.buttons,children:Object.keys(u).map(n=>e.jsx(i,{mode:"primary",color:n,children:y(n)},n))})}),e.jsx(r,{title:"Secondary",children:e.jsx(s,{direction:"row",style:t.buttons,children:Object.keys(u).map(n=>e.jsx(i,{mode:"secondary",color:n,children:y(n)},n))})}),e.jsx(r,{title:"Tertiary",children:e.jsx(s,{direction:"row",style:t.buttons,children:Object.keys(u).map(n=>e.jsx(i,{mode:"tertiary",color:n,children:y(n)},n))})})]}),a=()=>e.jsxs(k,{title:"Button states",children:[e.jsx(r,{title:"Small",children:e.jsx(i,{size:"small",color:"live",children:"Small button"})}),e.jsx(r,{title:"Disabled",children:e.jsx(i,{disabled:!0,color:"live",children:"Disabled"})}),e.jsxs(r,{title:"Loading",children:[e.jsxs(d,{alignItems:"center",direction:"row",space:12,children:[e.jsx(o,{size:"large",mode:"primary",children:"Refresh"}),e.jsx(o,{size:"small",mode:"primary",children:"Refresh"}),e.jsx(o,{size:"large",mode:"secondary",children:"Refresh"}),e.jsx(o,{size:"small",mode:"secondary",children:"Refresh"}),e.jsx(o,{size:"large",mode:"tertiary",children:"Refresh"}),e.jsx(o,{size:"small",mode:"tertiary",children:"Refresh"})]}),e.jsx(h,{height:12}),e.jsxs(d,{alignItems:"center",direction:"row",space:12,children:[e.jsx(o,{ariaLabel:"Refresh",icon:"arrow-clockwise-filled",size:"large",mode:"primary"}),e.jsx(o,{ariaLabel:"Refresh",icon:"arrow-clockwise-filled",size:"small",mode:"primary"}),e.jsx(o,{ariaLabel:"Refresh",icon:"arrow-clockwise-filled",size:"large",mode:"secondary"}),e.jsx(o,{ariaLabel:"Refresh",icon:"arrow-clockwise-filled",size:"small",mode:"secondary"}),e.jsx(o,{ariaLabel:"Refresh",icon:"arrow-clockwise-filled",size:"large",mode:"tertiary"}),e.jsx(o,{ariaLabel:"Refresh",icon:"arrow-clockwise-filled",size:"small",mode:"tertiary"})]}),e.jsx(h,{height:12}),e.jsxs(d,{alignItems:"center",direction:"row",space:12,children:[e.jsx(o,{icon:"arrow-clockwise-filled",size:"large",mode:"primary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",size:"small",mode:"primary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",size:"large",mode:"secondary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",size:"small",mode:"secondary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",size:"large",mode:"tertiary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",size:"small",mode:"tertiary",children:"Refresh"})]}),e.jsx(h,{height:12}),e.jsxs(d,{alignItems:"center",direction:"row",space:12,children:[e.jsx(o,{icon:"arrow-clockwise-filled",iconPosition:"end",size:"large",mode:"primary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",iconPosition:"end",size:"small",mode:"primary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",iconPosition:"end",size:"large",mode:"secondary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",iconPosition:"end",size:"small",mode:"secondary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",iconPosition:"end",size:"large",mode:"tertiary",children:"Refresh"}),e.jsx(o,{icon:"arrow-clockwise-filled",iconPosition:"end",size:"small",mode:"tertiary",children:"Refresh"})]})]}),e.jsx(r,{title:"With pill",children:e.jsx(i,{pill:!0,color:"live",children:"Pill"})}),e.jsx(r,{title:"With icon",children:e.jsxs(s,{direction:"row",style:t.buttons,children:[e.jsx(i,{icon:"add-circle-filled",color:"positive",children:"Add"}),e.jsx(i,{icon:"delete-filled",iconPosition:"end",color:"negative",children:"Remove"})]})}),e.jsx(r,{title:"Vertical",children:e.jsxs(s,{direction:"row",style:t.buttons,children:[e.jsx(i,{icon:"add-circle-regular",color:"positive",direction:"column",mode:"secondary",children:"Add"}),e.jsx(i,{icon:"delete-regular",iconPosition:"end",color:"negative",direction:"column",size:"small",mode:"secondary",children:"Remove"})]})})]});l.__docgenInfo={description:"",methods:[],displayName:"Colors"};a.__docgenInfo={description:"",methods:[],displayName:"ButtonStates"};var f,w,B;l.parameters={...l.parameters,docs:{...(f=l.parameters)==null?void 0:f.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Button colors">
+      <StoryPart title="Primary">
+        <Box direction="row" style={styles.buttons}>
+          {Object.keys(colors).map(color => <ButtonWithCounter key={color} mode="primary" color={(color as ColorVariants)}>
+              {capitalize(color)}
+            </ButtonWithCounter>)}
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Secondary">
+        <Box direction="row" style={styles.buttons}>
+          {Object.keys(colors).map(color => <ButtonWithCounter mode="secondary" key={color} color={(color as ColorVariants)}>
+              {capitalize(color)}
+            </ButtonWithCounter>)}
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Tertiary">
+        <Box direction="row" style={styles.buttons}>
+          {Object.keys(colors).map(color => <ButtonWithCounter mode="tertiary" key={color} color={(color as ColorVariants)}>
+              {capitalize(color)}
+            </ButtonWithCounter>)}
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(B=(w=l.parameters)==null?void 0:w.docs)==null?void 0:B.source}}};var p,x,j;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Button states">
+      <StoryPart title="Small">
+        <ButtonWithCounter size="small" color="live">
+          Small button
+        </ButtonWithCounter>
+      </StoryPart>
+
+      <StoryPart title="Disabled">
+        <ButtonWithCounter disabled={true} color="live">
+          Disabled
+        </ButtonWithCounter>
+      </StoryPart>
+
+      <StoryPart title="Loading">
+        <Stack alignItems="center" direction="row" space={12}>
+          <LoadingButton size="large" mode="primary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton size="small" mode="primary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton size="large" mode="secondary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton size="small" mode="secondary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton size="large" mode="tertiary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton size="small" mode="tertiary">
+            Refresh
+          </LoadingButton>
+        </Stack>
+
+        <Space height={12} />
+
+        <Stack alignItems="center" direction="row" space={12}>
+          <LoadingButton ariaLabel="Refresh" icon="arrow-clockwise-filled" size="large" mode="primary" />
+
+          <LoadingButton ariaLabel="Refresh" icon="arrow-clockwise-filled" size="small" mode="primary" />
+
+          <LoadingButton ariaLabel="Refresh" icon="arrow-clockwise-filled" size="large" mode="secondary" />
+
+          <LoadingButton ariaLabel="Refresh" icon="arrow-clockwise-filled" size="small" mode="secondary" />
+
+          <LoadingButton ariaLabel="Refresh" icon="arrow-clockwise-filled" size="large" mode="tertiary" />
+
+          <LoadingButton ariaLabel="Refresh" icon="arrow-clockwise-filled" size="small" mode="tertiary" />
+        </Stack>
+
+        <Space height={12} />
+
+        <Stack alignItems="center" direction="row" space={12}>
+          <LoadingButton icon="arrow-clockwise-filled" size="large" mode="primary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" size="small" mode="primary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" size="large" mode="secondary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" size="small" mode="secondary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" size="large" mode="tertiary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" size="small" mode="tertiary">
+            Refresh
+          </LoadingButton>
+        </Stack>
+
+        <Space height={12} />
+
+        <Stack alignItems="center" direction="row" space={12}>
+          <LoadingButton icon="arrow-clockwise-filled" iconPosition="end" size="large" mode="primary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" iconPosition="end" size="small" mode="primary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" iconPosition="end" size="large" mode="secondary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" iconPosition="end" size="small" mode="secondary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" iconPosition="end" size="large" mode="tertiary">
+            Refresh
+          </LoadingButton>
+
+          <LoadingButton icon="arrow-clockwise-filled" iconPosition="end" size="small" mode="tertiary">
+            Refresh
+          </LoadingButton>
+        </Stack>
+      </StoryPart>
+
+      <StoryPart title="With pill">
+        <ButtonWithCounter pill={true} color="live">
+          Pill
+        </ButtonWithCounter>
+      </StoryPart>
+
+      <StoryPart title="With icon">
+        <Box direction="row" style={styles.buttons}>
+          <ButtonWithCounter icon="add-circle-filled" color="positive">
+            Add
+          </ButtonWithCounter>
+
+          <ButtonWithCounter icon="delete-filled" iconPosition="end" color="negative">
+            Remove
+          </ButtonWithCounter>
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Vertical">
+        <Box direction="row" style={styles.buttons}>
+          <ButtonWithCounter icon="add-circle-regular" color="positive" direction="column" mode="secondary">
+            Add
+          </ButtonWithCounter>
+
+          <ButtonWithCounter icon="delete-regular" iconPosition="end" color="negative" direction="column" size="small" mode="secondary">
+            Remove
+          </ButtonWithCounter>
+        </Box>
+      </StoryPart>
+    </StoryBlock>;
+}`,...(j=(x=a.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};const E=["Colors","ButtonStates"];export{a as ButtonStates,l as Colors,E as __namedExportsOrder,T as default};
+//# sourceMappingURL=Button.stories-DTuBuKIg.js.map
---
+++ assets/8.8.2/Toast.stories-ueRWxgRX.js
@@ -0,0 +1,104 @@
+import{s as M,g as H,G as S,F as E,$ as P,i as Y,j as t,V as L,c as a,B as R,I as v,S as l,L as B,C as G,m as X,P as J,E as W,h as K}from"./ScrollView-Boi5CiBf.js";import{L as u}from"./LakeButton-k04cYsGm.js";import{t as C}from"./i18n-CmF6THGI.js";import{r as x}from"./index-uCp2LrAq.js";import{w as Q}from"./with-selector-3StRHtcq.js";import{s as U}from"./clipboard-BXAk6GqW.js";import{P as F}from"./Pressable-67_mt1oY.js";import{T as Z}from"./TransitionGroupView-Bzbw2yWv.js";import{S as tt,a as O}from"./_StoriesComponents-6_K3irzY.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BOkhicXD.js";import"./commonStyles-t4XfA7cz.js";import"./index-DSWO_SAH.js";import"./extends-CF3RwP-h.js";import"./rifm-BQe6kEtz.js";import"./utc-DJvBpWoA.js";import"./array-rjEBP2Ry.js";import"./tslib.es6-UMXCHJY6.js";import"./TransitionView-CxsCRnTM.js";function et(o){const s=new Set;let n=o;return{get:()=>n,set(e){n=typeof e=="function"?e(n):e,s.forEach(c=>c(n))},subscribe:e=>(s.add(e),()=>{s.delete(e)}),reset(){n=o,s.forEach(e=>e(n))}}}function ot(o){return o}function nt(o,s){return s===void 0&&(s=Object.is),Q.useSyncExternalStoreWithSelector(o.subscribe,o.get,o.get,ot,s)}const st=({duration:o,onEnd:s})=>{const n=new Set;let e=0,c=0;const i=()=>{const r=(e-Date.now())/o;n.forEach(m=>m(r)),y=window.requestAnimationFrame(i)};let y=window.requestAnimationFrame(i);const b=()=>{e=Date.now()+o,c=window.setTimeout(()=>{h(),s()},o)},w=()=>{window.clearTimeout(c),b()},f=r=>(n.add(r),()=>{n.delete(r)}),h=()=>{document.removeEventListener("visibilitychange",j),window.clearTimeout(c),window.cancelAnimationFrame(y)},j=()=>{document.removeEventListener("visibilitychange",j),w()};return document.hidden?document.addEventListener("visibilitychange",j):w(),{clear:h,reset:w,subscribe:f}},T=et([]),rt=()=>nt(T),q=o=>{var n;const s=T.get().find(e=>e.uid===o);s!=null&&((n=s.progress)==null||n.clear(),T.set(e=>e.filter(c=>c.uid!==o)))};let V=new WeakMap;const it=o=>{V=o},at=()=>V,p=({variant:o,title:s,description:n,error:e,autoClose:c})=>{var f;const i=`${o} - ${s} - ${n??""}`,y=T.get().find(h=>h.uid===i);if(y!=null)return(f=y.progress)==null||f.reset(),i;const w=c??(o==="info"||o==="success")?st({duration:1e4,onEnd:()=>{q(i)}}):void 0;return T.set(h=>[{uid:i,variant:o,title:s,description:n,error:e,progress:w},...h]),i},g=M.create({list:{position:"absolute",right:0,bottom:0,maxHeight:"100%",maxWidth:400,paddingVertical:8,width:"100%",zIndex:10},toastWrapper:{paddingHorizontal:16,paddingVertical:8},toast:{padding:24,borderRadius:4,borderWidth:1,borderLeftWidth:4,overflow:"hidden",boxShadow:H.modal},contentContainer:{paddingRight:36},closeButton:{zIndex:1,position:"absolute",width:24,height:24,right:24,top:0,bottom:0,margin:"auto"},progressBar:{height:2,transformOrigin:"left"},copyTooltip:{alignSelf:"flex-start"},copyButton:{alignItems:"center",flexDirection:"row",flexGrow:1,flexShrink:1}}),ct=o=>{const s=E.isArray(o)?o:[o];return E.filterMap(s,n=>n instanceof Error?S.Some(n):S.None())},lt=x.memo(({variant:o,uid:s,title:n,description:e,error:c,progress:i,onClose:y})=>{const b=x.useRef(null),[w,f]=x.useState("copy"),h=K(e),[j]=x.useState(()=>c==null?S.None():E.findMap(ct(c),m=>S.fromNullable(at().get(m)))),r=P(o).returnType().with("success",()=>"positive").with("error",()=>"negative").with("info",()=>"shakespear").with("warning",()=>"warning").exhaustive();return x.useEffect(()=>{if(!Y(i))return i.subscribe(m=>{b.current instanceof HTMLElement&&(b.current.style.transform=`scaleX(${m})`)})},[i]),t.jsx(L,{style:g.toastWrapper,children:t.jsxs(L,{style:[g.toast,{borderColor:a[r][200],borderLeftColor:a[r][500],backgroundColor:a[r][0]}],children:[t.jsxs(R,{style:g.contentContainer,children:[t.jsxs(R,{direction:"row",alignItems:"center",children:[P(o).with("success",()=>t.jsx(v,{name:"checkmark-circle-regular",size:20,color:a[r][700]})).with("error",()=>t.jsx(v,{name:"dismiss-circle-regular",size:20,color:a[r][700]})).with("info",()=>t.jsx(v,{name:"info-regular",size:20,color:a[r][700]})).with("warning",()=>t.jsx(v,{name:"warning-regular",size:20,color:a[r][700]})).exhaustive(),t.jsx(l,{width:12}),t.jsx(B,{variant:"regular",color:a[r][700],children:n})]}),h&&t.jsxs(t.Fragment,{children:[t.jsx(l,{height:8}),t.jsx(B,{variant:"smallRegular",color:a.gray[700],children:e})]}),P(j).with(S.P.None,()=>null).with(S.P.Some(G.select()),m=>t.jsxs(t.Fragment,{children:[t.jsx(l,{height:h?4:8}),t.jsx(X,{describedBy:"copy",onHide:()=>f("copy"),togglableOnFocus:!0,placement:"center",containerStyle:g.copyTooltip,content:w==="copy"?C("copyButton.copyTooltip"):C("copyButton.copiedTooltip"),children:t.jsxs(F,{style:g.copyButton,onPress:I=>{I.stopPropagation(),I.preventDefault(),U(m??""),f("copied")},children:[t.jsx(v,{color:a.gray[700],size:14,name:"copy-regular"}),t.jsx(l,{width:4}),t.jsxs(B,{numberOfLines:1,variant:"smallRegular",color:a.gray[700],children:["ID: ",m]})]})})]})).exhaustive()]}),t.jsx(F,{onPress:()=>y(s),style:g.closeButton,children:t.jsx(v,{name:"lake-close",size:24,color:a.gray[500]})}),i!=null&&t.jsxs(t.Fragment,{children:[t.jsx(l,{height:24}),t.jsx(L,{ref:b,role:"progressbar",style:[g.progressBar,{backgroundColor:a[r][500]}]})]})]})})}),D=()=>{const o=rt(),[s,n]=x.useState(()=>{});return x.useEffect(()=>{const e=document.createElement("div");return document.body.append(e),n(e),()=>{e.remove(),n(void 0)}},[]),s==null?null:t.jsx(J,{container:s,children:t.jsx(Z,{style:g.list,enter:W.fadeAndSlideInFromRight.enter,leave:W.fadeAndSlideInFromRight.leave,children:o.map(e=>t.jsx(lt,{uid:e.uid,variant:e.variant,title:e.title,description:e.description,error:e.error,progress:e.progress,onClose:q},e.uid))})})};D.__docgenInfo={description:"",methods:[],displayName:"ToastStack"};const d={button:{width:200}},Ot={title:"Interactivity/Toast",component:D},_=new WeakMap;it(_);const $=new Error;_.set($,"req-thvfknqp");const k=()=>t.jsxs(t.Fragment,{children:[t.jsx(D,{}),t.jsxs(tt,{title:"Toast",children:[t.jsxs(O,{title:"With only title",children:[t.jsx(u,{color:"positive",style:d.button,onPress:()=>p({variant:"success",title:"Successfully copy to clipboard !"}),children:"Open success toast"}),t.jsx(l,{height:16}),t.jsx(u,{color:"negative",style:d.button,onPress:()=>p({variant:"error",title:"Oops ! Something went wrong"}),children:"Open error toast"}),t.jsx(l,{height:16}),t.jsx(u,{color:"shakespear",style:d.button,onPress:()=>p({variant:"info",title:"Successfully copy to clipboard !"}),children:"Open success toast"}),t.jsx(l,{height:16}),t.jsx(u,{color:"warning",style:d.button,onPress:()=>p({variant:"warning",title:"Oops ! Something went wrong"}),children:"Open warning toast"})]}),t.jsxs(O,{title:"With title and description",children:[t.jsx(u,{color:"positive",style:d.button,onPress:()=>p({variant:"success",title:"Successfully copy to clipboard !",description:"You can now paste it anywhere"}),children:"Open success toast"}),t.jsx(l,{height:16}),t.jsx(u,{color:"negative",style:d.button,onPress:()=>p({variant:"error",title:"Oops ! Something went wrong",error:$,description:"Please retry or contact us"}),children:"Open error toast"}),t.jsx(l,{height:16}),t.jsx(u,{color:"shakespear",style:d.button,onPress:()=>p({variant:"info",title:"Successfully copy to clipboard !",description:"You can now paste it anywhere"}),children:"Open success toast"}),t.jsx(l,{height:16}),t.jsx(u,{color:"warning",style:d.button,onPress:()=>p({variant:"warning",title:"Oops ! Something went wrong",description:"Talk to an expert"}),children:"Open warning toast"})]}),t.jsxs(O,{title:"With long title and description",children:[t.jsx(u,{color:"positive",style:d.button,onPress:()=>p({variant:"success",title:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut"}),children:"Open success toast"}),t.jsx(l,{height:16}),t.jsx(u,{color:"positive",style:d.button,onPress:()=>p({variant:"success",title:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut"}),children:"Open success toast"})]})]})]});k.__docgenInfo={description:"",methods:[],displayName:"Default"};var A,N,z;k.parameters={...k.parameters,docs:{...(A=k.parameters)==null?void 0:A.docs,source:{originalSource:`() => {
+  return <>
+      <ToastStack />
+
+      <StoryBlock title="Toast">
+        <StoryPart title="With only title">
+          <LakeButton color="positive" style={styles.button} onPress={() => showToast({
+          variant: "success",
+          title: "Successfully copy to clipboard !"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="negative" style={styles.button} onPress={() => showToast({
+          variant: "error",
+          title: "Oops ! Something went wrong"
+        })}>
+            Open error toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="shakespear" style={styles.button} onPress={() => showToast({
+          variant: "info",
+          title: "Successfully copy to clipboard !"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="warning" style={styles.button} onPress={() => showToast({
+          variant: "warning",
+          title: "Oops ! Something went wrong"
+        })}>
+            Open warning toast
+          </LakeButton>
+        </StoryPart>
+
+        <StoryPart title="With title and description">
+          <LakeButton color="positive" style={styles.button} onPress={() => showToast({
+          variant: "success",
+          title: "Successfully copy to clipboard !",
+          description: "You can now paste it anywhere"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="negative" style={styles.button} onPress={() => showToast({
+          variant: "error",
+          title: "Oops ! Something went wrong",
+          error,
+          description: "Please retry or contact us"
+        })}>
+            Open error toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="shakespear" style={styles.button} onPress={() => showToast({
+          variant: "info",
+          title: "Successfully copy to clipboard !",
+          description: "You can now paste it anywhere"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="warning" style={styles.button} onPress={() => showToast({
+          variant: "warning",
+          title: "Oops ! Something went wrong",
+          description: "Talk to an expert"
+        })}>
+            Open warning toast
+          </LakeButton>
+        </StoryPart>
+
+        <StoryPart title="With long title and description">
+          <LakeButton color="positive" style={styles.button} onPress={() => showToast({
+          variant: "success",
+          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut"
+        })}>
+            Open success toast
+          </LakeButton>
+
+          <Space height={16} />
+
+          <LakeButton color="positive" style={styles.button} onPress={() => showToast({
+          variant: "success",
+          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl ut"
+        })}>
+            Open success toast
+          </LakeButton>
+        </StoryPart>
+      </StoryBlock>
+    </>;
+}`,...(z=(N=k.parameters)==null?void 0:N.docs)==null?void 0:z.source}}};const Et=["Default"];export{k as Default,Et as __namedExportsOrder,Ot as default};
+//# sourceMappingURL=Toast.stories-ueRWxgRX.js.map
