f42eaf7d7a596751bba52bbd908ffc7234afe7c4
Mathieu Acthernoene <zoontek@gmail.com>
2024-06-28 12:46:12 +0200

    Don't trim

--- scripts/release/helpers.ts
+++ scripts/release/helpers.ts
@@ -33,17 +33,15 @@ export const isExecKo = (cmd: string) =>
     .catch(() => true);
 
 export const quote = (value: string) => {
-  const trimmed = value.trim();
-
-  if (trimmed === "") {
+  if (value === "") {
     return "''";
   }
-  if (!/[^A-Za-z0-9_/:=-]/.test(trimmed)) {
-    return trimmed;
+  if (!/[^A-Za-z0-9_/:=-]/.test(value)) {
+    return value;
   }
 
   // from https://github.com/xxorax/node-shell-escape/blob/master/shell-escape.js
-  return `'${trimmed.replace(/'/g, "'\\''")}'`
+  return `'${value.replace(/'/g, "'\\''")}'`
     .replace(/^(?:'')+/g, "") // unduplicate single-quote at the beginning
     .replace(/\\'''/g, "\\'"); // remove non-escaped single-quote if there are enclosed between 2 escaped
 };
