1de73e087bbd86e8b7838417faee9234fb0926cc
Mathieu Acthernoene <zoontek@gmail.com>
2024-07-03 12:10:19 +0200

    Add deepEqual util (#487)

--- packages/lake/src/utils/object.ts
+++ packages/lake/src/utils/object.ts
@@ -23,3 +23,29 @@ export const omit = <T extends Record<PropertyKey, unknown>, const K extends key
     Object.entries(object).filter(([key]) => !disallowedKeys.has(key as K)),
   ) as Omit<T, K>;
 };
+
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+export const deepEqual = (a: unknown, b: unknown): boolean => {
+  if (Object.is(a, b)) {
+    return true;
+  }
+  if (typeof a !== "object" || a === null || typeof b !== "object" || b === null) {
+    return false;
+  }
+
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+
+  if (aKeys.length !== bKeys.length) {
+    return false;
+  }
+
+  for (const key of aKeys) {
+    if (!hasOwnProperty.call(b, key) || !deepEqual(a[key as never], b[key as never])) {
+      return false;
+    }
+  }
+
+  return true;
+};
