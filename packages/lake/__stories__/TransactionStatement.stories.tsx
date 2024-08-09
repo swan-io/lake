0dc52bb553f010d1025bd9913d552a876e21cdeb
Mathieu Acthernoene <zoontek@gmail.com>
2024-08-09 16:29:24 +0200

    Add story

--- packages/lake/__stories__/TransactionStatement.stories.tsx
+++ packages/lake/__stories__/TransactionStatement.stories.tsx
@@ -14,11 +14,25 @@ export const Default = () => {
       <StoryPart title="Default">
         <TransactionStatement
           version="v1"
-          partnerColor="#0f6fde"
           partnerLogoUrl="https://s3.eu-west-1.amazonaws.com/data.master.oina.ws/7a90851b-40f8-479d-9e0e-86d12fb001b5/SANDBOX/logo-749a2fb5-230b-4ae6-98bd-65bdb3c987c9.png"
-          style={{
-            backgroundColor: invariantColors.white,
-          }}
+          generationDate="08/12/2024"
+          executionDate="03/28/2024"
+          type="International credit transfer sent"
+          amount="12.00 EUR"
+          targetTransferAmount="10.25 GBP"
+          exchangeRate="1 EUR = 0,8548 GBP"
+          fees="1 EUR"
+          label="Transfer to Alexandra ERZINGOVIVOVA"
+          reference="12345-EIHJDKB-2849986398698-HLKHSFT"
+          debtorName="Green co"
+          debtorAccountNumber="FR7699999001001477659048183"
+          debtorBankName="Swan"
+          debtorBankIdentifier="SWNBFR22"
+          creditorName="Yellow corp"
+          creditorAccountNumber="FR7699999001001899407676183"
+          creditorBankName="Acme Bank"
+          creditorBankIdentifier="GNCFFRCQXXX"
+          style={{ backgroundColor: invariantColors.white }}
         />
       </StoryPart>
     </StoryBlock>
--- packages/shared-business/src/components/TransactionStatement.tsx
+++ packages/shared-business/src/components/TransactionStatement.tsx
@@ -4,12 +4,12 @@ import { Separator } from "@swan-io/lake/src/components/Separator";
 import { Space } from "@swan-io/lake/src/components/Space";
 import { Stack } from "@swan-io/lake/src/components/Stack";
 import { SwanLogo } from "@swan-io/lake/src/components/SwanLogo";
-import { WithPartnerAccentColor } from "@swan-io/lake/src/components/WithPartnerAccentColor";
 import { colors, fonts, interFontStyle } from "@swan-io/lake/src/constants/design";
 import { isNotNullishOrEmpty } from "@swan-io/lake/src/utils/nullish";
 import { CSSProperties } from "react";
 import { StyleProp, StyleSheet, Text, TextStyle, View, ViewStyle } from "react-native";
 import { match } from "ts-pattern";
+import { t } from "../utils/i18n";
 
 const LOGO_MAX_HEIGHT = 24;
 const LOGO_MAX_WIDTH = 150;
@@ -42,14 +42,23 @@ const styles = StyleSheet.create({
     color: colors.swan[500],
     fontWeight: "600",
   },
-  entryName: {
+  lineName: {
     ...getTextStyle("sans", 10),
     color: colors.gray[700],
   },
-  entryValue: {
+  lineValue: {
     ...getTextStyle("sans", 10),
     color: colors.swan[500],
     fontWeight: "600",
+  },
+  generationInfos: {
+    ...getTextStyle("sans", 8),
+    color: colors.gray[700],
+  },
+  footer: {
+    ...getTextStyle("sans", 8),
+    color: colors.gray[500],
+    fontWeight: "300",
   },
   defaultLogo: {
     height: LOGO_MAX_HEIGHT,
@@ -61,18 +70,44 @@ const styles = StyleSheet.create({
   },
 });
 
-const SectionEntry = ({ name, value }: { name: string; value: string }) => (
+type EntryProps = {
+  name: string;
+  value: string;
+};
+
+const Line = ({ name, value }: EntryProps) => (
   <Box direction="row" alignItems="center">
-    <Text style={styles.entryName}>{name}</Text>
+    <Text style={styles.lineName}>{name}</Text>
     <Fill minWidth={8} />
-    <Text style={styles.entryValue}>{value}</Text>
+    <Text style={styles.lineValue}>{value}</Text>
   </Box>
 );
 
 type TransactionStatementV1Props = {
   version: "v1";
-  partnerColor: string;
+
   partnerLogoUrl?: string;
+  generationDate: string;
+
+  executionDate: string;
+  type: string;
+  amount: string;
+  targetTransferAmount?: string;
+  exchangeRate?: string;
+  fees?: string;
+  label: string;
+  reference: string;
+
+  debtorName: string;
+  debtorAccountNumber: string;
+  debtorBankName?: string;
+  debtorBankIdentifier?: string;
+
+  creditorName: string;
+  creditorAccountNumber: string;
+  creditorBankName?: string;
+  creditorBankIdentifier?: string;
+
   style?: StyleProp<ViewStyle>;
 };
 
@@ -84,102 +119,130 @@ const logoStyle: CSSProperties = {
 };
 
 export const TransactionStatementV1 = ({
-  partnerColor,
   partnerLogoUrl,
+  generationDate,
+
+  executionDate,
+  type,
+  amount,
+  targetTransferAmount,
+  exchangeRate,
+  fees,
+  label,
+  reference,
+
+  debtorName,
+  debtorAccountNumber,
+  debtorBankName,
+  debtorBankIdentifier,
+
+  creditorName,
+  creditorAccountNumber,
+  creditorBankName,
+  creditorBankIdentifier,
+
   style,
 }: TransactionStatementV1Props) => {
   return (
-    <WithPartnerAccentColor color={partnerColor}>
-      <View style={[styles.container, style]}>
-        <Box direction="row" alignItems="center">
-          {isNotNullishOrEmpty(partnerLogoUrl) ? (
-            <img src={partnerLogoUrl} style={logoStyle} />
-          ) : (
-            <SwanLogo style={styles.defaultLogo} />
-          )}
+    <View style={[styles.container, style]}>
+      <Box direction="row" alignItems="center">
+        {isNotNullishOrEmpty(partnerLogoUrl) ? (
+          <img src={partnerLogoUrl} style={logoStyle} />
+        ) : (
+          <SwanLogo style={styles.defaultLogo} />
+        )}
 
-          <Separator horizontal={true} space={8} />
-          <Text style={styles.partnershipText}>{"In partnership with"}</Text>
-          <Space width={4} />
-          <SwanLogo color={colors.gray[900]} style={styles.swanLogo} />
-        </Box>
+        <Separator horizontal={true} space={8} />
+        <Text style={styles.partnershipText}>{t("transactionStatement.partnership")}</Text>
+        <Space width={4} />
+        <SwanLogo color={colors.gray[900]} style={styles.swanLogo} />
+      </Box>
 
-        <Space height={24} />
-        <Text style={styles.pageTitle}>Transaction confirmation</Text>
-        <Space height={24} />
-        <Text style={styles.sectionTitle}>Information</Text>
-        <Space height={8} />
+      <Space height={24} />
+      <Text style={styles.pageTitle}>{t("transactionStatement.title.document")}</Text>
+      <Space height={24} />
+      <Text style={styles.sectionTitle}>{t("transactionStatement.title.information")}</Text>
+      <Space height={8} />
 
-        <Stack space={8}>
-          <SectionEntry name="Execution date" value="03/28/2024" />
-          <SectionEntry name="Type" value="International credit transfer sent" />
-          <SectionEntry name="Amount" value="12.00 EUR" />
-          <SectionEntry name="Target transfer amount" value="10.25 GBP" />
-          <SectionEntry name="Exchange rate" value="1 EUR = 0,8548 GBP" />
-          <SectionEntry name="Fees" value="1 EUR" />
-          <SectionEntry name="Label" value="Transfer to Alexandra ERZINGOVIVOVA" />
-          <SectionEntry name="Reference" value="12345-EIHJDKB-2849986398698-HLKHSFT" />
-        </Stack>
+      <Stack space={8}>
+        <Line name={t("transactionStatement.information.executionDate")} value={executionDate} />
+        <Line name={t("transactionStatement.information.type")} value={type} />
+        <Line name={t("transactionStatement.information.amount")} value={amount} />
 
-        <Space height={24} />
-        <Text style={styles.sectionTitle}>Debtor information</Text>
-        <Space height={8} />
+        {isNotNullishOrEmpty(targetTransferAmount) && (
+          <Line
+            name={t("transactionStatement.information.targetTransferAmount")}
+            value={targetTransferAmount}
+          />
+        )}
 
-        <Stack space={8}>
-          <SectionEntry name="Debtor’s name" value="Green co" />
-          <SectionEntry name="Debtor's account number" value="FR7699999001001477659048183" />
-          <SectionEntry name="Debtor’s bank name" value="Swan" />
-          <SectionEntry name="Debtor's bank identifier" value="SWNBFR22" />
-        </Stack>
+        {isNotNullishOrEmpty(exchangeRate) && (
+          <Line name={t("transactionStatement.information.exchangeRate")} value={exchangeRate} />
+        )}
 
-        <Space height={24} />
-        <Text style={styles.sectionTitle}>Creditor information</Text>
-        <Space height={8} />
+        {isNotNullishOrEmpty(fees) && (
+          <Line name={t("transactionStatement.information.fees")} value={fees} />
+        )}
 
-        <Stack space={8}>
-          <SectionEntry name="Creditor’s name" value="Yellow corp" />
-          <SectionEntry name="Creditor's account number" value="FR7699999001001899407676183" />
-          <SectionEntry name="Creditor’s bank name" value="Acme Bank" />
-          <SectionEntry name="Creditor's bank identifier" value="GNCFFRCQXXX" />
-        </Stack>
+        <Line name="Label" value={label} />
+        <Line name="Reference" value={reference} />
+      </Stack>
 
-        <Fill minHeight={8} />
+      <Space height={24} />
+      <Text style={styles.sectionTitle}>{t("transactionStatement.title.debtor")}</Text>
+      <Space height={8} />
 
-        <Text
-          style={{
-            ...getTextStyle("sans", 8),
-            color: colors.gray[700],
-          }}
-        >
-          Generated on: 08/12/2024
-        </Text>
+      <Stack space={8}>
+        <Line name={t("transactionStatement.debtor.name")} value={debtorName} />
+        <Line name={t("transactionStatement.debtor.accountNumber")} value={debtorAccountNumber} />
 
-        <Space height={8} />
+        {isNotNullishOrEmpty(debtorBankName) && (
+          <Line name={t("transactionStatement.debtor.bankName")} value={debtorBankName} />
+        )}
 
-        <Text
-          style={{
-            ...getTextStyle("sans", 8),
-            color: colors.gray[700],
-          }}
-        >
-          This document is confirmation of your transaction request. It doesn't constitute proof of
-          transaction execution. Your transaction will be executed after passing a mandatory
-          verification process.
-        </Text>
+        {isNotNullishOrEmpty(debtorBankIdentifier) && (
+          <Line
+            name={t("transactionStatement.debtor.bankIdentifier")}
+            value={debtorBankIdentifier}
+          />
+        )}
+      </Stack>
 
-        <Separator space={24} />
+      <Space height={24} />
+      <Text style={styles.sectionTitle}>{t("transactionStatement.title.creditor")}</Text>
+      <Space height={8} />
 
-        <Text
-          style={{
-            ...getTextStyle("sans", 8),
-            color: colors.gray[500],
-            fontWeight: "300",
-          }}
-        >
-          {`SWAN is a simplified joint-stock company (SAS) registered with the Bobigny Trade and Companies Register under number 853 827 103, with a capital of 16,999.66 Euros, VAT number FR90853827103 and whose registered office is located at 95 Avenue du Président Wilson, 93100 Montreuil, FRANCE.\nIn its capacity as an Electronic Money institution offering payment services under French law approved by the ACPR, SWAN is registered with the latter under number 17328.`}
-        </Text>
-      </View>
-    </WithPartnerAccentColor>
+      <Stack space={8}>
+        <Line name={t("transactionStatement.creditor.name")} value={creditorName} />
+
+        <Line
+          name={t("transactionStatement.creditor.accountNumber")}
+          value={creditorAccountNumber}
+        />
+
+        {isNotNullishOrEmpty(creditorBankName) && (
+          <Line name={t("transactionStatement.creditor.bankName")} value={creditorBankName} />
+        )}
+
+        {isNotNullishOrEmpty(creditorBankIdentifier) && (
+          <Line
+            name={t("transactionStatement.creditor.bankIdentifier")}
+            value={creditorBankIdentifier}
+          />
+        )}
+      </Stack>
+
+      <Fill minHeight={8} />
+
+      <Text style={styles.generationInfos}>
+        {t("transactionStatement.generationDate", { date: generationDate })}
+      </Text>
+
+      <Space height={8} />
+      <Text style={styles.generationInfos}>{t("transactionStatement.generationInfos")}</Text>
+      <Separator space={24} />
+      <Text style={styles.footer}>{t("transactionStatement.footer")}</Text>
+    </View>
   );
 };
 
