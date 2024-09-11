940bdb838142752644f6a064998f7235a3b8b0ec
bloodyowl <bloodyowl@users.noreply.github.com>
2024-09-11 16:40:10 +0000

    deploy: e812213d2ca50886498fe2c132f8b2697383e4a2

---
+++ assets/8.14.7/CollapsibleAlert.stories-DnxBGZw0.js
@@ -0,0 +1,26 @@
+import{j as t,i as b,s as f}from"./ScrollView-CnkSV7w6.js";import{L as T}from"./LakeAlert-Dly2uCsx.js";import{L}from"./LakeButton-CC8Q2vJZ.js";import{u as v}from"./useBoolean-DHW5baxv.js";import{t as o}from"./i18n-DUohf3hG.js";import{S as x,a as i}from"./_StoriesComponents-DbmhJPm-.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BU4L-DQy.js";import"./commonStyles-t4XfA7cz.js";import"./useResponsive-CzIqCC5v.js";import"./Pressable-BGOPdkuK.js";import"./index-Ckk5_94c.js";import"./extends-CF3RwP-h.js";import"./rifm-CYIyesWt.js";import"./utc-DJvBpWoA.js";import"./string-CfGb_hXU.js";import"./array-rjEBP2Ry.js";const e=({showMoreLabel:u=o("common.showMore"),showLessLabel:c=o("common.showLess"),initialCollapsed:d=!1,children:r,...h})=>{const[a,{toggle:y}]=v(d);return t.jsx(T,{anchored:!0,...h,callToAction:b(r)?t.jsx(L,{onPress:y,mode:"tertiary",size:"small",children:a?c:u}):null,children:a?r:null})};e.__docgenInfo={description:"",methods:[],displayName:"CollapsibleLakeAlert",props:{showMoreLabel:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:'t("common.showMore")',computed:!0}},showLessLabel:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:'t("common.showLess")',computed:!0}},initialCollapsed:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}},children:{required:!1,tsType:{name:"ReactNode"},description:""}}};const s=f.create({part:{padding:10,maxWidth:600}}),E={title:"Informations/CollapsibleAlert",component:e},l=()=>t.jsxs(x,{title:"Initially collapsed",children:[t.jsx(i,{title:"info",style:s.part,children:t.jsx(e,{variant:"info",title:"Title",subtitle:"This is a subtitle",children:"This alert is initially closed"})}),t.jsx(i,{title:"Initially open",style:s.part,children:t.jsx(e,{variant:"warning",title:"Title",subtitle:"This is a subtitle",initialCollapsed:!0,children:"This alert is initially open"})}),t.jsx(i,{title:"Custom labels",style:s.part,children:t.jsx(e,{variant:"error",title:"Title",subtitle:"This is a subtitle",showLessLabel:"Give me less",showMoreLabel:"Give me more",children:"With custom labels"})}),t.jsx(i,{title:"Without children",style:s.part,children:t.jsx(e,{variant:"info",title:"Title",subtitle:"This is a subtitle"})})]});l.__docgenInfo={description:"",methods:[],displayName:"Defaults"};var n,p,m;l.parameters={...l.parameters,docs:{...(n=l.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  return <StoryBlock title="Initially collapsed">
+      <StoryPart title="info" style={styles.part}>
+        <CollapsibleLakeAlert variant="info" title="Title" subtitle="This is a subtitle">
+          This alert is initially closed
+        </CollapsibleLakeAlert>
+      </StoryPart>
+
+      <StoryPart title="Initially open" style={styles.part}>
+        <CollapsibleLakeAlert variant="warning" title="Title" subtitle="This is a subtitle" initialCollapsed={true}>
+          This alert is initially open
+        </CollapsibleLakeAlert>
+      </StoryPart>
+
+      <StoryPart title="Custom labels" style={styles.part}>
+        <CollapsibleLakeAlert variant="error" title="Title" subtitle="This is a subtitle" showLessLabel="Give me less" showMoreLabel="Give me more">
+          With custom labels
+        </CollapsibleLakeAlert>
+      </StoryPart>
+
+      <StoryPart title="Without children" style={styles.part}>
+        <CollapsibleLakeAlert variant="info" title="Title" subtitle="This is a subtitle" />
+      </StoryPart>
+    </StoryBlock>;
+}`,...(m=(p=l.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const R=["Defaults"];export{l as Defaults,R as __namedExportsOrder,E as default};
+//# sourceMappingURL=CollapsibleAlert.stories-DnxBGZw0.js.map
