0f1d9ff4d172577e5bac3693b06b06221b66fc77
bloodyowl <bloodyowl@users.noreply.github.com>
2024-10-05 18:58:22 +0000

    deploy: ae46db5dccaa4e9c9f428b1cde71b09d45e56753

---
+++ assets/8.18.10/flags-D1RAw7db.js
@@ -0,0 +1,2 @@
+const s="/lake/assets/8.18.10/flags-CcYoMcJD.svg";export{s as default};
+//# sourceMappingURL=flags-D1RAw7db.js.map
--- assets/8.18.9/flags-DJSVLebx.js
+++
@@ -1,2 +0,0 @@
-const s="/lake/assets/8.18.9/flags-CcYoMcJD.svg";export{s as default};
-//# sourceMappingURL=flags-DJSVLebx.js.map
