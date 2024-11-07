706e42b58a9027d8c8c08f5ead87adb0ee9b9413
zoontek <zoontek@users.noreply.github.com>
2024-11-07 10:51:28 +0000

    deploy: bd0835f390fe030979fffd0aa5f0a07648f4ffe4

---
+++ assets/11.1.3/RightPanel.stories-kXVjh8rk.js
@@ -0,0 +1,22 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{r as u}from"./index-Cs7sjTYM.js";import{s as d,c as n,B as y,V as s,S as l}from"./ScrollView-DAMcnhPN.js";import{L as g}from"./LakeButton-DJuoDYYV.js";import{R as c}from"./RightPanel-BTlzf3BY.js";import{S as b}from"./_StoriesComponents-4WTDdsYg.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DJFdew98.js";import"./commonStyles-t4XfA7cz.js";import"./Pressable-8N6ahZPW.js";import"./index-CJghV6hQ.js";import"./extends-CF3RwP-h.js";import"./useBodyClassName-DMoYUPTW.js";import"./FocusTrap-HMOsJfWh.js";import"./array-BfAlyugE.js";import"./Suspendable-Bl-Qq_g-.js";import"./ResponsiveContainer-BcpmGxdZ.js";import"./TransitionView-_995VKea.js";import"./Separator-qitoqTAJ.js";const t=d.create({button:{alignSelf:"flex-start"},panelContent:{padding:16},block1:{width:120,height:60,backgroundColor:n.gray[200],borderRadius:8},block2:{width:200,height:60,backgroundColor:n.gray[200],borderRadius:8},block3:{width:180,height:60,backgroundColor:n.gray[200],borderRadius:8}}),T={title:"Layout/RightPanel",component:c},o=()=>{const[h,r]=u.useState(!1),m=()=>{r(!0)};return e.jsxs(b,{title:"RightPanel",description:["RightPanel is a component that allows you to render a panel on the right side of the screen.","To make this component work, you need a root element with the id `full-page-layer-root` below your #root-app element","You can open it by clicking on the button below."],children:[e.jsx(g,{onPress:m,size:"small",style:t.button,children:"Open right panel"}),e.jsx(c,{visible:h,onPressClose:()=>r(!1),children:e.jsxs(y,{style:t.panelContent,children:[e.jsx(s,{style:t.block1}),e.jsx(l,{height:16}),e.jsx(s,{style:t.block2}),e.jsx(l,{height:16}),e.jsx(s,{style:t.block3})]})})]})};o.__docgenInfo={description:"",methods:[],displayName:"Default"};var i,a,p;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  const [isOpen, setIsOpen] = useState(false);
+  const open = () => {
+    setIsOpen(true);
+  };
+  return <StoryBlock title="RightPanel" description={["RightPanel is a component that allows you to render a panel on the right side of the screen.", "To make this component work, you need a root element with the id \`full-page-layer-root\` below your #root-app element", "You can open it by clicking on the button below."]}>
+      <LakeButton onPress={open} size="small" style={styles.button}>
+        Open right panel
+      </LakeButton>
+
+      <RightPanel visible={isOpen} onPressClose={() => setIsOpen(false)}>
+        <Box style={styles.panelContent}>
+          <View style={styles.block1} />
+          <Space height={16} />
+          <View style={styles.block2} />
+          <Space height={16} />
+          <View style={styles.block3} />
+        </Box>
+      </RightPanel>
+    </StoryBlock>;
+}`,...(p=(a=o.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};const Y=["Default"];export{o as Default,Y as __namedExportsOrder,T as default};
+//# sourceMappingURL=RightPanel.stories-kXVjh8rk.js.map
---
+++ assets/11.1.3/Suspense.stories-CbmS9kMd.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{a9 as h,aa as u}from"./ScrollView-DAMcnhPN.js";import{r as t}from"./index-Cs7sjTYM.js";import{a as S,L as c}from"./LakeButton-DJuoDYYV.js";import{S as k}from"./SwanLogo-Ca95AOwG.js";import{S as f}from"./_StoriesComponents-4WTDdsYg.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DJFdew98.js";import"./commonStyles-t4XfA7cz.js";import"./Pressable-8N6ahZPW.js";import"./index-CJghV6hQ.js";import"./extends-CF3RwP-h.js";import"./Separator-qitoqTAJ.js";const L=s=>{const o=t.useRef(s.isDone()),i=t.useRef(h.make()),[n,a]=i.current,l=t.useMemo(()=>n.toPromise(),[n]);if(t.useEffect(()=>{s.isDone()&&(a(void 0),o.current=!0)},[s,a]),!o.current)throw l},T={title:"Utils/useAsyncDataSuspense",component:k},x=({asyncData:s})=>(L(s),s.match({NotAsked:()=>e.jsx("h1",{children:"Component not asked"}),Loading:()=>e.jsx("h1",{children:"Component loading"}),Done:o=>e.jsx("h1",{children:o})})),r=()=>{const[s,o]=t.useState(0),[i,n]=t.useState(u.NotAsked());return e.jsxs(f,{title:"SwanLogo",children:[e.jsxs(S,{children:[e.jsx(c,{onPress:()=>o(a=>a+1),children:"Reset"}),e.jsx(c,{onPress:()=>n(u.NotAsked()),children:"NotAsked"}),e.jsx(c,{onPress:()=>n(u.Loading()),children:"Loading"}),e.jsx(c,{onPress:()=>n(u.Done("hello there")),children:"Done"})]}),e.jsx(t.Suspense,{fallback:e.jsx("h1",{children:"Suspense loading"}),children:e.jsx(x,{asyncData:i},s)})]})};r.__docgenInfo={description:"",methods:[],displayName:"useAsyncDataSuspenseDemos"};var p,d,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`() => {
+  const [refreshCount, setRefreshCount] = useState(0);
+  const [state, setState] = useState<AsyncData<string>>(AsyncData.NotAsked());
+  return <StoryBlock title="SwanLogo">
+      <LakeButtonGroup>
+        <LakeButton onPress={() => setRefreshCount(x => x + 1)}>Reset</LakeButton>
+        <LakeButton onPress={() => setState(AsyncData.NotAsked())}>NotAsked</LakeButton>
+        <LakeButton onPress={() => setState(AsyncData.Loading())}>Loading</LakeButton>
+        <LakeButton onPress={() => setState(AsyncData.Done("hello there"))}>Done</LakeButton>
+      </LakeButtonGroup>
+
+      <Suspense fallback={<h1>Suspense loading</h1>}>
+        <TestComponent asyncData={state} key={refreshCount} />
+      </Suspense>
+    </StoryBlock>;
+}`,...(m=(d=r.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const v=["useAsyncDataSuspenseDemos"];export{v as __namedExportsOrder,T as default,r as useAsyncDataSuspenseDemos};
+//# sourceMappingURL=Suspense.stories-CbmS9kMd.js.map
