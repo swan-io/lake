55ae6e99d5a79c436e375ecf752b8d4c44a5949c
sandrine-ds <sandrine-ds@users.noreply.github.com>
2025-05-13 08:58:00 +0000

    deploy: 9082b677f874a4ee02680317acf647578ff518f7

--- assets/12.1.0/flags-CKig6sk7.js
+++
@@ -1,2 +0,0 @@
-const s="/lake/assets/12.1.0/flags-CcYoMcJD.svg";export{s as default};
-//# sourceMappingURL=flags-CKig6sk7.js.map
---
+++ assets/12.2.0/ResponsiveContainer.stories-dPZKVSoN.js
@@ -0,0 +1,35 @@
+import{j as e}from"./jsx-runtime-BjG_zV1W.js";import{r as b}from"./index-D9D9kDko.js";import{V as l,s as x,S as n,m as y,B as s,L as r,c as u}from"./ScrollView-CRqCiH49.js";import{L as f}from"./LakeLabel-Ciqqncmo.js";import{R as p}from"./ResponsiveContainer-Dsj4u0f5.js";import{S as k}from"./Switch-C8fuqilv.js";import{S as g}from"./_StoriesComponents-fE27-gHt.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D0hBKHsB.js";import"./commonStyles-BBEkV6IC.js";import"./v4-C6aID195.js";import"./index-uPcVX-2d.js";import"./extends-CF3RwP-h.js";import"./Separator-Bkm-h-Cw.js";const t=x.create({container:{width:"100%"},containerMobile:{maxWidth:400},switchContainer:{alignSelf:"flex-start"},block:{width:120,height:60,backgroundColor:u.gray[0],borderRadius:8}}),T={title:"Layout/ResponsiveContainer",component:p},o=()=>{const[a,m]=b.useState(!1);return e.jsxs(g,{title:"ResponsiveContainer",description:["ResponsiveContainer is a component that allows you to render different content depending on the screen size.",'You can try it by toggling the "Mobile mode" switch'],children:[e.jsx(f,{label:"Mobile mode",render:()=>e.jsx(l,{style:t.switchContainer,children:e.jsx(k,{value:a,onValueChange:m})})}),e.jsx(n,{height:16}),e.jsx(l,{style:a?t.containerMobile:t.container,children:e.jsx(p,{breakpoint:y.tiny,children:({small:i})=>e.jsxs(s,{direction:i?"column":"row",children:[e.jsx(s,{justifyContent:"center",alignItems:"center",style:t.block,children:e.jsx(r,{children:i?"Mobile size":"Desktop size"})}),e.jsx(n,{width:16,height:16}),e.jsx(s,{justifyContent:"center",alignItems:"center",style:t.block,children:e.jsx(r,{children:i?"Mobile size":"Desktop size"})}),e.jsx(n,{width:16,height:16}),e.jsx(s,{justifyContent:"center",alignItems:"center",style:t.block,children:e.jsx(r,{children:i?"Mobile size":"Desktop size"})})]})})})]})};o.__docgenInfo={description:"",methods:[],displayName:"Default"};var c,d,h;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`() => {
+  const [forceMobileWidth, setForceMobileWidth] = useState(false);
+  return <StoryBlock title="ResponsiveContainer" description={["ResponsiveContainer is a component that allows you to render different content depending on the screen size.", 'You can try it by toggling the "Mobile mode" switch']}>
+      <LakeLabel label="Mobile mode" render={() => <View style={styles.switchContainer}>
+            <Switch value={forceMobileWidth} onValueChange={setForceMobileWidth} />
+          </View>} />
+
+      <Space height={16} />
+
+      <View style={forceMobileWidth ? styles.containerMobile : styles.container}>
+        <ResponsiveContainer breakpoint={breakpoints.tiny}>
+          {({
+          small
+        }) => <Box direction={small ? "column" : "row"}>
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+
+              <Space width={16} height={16} />
+
+              <Box justifyContent="center" alignItems="center" style={styles.block}>
+                <LakeText>{small ? "Mobile size" : "Desktop size"}</LakeText>
+              </Box>
+            </Box>}
+        </ResponsiveContainer>
+      </View>
+    </StoryBlock>;
+}`,...(h=(d=o.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};const _=["Default"];export{o as Default,_ as __namedExportsOrder,T as default};
+//# sourceMappingURL=ResponsiveContainer.stories-dPZKVSoN.js.map
