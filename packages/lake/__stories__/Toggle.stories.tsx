c72bc570c917e485be0afc26802b9eea7186b3a4
Mathieu Acthernoene <zoontek@gmail.com>
2025-08-22 16:03:00 +0200

    Add Toggle to lake

---
+++ packages/lake/__stories__/Toggle.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta } from "@storybook/react";
+import { useState } from "react";
+import { Box } from "../src/components/Box";
+import { Toggle } from "../src/components/Toggle";
+import { StoryBlock, StoryPart } from "./_StoriesComponents";
+
+export default {
+  title: "Forms/Toggle",
+  component: Toggle,
+} as Meta<typeof Toggle>;
+
+export const Variations = () => {
+  const [value, setValue] = useState(false);
+
+  return (
+    <StoryBlock title="Variations" description="Toggle component">
+      <StoryPart title="Desktop">
+        <Box alignItems="start">
+          <Toggle
+            value={value}
+            onToggle={setValue}
+            compact={false}
+            labelOff="Canceled"
+            labelOn="Active"
+          />
+        </Box>
+      </StoryPart>
+
+      <StoryPart title="Mobile">
+        <Box alignItems="start">
+          <Toggle
+            value={value}
+            onToggle={setValue}
+            compact={true}
+            labelOff="Canceled"
+            labelOn="Active"
+          />
+        </Box>
+      </StoryPart>
+    </StoryBlock>
+  );
+};
