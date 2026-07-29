110ce88bf48afa6a6972aa3725806417e4c2b3f2
maevags <maevags@users.noreply.github.com>
2026-07-29 09:21:47 +0000

    deploy: e804b581f749c866abe222d7a1b218514448867c

---
+++ assets/15.3.1/function-DEPlKuE-.js
@@ -0,0 +1,2 @@
+import{i as e}from"./preload-helper-B45gAKPr.js";import{E as t}from"./ScrollView-XlEVb_hC.js";var n,r=e((()=>{t(),n=()=>{}}));export{n,r as t};
+//# sourceMappingURL=function-DEPlKuE-.js.map
