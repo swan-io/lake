f0f4b44ee20c9ad381c4e2cc2692178f9b007334
maevags <maevags@users.noreply.github.com>
2026-07-31 12:30:21 +0000

    deploy: f15952dd8a9f3720dadb4b9df51b9fd9d8e0ea14

---
+++ assets/15.4.0/function-3finZp4J.js
@@ -0,0 +1,2 @@
+import{i as e}from"./preload-helper-CCSz8wUY.js";import{E as t}from"./ScrollView-CSEbU6St.js";var n,r=e((()=>{t(),n=()=>{}}));export{n,r as t};
+//# sourceMappingURL=function-3finZp4J.js.map
