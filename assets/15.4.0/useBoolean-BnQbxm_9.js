727b765fe8032e044a629876781dc281f0e9b631
maevags <maevags@users.noreply.github.com>
2026-07-31 14:31:01 +0000

    deploy: ccea4211bdd04081a426da2482252171849f780e

---
+++ assets/15.4.0/useBoolean-BnQbxm_9.js
@@ -0,0 +1,2 @@
+import{c as e,i as t}from"./preload-helper-CCSz8wUY.js";import{o as n}from"./iframe-IV4P6Ukc.js";var r,i,a=t((()=>{r=e(n()),i=e=>{let[t,n]=(0,r.useState)(e);return[t,(0,r.useMemo)(()=>({on:()=>n(!0),off:()=>n(!1),toggle:()=>n(e=>!e)}),[])]}}));export{i as n,a as t};
+//# sourceMappingURL=useBoolean-BnQbxm_9.js.map
