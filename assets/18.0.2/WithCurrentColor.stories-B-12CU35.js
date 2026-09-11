a15dd4ab8e87360dedca2de1a02cb50de6ea712f
maevags <maevags@users.noreply.github.com>
2026-09-11 15:07:19 +0000

    deploy: 31fa39a599db9cf6ce69992aca1c7b6443cfa836

---
+++ assets/18.0.2/WithCurrentColor.stories-B-12CU35.js
@@ -0,0 +1,50 @@
+import{i as e}from"./preload-helper-CCSz8wUY.js";import{Bt as t,Et as n,Ft as r,et as i,g as a,h as o,ht as s,m as c,p as l,vt as u}from"./ScrollView-CngNdHRP.js";import{r as d,t as f}from"./_StoriesComponents-CzBpHI1t.js";var p,m,h,g,_;e((()=>{n(),a(),c(),u(),d(),p=i(),m=t.create({container:{flexWrap:`wrap`},blockContainer:{width:60,height:40,margin:8},currentColorBlock:{width:`100%`,height:`100%`,borderRadius:4,backgroundColor:s.current[400]}}),h={title:`Utilities/WithCurrentColor`,component:l},g=()=>(0,p.jsx)(f,{title:`WithCurrentColor`,description:`This component change the color of all its children using 'colors.current[shade]'`,children:(0,p.jsxs)(o,{direction:`row`,style:m.container,children:[(0,p.jsx)(l,{style:m.blockContainer,variant:`gray`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`live`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`sandbox`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`positive`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`warning`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`negative`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`swan`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`shakespear`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`darkPink`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`sunglow`,children:(0,p.jsx)(r,{style:m.currentColorBlock})}),(0,p.jsx)(l,{style:m.blockContainer,variant:`mediumSladeBlue`,children:(0,p.jsx)(r,{style:m.currentColorBlock})})]})}),g.__docgenInfo={description:``,methods:[],displayName:`Colors`},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`() => {
+  return <StoryBlock title="WithCurrentColor" description="This component change the color of all its children using 'colors.current[shade]'">
+      <Box direction="row" style={styles.container}>
+        <WithCurrentColor style={styles.blockContainer} variant="gray">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="live">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="sandbox">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="positive">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="warning">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="negative">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="swan">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="shakespear">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="darkPink">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="sunglow">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+
+        <WithCurrentColor style={styles.blockContainer} variant="mediumSladeBlue">
+          <View style={styles.currentColorBlock} />
+        </WithCurrentColor>
+      </Box>
+    </StoryBlock>;
+}`,...g.parameters?.docs?.source}}},_=[`Colors`]}))();export{g as Colors,_ as __namedExportsOrder,h as default};
+//# sourceMappingURL=WithCurrentColor.stories-B-12CU35.js.map
---
+++ assets/18.0.2/function-D8PAlMDe.js
@@ -0,0 +1,2 @@
+import{i as e}from"./preload-helper-CCSz8wUY.js";import{C as t}from"./ScrollView-CngNdHRP.js";var n,r=e((()=>{t(),n=()=>{}}));export{n,r as t};
+//# sourceMappingURL=function-D8PAlMDe.js.map
